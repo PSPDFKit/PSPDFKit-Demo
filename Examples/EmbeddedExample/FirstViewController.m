3cf7eb4375e3a672611d437542bf813c80b6d5b5
Peter Steinberger <me@petersteinberger.com>
2011-08-04 16:42:37 +0200

    v1.2: add embedded example, allow viewController nesting
    Former-commit-id: da6233cbd97e0c7dbc6bdc929524514c4a7239e2

---
+++ Examples/EmbeddedExample/FirstViewController.m
@@ -0,0 +1,99 @@
+//
+//  FirstViewController.m
+//  EmbeddedExample
+//
+//  Created by Peter Steinberger on 8/4/11.
+//  Copyright 2011 Peter Steinberger. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@implementation FirstViewController
+
+@synthesize pdfController = pdfController_;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - NSObject
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil; {
+    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
+        self.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemFeatured tag:1];
+    }
+    return self;
+}
+
+- (void)dealloc {
+    [pdfController_ release];
+    [super dealloc];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - UIView
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    
+    // add pdf controller as subcontroller (iOS4 way, iOS5 has new, better methods for that)
+    NSString *path = [[[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Samples"] stringByAppendingPathComponent:@"Developers_Guide_8th.pdf"];
+    PSPDFDocument *document = [PSPDFDocument PDFDocumentWithUrl:[NSURL fileURLWithPath:path]];
+    self.pdfController = [[[PSPDFViewController alloc] initWithDocument:document] autorelease];
+    
+    self.pdfController.scrobbleBarEnabled = NO;
+    
+    [self.pdfController viewWillAppear:NO];
+    self.pdfController.view.frame = CGRectMake(80, 80, 600, 900);
+    self.pdfController.view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [self.view addSubview:self.pdfController.view];
+    [self.pdfController viewDidAppear:NO];
+    
+    // add a border
+    self.pdfController.view.layer.borderColor = [UIColor blueColor].CGColor;
+    self.pdfController.view.layer.borderWidth = 2.f;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    // relay any rotation that may happened while we were offscreen
+    UIInterfaceOrientation currentOrientation = [[UIApplication sharedApplication] statusBarOrientation];
+    if (currentOrientation != lastOrientation_) {
+        [self.pdfController willRotateToInterfaceOrientation:currentOrientation duration:0.f];
+        [self.pdfController willAnimateRotationToInterfaceOrientation:currentOrientation duration:0.f];
+        [self.pdfController didRotateFromInterfaceOrientation:lastOrientation_];
+        lastOrientation_ = currentOrientation;
+    }
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+    
+    // remove subcontroller
+    [self.pdfController viewWillDisappear:NO];
+    [self.pdfController.view removeFromSuperview];
+    [self.pdfController viewDidDisappear:NO];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return YES;
+}
+
+// relay rotation events
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+    [self.pdfController willRotateToInterfaceOrientation:toInterfaceOrientation duration:duration];
+    
+    // save orientation in case we rotate while off-screen
+    lastOrientation_ = toInterfaceOrientation;
+}
+
+- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+    [self.pdfController willAnimateRotationToInterfaceOrientation:toInterfaceOrientation duration:duration];
+}
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation {
+    [self.pdfController didRotateFromInterfaceOrientation:fromInterfaceOrientation];
+}
+
+@end
---
+++ Examples/EmbeddedExample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  EmbeddedExample
+//
+//  Created by Peter Steinberger on 8/4/11.
+//  Copyright 2011 Peter Steinberger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
