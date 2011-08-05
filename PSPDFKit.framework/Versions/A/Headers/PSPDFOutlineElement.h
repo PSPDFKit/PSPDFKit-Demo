739389232d1483decffbe439447f4723bbda5b26
Peter Steinberger <me@petersteinberger.com>
2011-08-06 00:03:16 +0200

    push v1.2.3: improved outline, parse rotated pdfs, bugfixes

--- PSPDFKit.framework/Versions/A/Headers/PSPDFOutlineElement.h
+++ PSPDFKit.framework/Versions/A/Headers/PSPDFOutlineElement.h
@@ -10,18 +10,22 @@
     NSString *title_;
     NSUInteger page_;
     NSArray *elements_;
+    NSUInteger level_;
 }
 
-// init
-- (id)initWithTitle:(NSString *)title page:(NSUInteger)page elements:(NSArray *)elements;
+/// init
+- (id)initWithTitle:(NSString *)title page:(NSUInteger)page elements:(NSArray *)elements level:(NSUInteger)level;
 
-// outline title
-@property (nonatomic, retain) NSString *title;
+/// outline title
+@property(nonatomic, retain) NSString *title;
 
-// page reference
-@property (nonatomic, assign) NSUInteger page;
+/// page reference
+@property(nonatomic, assign) NSUInteger page;
 
-// child elements
-@property (nonatomic, retain, readonly) NSArray *elements;
+/// child elements
+@property(nonatomic, retain, readonly) NSArray *elements;
+
+// current outline level
+@property(nonatomic, assign) NSUInteger level;
 
 @end
