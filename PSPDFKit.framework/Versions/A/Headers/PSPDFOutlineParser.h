15fc9bce25393b97bf8cecc3580bc9273c9abe4a
Peter Steinberger <me@petersteinberger.com>
2011-08-06 00:05:48 +0200

    add missing header

---
+++ PSPDFKit.framework/Versions/A/Headers/PSPDFOutlineParser.h
@@ -0,0 +1,28 @@
+//
+//  PSPDFOutlineParser.h
+//  PSPDFKit
+//
+//  Created by Peter Steinberger on 8/2/11.
+//  Copyright 2011 Peter Steinberger. All rights reserved.
+//
+
+@class PSPDFOutlineElement;
+
+#define kPSPDFOutlineParserErrorDomain @"kPSPDFOutlineParserErrorDomain"
+
+@interface PSPDFOutlineParser : NSObject {
+    PSPDFDocument *document_;
+    NSMutableDictionary *namedDestinations_;
+    NSArray *outline_;
+}
+
+/// init outline parser with document
+- (id)initWithDocument:(PSPDFDocument *)document;
+
+/// parse document, return outline (PSDFOutlineElements)
+- (NSArray *)parseDocument;
+
+/// returns cached outline. starts parsing if outline is not yet created.
+@property(nonatomic, retain, readonly) NSArray *outline;
+
+@end
