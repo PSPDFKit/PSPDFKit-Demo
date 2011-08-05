3830ace9250a8f5818fbef4890d31ef6896c1eae
Peter Steinberger <me@petersteinberger.com>
2011-08-06 00:03:16 +0200

    push v1.2.3: improved outline, parse rotated pdfs, bugfixes
    Former-commit-id: 5c74bf640875a27d9811b34b305e9c9dfa26c117

--- PSPDFKit.framework/Versions/A/Headers/PSPDFCache.h
+++ PSPDFKit.framework/Versions/A/Headers/PSPDFCache.h
@@ -15,16 +15,16 @@ enum {
 }typedef PSPDFCacheStrategy;
 
 enum {
-    PSPDFSizeNative,     // single page portrait device screen size
-    PSPDFSizeThumbnail,  // defined as above in kPSPDFThumbnailSize
-    PSPDFSizeTiny        // tiny is memory-only
+    PSPDFSizeNative,     /// single page portrait device screen size
+    PSPDFSizeThumbnail,  /// defined as above in kPSPDFThumbnailSize
+    PSPDFSizeTiny        /// tiny is memory-only
 }typedef PSPDFSize;
 
 @protocol PSPDFCacheDelegate
 - (void)didCachePageForDocument:(PSPDFDocument *)document page:(NSUInteger)page image:(UIImage *)cachedImage size:(PSPDFSize)size;
 @end
 
-// queue item for the cache
+/// queue item for the cache
 @interface PSPDFCacheQueuedDocument : NSObject {
     NSString *uid_;
     NSUInteger page_;
@@ -38,7 +38,7 @@ enum {
 @property(assign, getter=isCaching) BOOL caching;
 @end
 
-// renders magazine pages and caches them
+/// renders magazine pages and caches them
 @interface PSPDFCache : NSObject <NSCacheDelegate> {
     NSMutableDictionary *cachedFiles_;
     BOOL cacheFileDictLoaded_;
@@ -60,74 +60,74 @@ enum {
     NSString *cachedCacheDirectory_;
 }
 
-// cache is a singleton
+/// cache is a singleton
 + (PSPDFCache *)sharedPSPDFCache;
 
-// check if document is cached
+/// check if document is cached
 - (BOOL)isImageCachedForDocument:(PSPDFDocument *)document page:(NSUInteger)page size:(PSPDFSize)size;
 
-// returns cached image of document. If not found, add to TOP of current caching queue
+/// returns cached image of document. If not found, add to TOP of current caching queue
 - (UIImage *)cachedImageForDocument:(PSPDFDocument *)document page:(NSUInteger)page size:(PSPDFSize)size;
 
-// returns cached image of document. preload decompresses the image in the background.
+/// returns cached image of document. preload decompresses the image in the background.
 - (UIImage *)cachedImageForDocument:(PSPDFDocument *)document page:(NSUInteger)page size:(PSPDFSize)size preload:(BOOL)preload;
 
-// start document caching (update often to improve cache hits)
+/// start document caching (update often to improve cache hits)
 - (void)cacheDocument:(PSPDFDocument *)aDocument startAtPage:(NSUInteger)startPage size:(PSPDFSize)size;
 
-// stop document caching
+/// stop document caching
 - (void)stopCachingDocument:(PSPDFDocument *)aDocument;
 
-// clear cache
+/// clear cache
 - (void)removeCacheForDocument:(PSPDFDocument *)aDocument deleteDocument:(BOOL)deleteMagazine;
 
-// clear whole cache directory
+/// clear whole cache directory
 - (BOOL)clearCache;
 
-// delegate (uses MAZeroWeakRef to weak/nil out deallocated delegates)
+/// delegate (uses MAZeroWeakRef to weak/nil out deallocated delegates)
 - (void)addDelegate:(id<PSPDFCacheDelegate>)aDelegate;
 
-// you *can* manually remove delegates, but it's not needed. return YES on success
+/// you *can* manually remove delegates, but it's not needed. return YES on success
 - (BOOL)removeDelegate:(id<PSPDFCacheDelegate>)aDelegate;
 
-// set up caching strategy
+/// set up caching strategy
 @property(assign) PSPDFCacheStrategy strategy;
 
-// maximum number of cached documents
+/// maximum number of cached documents
 @property(assign) NSUInteger numberOfMaximumCachedDocuments;
 
-// only relevant in strategy PSPDFCacheOnlyThumbnailsAndNearPages
+/// only relevant in strategy PSPDFCacheOnlyThumbnailsAndNearPages
 @property(assign) NSUInteger numberOfNearCachedPages;
 
 
-// PNG needs ~200% more space, but is both faster in creation, loading and looks nicer. Defaults to NO.
+/// PNG needs ~200% more space, but is both faster in creation, loading and looks nicer. Defaults to NO.
 @property(assign) BOOL largeImagesUseJPGFormat;
 
-// compression strength used. Defaults to 0.8
+/// compression strength used. Defaults to 0.8
 @property(assign) CGFloat JPGFormatCompression;
 
-// defaults to CGSizeMake(200, 400);
+/// defaults to CGSizeMake(200, 400);
 @property(assign) CGSize thumbnailSize;
 
-// defaults to CGSizeMake(50, 100);
+/// defaults to CGSizeMake(50, 100);
 @property(assign) CGSize tinySize;
 
-// cache files are saved in a subdirectory of NSCachesDirectory. Defaults to "PSPDFKit".
+/// cache files are saved in a subdirectory of NSCachesDirectory. Defaults to "PSPDFKit".
 @property(nonatomic, copy) NSString *cacheDirectory;
 
 @end
 
 
-// additional thumbnail cache control helper
+/// additional thumbnail cache control helper
 @interface PSPDFCache (PSPDFKitThumbnailCache)
 
-// save image in an NSCache object for specified identifier.
+/// save image in an NSCache object for specified identifier.
 - (void)cacheImage:(UIImage *)image document:(PSPDFDocument *)document page:(NSUInteger)page size:(PSPDFSize)size;
 
-// load image for a certain document page
+/// load image for a certain document page
 - (UIImage *)imageForDocument:(PSPDFDocument *)document page:(NSUInteger)page size:(PSPDFSize)size;
 
-// clear thumbnail memory cache
+/// clear thumbnail memory cache
 - (void)clearThumbnailMemoryCache;
 
 @end
--- PSPDFKit.framework/Versions/A/Headers/PSPDFDocument.h
+++ PSPDFKit.framework/Versions/A/Headers/PSPDFDocument.h
@@ -8,7 +8,7 @@
 
 #import "PSPDFCache.h"
 
-@class PSPDFDocumentSearcher;
+@class PSPDFDocumentSearcher, PSPDFOutlineParser;
 
 // Represents a single, logical, pdf document. (one or many pdf files)
 // Can be overriden to support custom collections.
@@ -20,80 +20,85 @@
     
     BOOL aspectRatioEqual_;
     BOOL searchEnabled_;
+    BOOL outlineEnabled_;
     BOOL isDestroyed_;
     NSUInteger pageCount_;
     NSMutableDictionary *rectBoxCache_;
     NSMutableDictionary *pageCountCache_;
     
-#ifdef kPSPDFKitSearchFeature
     PSPDFDocumentSearcher *documentSearcher_;
-#endif
+    PSPDFOutlineParser *outlineParser_;
 }
 
-// initialize empty PSPDFDocument 
+/// initialize empty PSPDFDocument 
 + (PSPDFDocument *)PDFDocument;
 
-// initalize PSPDFDocument with distinct path and an array of files
+/// initalize PSPDFDocument with distinct path and an array of files
 + (PSPDFDocument *)PDFDocumentWithBaseUrl:(NSURL *)baseUrl files:(NSArray *)files;
 
-// initializes PSPDFDocument with a single file
+/// initializes PSPDFDocument with a single file
 + (PSPDFDocument *)PDFDocumentWithUrl:(NSURL *)url;
 
 - (id)init;
 - (id)initWithUrl:(NSURL *)url;
 - (id)initWithBaseUrl:(NSURL *)basePath files:(NSArray *)files;
 
-// returns path for a single page (in case pages are split up)
+/// returns path for a single page (in case pages are split up)
 - (NSURL *)pathForPage:(NSUInteger)page;
 
-// return pdf page count
+/// return pdf page count
 - (NSUInteger)pageCount;
 
-// return pdf page number. this may be different if a collection of pdfs is used a one big document
+/// return pdf page number. this may be different if a collection of pdfs is used a one big document
 - (NSUInteger)pageNumberForPage:(NSUInteger)page;
 
-// aspect ration format is automatically cached and analyzed per page
+/// aspect ration format is automatically cached and analyzed per page
 - (CGRect)rectBoxForPage:(NSUInteger)page;
 
-// if you change internal properties (like file count), cache needs to be cleared
+/// if you change internal properties (like file count), cache needs to be cleared
 - (void)clearCache;
 
-// creats internal cache for faster display. override to provide custom caching. called in a thread.
+/// creats internal cache for faster display. override to provide custom caching. called in a thread.
 - (void)fillCache;
 
-// return plain thumbnail path, if thumbnail already exists. override if you pre-provide thumbnails. Returns nil on default.
+/// return plain thumbnail path, if thumbnail already exists. override if you pre-provide thumbnails. Returns nil on default.
 - (NSURL *)thumbnailPathForPage:(NSUInteger)page;
 
-// return true if you want drawOverlayRect to be called.
+/// return true if you want drawOverlayRect to be called.
 - (BOOL)shouldDrawOverlayRectForSize:(PSPDFSize)size;
 
-// can be overridden to draw an overlay on the pdf - will be called from drawing threads!
+/// can be overridden to draw an overlay on the pdf - will be called from drawing threads!
 - (void)drawOverlayRect:(CGRect)rect inContext:(CGContextRef)context forPage:(NSUInteger)page zoomScale:(CGFloat)zoomScale size:(PSPDFSize)size;
 
-// defaults to nil. can be overridden to provide custom text.
-// if this returns nil for a site, we'll try to extract text ourselves
+/// defaults to nil. can be overridden to provide custom text.
+/// if this returns nil for a site, we'll try to extract text ourselves
 - (NSString *)pageContentForPage:(NSUInteger)page;
 
-// document title as shown in the controller
+/// document title as shown in the controller
 @property(nonatomic, copy) NSString *title;
 
-// For caching, provide a unique uid here
+/// For caching, provide a unique uid here
 @property(nonatomic, copy) NSString *uid;
 
-// common base path for pdf files
+/// common base path for pdf files
 @property(nonatomic, retain) NSURL *basePath;
 
-// array of pdf files
+/// array of pdf files
 @property(nonatomic, copy) NSArray *files;
 
-// if aspect ratio is equal on all pages, you can enable this for even better performance. Defaults to YES.
+/// if aspect ratio is equal on all pages, you can enable this for even better performance. Defaults to YES.
 @property(nonatomic, assign, getter=isAspectRatioEqual, readonly) BOOL aspectRatioEqual;
 
-// text extraction is not possible for all pdfs. disable search if not working. Defaults to YES if kPSPDFKitSearchFeature is set.
+/// text extraction is not possible for all pdfs. disable search if not working. Defaults to YES.
 @property(nonatomic, assign, getter=isSearchEnabled) BOOL searchEnabled;
 
-#ifdef kPSPDFKitSearchFeature
-@property (nonatomic, retain, readonly) PSPDFDocumentSearcher *documentSearcher;
-#endif
+/// outline/ table of contents extraction. disable if no outline available. Defaults to YES.
+@property(nonatomic, assign, getter=isOutlineEnabled) BOOL outlineEnabled;
+
+/// Text extraction class for current document. Readonly.
+@property(nonatomic, retain, readonly) PSPDFDocumentSearcher *documentSearcher;
+
+/// Outline extraction class for current document. Readonly.
+@property(nonatomic, retain, readonly) PSPDFOutlineParser *outlineParser;
 
 @end
