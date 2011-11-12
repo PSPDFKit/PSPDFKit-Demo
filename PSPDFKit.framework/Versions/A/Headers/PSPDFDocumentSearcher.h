21498e8596b85b73520e5807d1b9ff0e03bf712c
Peter Steinberger <me@petersteinberger.com>
2011-11-12 22:17:13 +0100

    push v1.6.17 - new api to extract text

--- PSPDFKit.framework/Versions/A/Headers/PSPDFDocumentSearcher.h
+++ PSPDFKit.framework/Versions/A/Headers/PSPDFDocumentSearcher.h
@@ -11,7 +11,7 @@
 #define kSearchPreviewSubStringStart 20
 #define kSearchPreviewSubStringEnd 60
 
-// Helper class to extract text from a pdf
+/// Helper class to extract text from a pdf
 @interface PSPDFDocumentSearcher : NSObject {
     PSPDFDocument *document_;
     CGPDFOperatorTableRef table_;
@@ -19,17 +19,22 @@
     NSMutableString *currentData_;
 }
 
-// main init
+/// initialize with the document
 - (id)initWithDocument:(PSPDFDocument *)document;
 
-// YES if we extracted all text from the document
+// YES if text could be extracted
 - (BOOL)isReadyToSearch;
 
-// starts document parsing. further calls will be ignored.
+/// parses the document. further calls will be ignored.
 - (void)parseDocument;
 
+/// searches for text occurence. If document was not yet parsed, it will be now.
 - (NSArray *)searchForString:(NSString *)searchText;
 
+/// get text for a specific page. Page starts at 0. Returns nil if no text is available or page is incorrect.
+- (NSString *)textForPage:(NSUInteger)page;
+
+/// the document that was set
 @property (nonatomic, assign, readonly) PSPDFDocument *document;
 
 @end
