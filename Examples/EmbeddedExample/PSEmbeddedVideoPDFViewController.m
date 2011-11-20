4a2286322be14bee7db7c3fb1ac108f029d8865c
Peter Steinberger <me@petersteinberger.com>
2011-11-20 12:25:57 +0100

    simplify embedded video example

--- Examples/EmbeddedExample/PSEmbeddedVideoPDFViewController.m
+++ Examples/EmbeddedExample/PSEmbeddedVideoPDFViewController.m
@@ -10,8 +10,6 @@
 
 @implementation PSEmbeddedVideoPDFViewController
 
-@synthesize moviePlayer = moviePlayer_;
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 #pragma mark - NSObject
 
@@ -24,23 +22,7 @@
 }
 
 - (void)dealloc {
-    [moviePlayer_ stop];
-    [moviePlayer_ release];
     [super dealloc];
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-#pragma mark - UIView
-
-- (void)viewWillDisappear:(BOOL)animated {
-    [super viewWillDisappear:animated];
-    
-    [self.moviePlayer pause];
-}
-
-- (void)viewDidUnload {
-    [super viewDidUnload];
-    self.moviePlayer = nil;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -50,63 +32,5 @@
 - (UIBarButtonItem *)toolbarBackButton {
     return nil;
 }
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-#pragma mark - PSPDFViewControllerDelegate
-
-- (void)pdfViewController:(PSPDFViewController *)pdfController didShowPage:(NSUInteger)page {
-
-}
-
-- (void)pdfViewController:(PSPDFViewController *)pdfController didLoadPage:(NSUInteger)page pdfScrollView:(PSPDFScrollView *)pdfScrollView; {
-
-    // in a more complex example, you may wanna parse mockup data (e.g. from a JSON) and parse this to determine
-    // which videos should be added where. There also be more than one player active, so you could use a dictionary to reference them.
-    if (page == 1) {
-        if (!self.moviePlayer) {
-            MPMoviePlayerController *moviePlayer = [[MPMoviePlayerController alloc] initWithContentURL:[NSURL URLWithString:@"http://km.support.apple.com/library/APPLE/APPLECARE_ALLGEOS/HT1211/sample_iTunes.mov"]];
-            self.moviePlayer = moviePlayer;
-            moviePlayer.view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-            
-            // add black border
-            moviePlayer.view.layer.borderColor = [UIColor blackColor].CGColor;
-            moviePlayer.view.layer.borderWidth = 1.f;
-        }
-        
-        CGRect pageRect = pdfScrollView.compoundView.frame;
-        CGSize videoSize = PSIsIpad() ? CGSizeMake(500, 375) : CGSizeMake(220, 170);
-        CGRect videoFrame = CGRectMake(floorf((pageRect.size.width-videoSize.width)/2), floorf((pageRect.size.width-videoSize.height)/2), videoSize.width, videoSize.height);
-        self.moviePlayer.view.frame = videoFrame;
-        [pdfScrollView.compoundView addSubview:self.moviePlayer.view];
-
-        // auto-play
-        [self.moviePlayer play];
-        
-                
-        // in your full featured app, you may wanna customized the controls
-        //moviePlayer.controlStyle = MPMovieControlStyleNone;
-        
-        // animate movieplayer appearance
-        /*
-        self.moviePlayer.view.alpha = 0.f;
-        [UIView animateWithDuration:0.25f delay:0.5f options:UIViewAnimationOptionAllowUserInteraction animations:^(void) {
-            self.moviePlayer.view.alpha = 1.f;
-        } completion:nil];
-         */
-        
-        // you can also embeddd images (which may be replaced by an MPMoviePlayerController on click)
-        //UIImageView *testImage = [[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"star"]] autorelease];
-        //[testImage sizeToFit];
-        //[pdfScrollView.compoundView addSubview:testImage];
-    }    
-}
-
-- (void)pdfViewController:(PSPDFViewController *)pdfController didUnloadPage:(NSUInteger)page pdfScrollView:(PSPDFScrollView *)pdfScrollView; {
-    if (page == 1) {
-        [self.moviePlayer pause];
-        [self.moviePlayer.view removeFromSuperview];
-    }
-}
-
 
 @end
