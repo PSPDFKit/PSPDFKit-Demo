928e92f6923155f077ff669b6dc8228877b735f4
Peter Steinberger <me@petersteinberger.com>
2011-11-25 21:57:50 +0100

    remove custom drawAnnotation code
    PSPDFKit 1.7 has a custom class for this (PSPDFLinkAnnotation)

--- Examples/PSPDFKitExample/PSPDFMagazine.m
+++ Examples/PSPDFKitExample/PSPDFMagazine.m
@@ -70,48 +70,6 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 #pragma mark - PSPDFDocument
 
-- (void)drawAnnotations:(NSArray *)annotations inContext:(CGContextRef)context pageInfo:(PSPDFPageInfo *)pageInfo pageRect:(CGRect)pageRect; {
-    // only render if annotation parser is available
-    if ([annotations count]) {
-        float yellowComponents[4] = { 1, 1, 0.3f, 1 };
-        CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
-        CGContextSetStrokeColorSpace(context, rgbColorSpace);
-        CGColorRef yellow = CGColorCreate(rgbColorSpace, yellowComponents);
-        CGContextSetStrokeColorWithColor(context, yellow);
-        
-        for (PSPDFAnnotation *linkAnnotation in annotations) {
-            
-            // keynote is creating weird double links; ignore the small "shadow" link
-            if (linkAnnotation.pdfRectangle.size.height <= 5) {
-                continue;
-            }
-            
-            CGPoint pt1 = [PSPDFTilingView convertPDFPointToViewPoint:linkAnnotation.pdfRectangle.origin rect:pageInfo.pageRect rotation:pageInfo.pageRotation pageRect:pageRect];
-            CGPoint pt2 = CGPointMake(linkAnnotation.pdfRectangle.origin.x + linkAnnotation.pdfRectangle.size.width, 
-                                      linkAnnotation.pdfRectangle.origin.y + linkAnnotation.pdfRectangle.size.height);
-            pt2 = [PSPDFTilingView convertPDFPointToViewPoint:pt2 rect:pageInfo.pageRect rotation:pageInfo.pageRotation pageRect:pageRect];
-            
-            CGRect linkRectangle = CGRectMake(pt1.x, pt1.y, pt2.x - pt1.x, pt2.y - pt1.y);
-            
-            // add round rect path
-            CGFloat radius = 1.f;                         
-            CGFloat minx = CGRectGetMinX(linkRectangle), midx = CGRectGetMidX(linkRectangle), maxx = CGRectGetMaxX(linkRectangle); 
-            CGFloat miny = CGRectGetMinY(linkRectangle), midy = CGRectGetMidY(linkRectangle), maxy = CGRectGetMaxY(linkRectangle); 
-            CGContextMoveToPoint(context, minx, midy); 
-            CGContextAddArcToPoint(context, minx, miny, midx, miny, radius); 
-            CGContextAddArcToPoint(context, maxx, miny, maxx, midy, radius); 
-            CGContextAddArcToPoint(context, maxx, maxy, midx, maxy, radius); 
-            CGContextAddArcToPoint(context, minx, maxy, minx, midy, radius); 
-            CGContextClosePath(context); 
-            
-            CGContextStrokePath(context);
-        }
-        
-        CGColorRelease(yellow);
-        CGColorSpaceRelease(rgbColorSpace);
-    }
-}
-
 - (void)setDownloading:(BOOL)downloading {
     if (downloading != downloading_) {
         downloading_ = downloading;
