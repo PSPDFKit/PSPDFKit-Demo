20c2004a037aa6430f151cfd04a31895a3553557
Peter Steinberger <steipete@gmail.com>
2011-12-01 11:48:30 +0100

    add comments about the controllers

--- Examples/EmbeddedExample/PSPDFAnnotationTestController.h
+++ Examples/EmbeddedExample/PSPDFAnnotationTestController.h
@@ -5,6 +5,7 @@
 //  Copyright (c) 2011 Peter Steinberger. All rights reserved.
 //
 
+/// Example to show the multimedia annotation features.
 @interface PSPDFAnnotationTestController : PSPDFViewController <PSPDFViewControllerDelegate> {
 }
 
--- Examples/EmbeddedExample/PSPDFEmbeddedTestController.h
+++ Examples/EmbeddedExample/PSPDFEmbeddedTestController.h
@@ -5,6 +5,7 @@
 //  Copyright 2011 Peter Steinberger. All rights reserved.
 //
 
+/// Exmaple how to correctly embedd (in an iOS4 compatible way) the view controller within another view controller.
 @interface PSPDFEmbeddedTestController : UIViewController
 
 - (IBAction)appendDocument;
