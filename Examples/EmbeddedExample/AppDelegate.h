35b20b1e82163c09d132326656ebcc044f65e344
Peter Steinberger <steipete@gmail.com>
2011-12-01 11:48:15 +0100

    remove clutter

--- Examples/EmbeddedExample/AppDelegate.h
+++ Examples/EmbeddedExample/AppDelegate.h
@@ -5,8 +5,6 @@
 //  Copyright 2011 Peter Steinberger. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 #define kDevelopersGuideFileName @"DevelopersGuide.pdf"
 #define kPaperExampleFileName @"amazon-dynamo-sosp2007.pdf"
 #define kMacbookAirFileName @"macbook_air_users_guide.pdf"
