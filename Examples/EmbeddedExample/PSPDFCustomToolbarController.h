08697e9c0bcbb667bb7d3bd252c485b6e27f1a50
Peter Steinberger <steipete@gmail.com>
2011-12-01 11:48:30 +0100

    add comments about the controllers

--- Examples/EmbeddedExample/PSPDFCustomToolbarController.h
+++ Examples/EmbeddedExample/PSPDFCustomToolbarController.h
@@ -5,6 +5,7 @@
 //  Copyright 2011 Peter Steinberger. All rights reserved.
 //
 
+/// Example to show how to add a custom toolbar.
 @interface PSPDFCustomToolbarController : PSPDFViewController <PSPDFViewControllerDelegate> {
     UISegmentedControl *customViewModeSegment_;
 }
