3b7273eff555a911d53198ead745cada900b6596
Peter Steinberger <steipete@gmail.com>
2011-12-01 11:48:15 +0100

    remove clutter
    Former-commit-id: a700fc5d6aa7b5df8bb8187f3fe9f278b0782ad4

--- Examples/EmbeddedExample/main.m
+++ Examples/EmbeddedExample/main.m
@@ -2,11 +2,8 @@
 //  main.m
 //  EmbeddedExample
 //
-//  Created by Peter Steinberger on 8/4/11.
 //  Copyright 2011 Peter Steinberger. All rights reserved.
 //
-
-#import <UIKit/UIKit.h>
 
 int main(int argc, char *argv[]) {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
