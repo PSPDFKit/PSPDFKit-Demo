2e7bb74081ca8af9c91db9c990210cb561f8a1ed
Peter Steinberger <me@petersteinberger.com>
2011-12-14 18:47:31 +0100

    replace AGGridView with GMGridView, improve open animation
    Former-commit-id: b6972cc70ec72ee372938647098c8b1e3fbbc21f

--- Examples/PSPDFKitExample/PSPDFGridController.h
+++ Examples/PSPDFKitExample/PSPDFGridController.h
@@ -5,21 +5,19 @@
 //  Copyright 2011 Peter Steinberger. All rights reserved.
 //
 
-#import "PSPDFGridView.h"
 #import "PSPDFBasicViewController.h"
 #import "PSPDFStoreManager.h"
 
 @class PSPDFMagazineFolder;
 
-@interface PSPDFGridController : PSPDFBasicViewController <PSPDFStoreManagerDelegate, AQGridViewDelegate, AQGridViewDataSource> {    
+@interface PSPDFGridController : PSPDFBasicViewController <PSPDFStoreManagerDelegate, GMGridViewActionDelegate, GMGridViewDataSource> {    
     CGRect baseGridPosition_;
-    NSUInteger lastNumbersOfItemsInGridView_;
 }
 
 - (id)initWithMagazineFolder:(PSPDFMagazineFolder *)aMagazineFolder;
 - (void)updateGrid;
 
-@property(nonatomic, strong) PSPDFGridView *gridView;
+@property(nonatomic, strong) GMGridView *gridView;
 @property(nonatomic, strong, readonly) PSPDFMagazineFolder *magazineFolder;
 
 @end
