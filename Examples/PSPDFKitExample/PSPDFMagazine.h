d63fd9f6f0303fba8b08df9305b736799e54ff6d
Peter Steinberger <me@petersteinberger.com>
2011-12-23 13:13:01 +0100

    don't allow non-deletable magazines to be deleted

--- Examples/PSPDFKitExample/PSPDFMagazine.h
+++ Examples/PSPDFKitExample/PSPDFMagazine.h
@@ -7,25 +7,32 @@
 
 @class PSPDFMagazineFolder;
 
+/// Represents a magazine in the PDFKitExample.
 @interface PSPDFMagazine : PSPDFDocument
 
+/// Initializes a magazine object with the specified path.
 + (PSPDFMagazine *)magazineWithPath:(NSString *)path;
 
+/// Returns the coverImage, which is the first page of the magazine.
+/// Here we download that from the web to have sth to show before the pdf is available.
 - (UIImage *)coverImage;
 
-/// magazine folder
+/// Magazine folder. Weak to break the retain cycle.
 @property(nonatomic, ps_weak) PSPDFMagazineFolder *folder; // weak!
 
-/// url for downloading the pdf
+/// Url for downloading the pdf.
 @property(nonatomic, strong) NSURL *url;
 
-// url for downloading image
+// Url for downloading image.
 @property(nonatomic, strong) NSURL *imageUrl;
 
-/// true if magazine is currently downloading
+/// UES if magazine is currently downloading.
 @property(nonatomic, assign, getter=isDownloading) BOOL downloading;
 
-/// true if magazine is on-disk and/or sucessfully downloaded
+/// YES if magazine is on-disk and/or sucessfully downloaded.
 @property(nonatomic, assign, getter=isAvailable) BOOL available;
+
+/// YES if the magazine can be deleted. NO if it's within the app bundle, which can't be edited.
+@property(nonatomic, assign, getter=isDeletable, readonly) BOOL deletable;
 
 @end
