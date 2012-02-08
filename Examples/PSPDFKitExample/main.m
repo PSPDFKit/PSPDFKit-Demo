80830f927ffaa016b4dfd729bcba0bbed9bfea24
Peter Steinberger <me@petersteinberger.com>
2012-02-07 21:52:54 -0800

    don't hardcode AppDelegate-string

--- Examples/PSPDFKitExample/main.m
+++ Examples/PSPDFKitExample/main.m
@@ -5,9 +5,11 @@
 //  Copyright 2011 Peter Steinberger. All rights reserved.
 //
 
+#import "AppDelegate.h"
+
 int main(int argc, char *argv[]) {
     @autoreleasepool {
-        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        int retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
         return retVal;
     }
 }
