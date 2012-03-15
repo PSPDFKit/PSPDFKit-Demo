0776b9a16e6e9f367ca18acbd06263e08bf896ba
Peter Steinberger <me@petersteinberger.com>
2012-03-14 23:27:33 -0700

    clean up minimal example
    Former-commit-id: 5278cc3c62efcb817c8a93289b50b0870a250734

--- Examples/MinimalExample-ARC/main.m
+++ Examples/MinimalExample-ARC/main.m
@@ -2,16 +2,13 @@
 //  main.m
 //  MinimalExample
 //
-//  Created by Peter Steinberger on 7/26/11.
 //  Copyright 2011 Peter Steinberger. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-
 #import "MinimalExampleAppDelegate.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([MinimalExampleAppDelegate class]));
     }
