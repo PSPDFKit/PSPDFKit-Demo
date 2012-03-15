3e254b702e385baaf0ce3952b6bd3d1648874e72
Peter Steinberger <me@petersteinberger.com>
2012-03-14 23:27:33 -0700

    clean up minimal example

--- Examples/MinimalExample-ARC/MinimalExampleAppDelegate.m
+++ Examples/MinimalExample-ARC/MinimalExampleAppDelegate.m
@@ -2,26 +2,10 @@
 //  MinimalExampleAppDelegate.m
 //  MinimalExample
 //
-//  Created by Peter Steinberger on 7/26/11.
 //  Copyright 2011 Peter Steinberger. All rights reserved.
 //
 
 #import "MinimalExampleAppDelegate.h"
-
-// this is one way of overriding PSPDFViewController. However, subclassing is advised
-@implementation PSPDFViewController (PSPDFMinimalExampleCategoryOverride)
-
-// fixes warning on Xcode 4.3
-#pragma clang diagnostic push
-#if __clang_minor__ > 0
-#pragma clang diagnostic ignored "-Wobjc-protocol-method-implementation"
-#endif
-- (UIBarButtonItem *)toolbarBackButton {
-    return nil;
-}
-#pragma clang diagnostic pop
-
-@end
 
 @implementation MinimalExampleAppDelegate
 
@@ -30,9 +14,15 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
-    NSString *path = [[[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Samples"] stringByAppendingPathComponent:@"DevelopersGuide.pdf"];
+    NSString *path = [[[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Samples"] stringByAppendingPathComponent:@"PSPDFKit.pdf"];
     PSPDFDocument *document = [PSPDFDocument PDFDocumentWithUrl:[NSURL fileURLWithPath:path]];
+    
+    //kPSPDFKitDebugScrollViews = YES;
+        
     PSPDFViewController *pdfController = [[PSPDFViewController alloc] initWithDocument:document];
+    pdfController.pageCurlEnabled = YES;
+	pdfController.pageMode = PSPDFPageModeDouble;
+
     UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:pdfController];
     self.window.rootViewController = navController;
     
@@ -40,5 +30,16 @@
     [self.window makeKeyAndVisible];
     return YES;
 }
+
+@end
+
+// this is one way of overriding PSPDFViewController. However, subclassing is advised
+@implementation PSPDFViewController (PSPDFMinimalExampleCategoryOverride)
+
+// fixes warning on Xcode 4.3
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wobjc-protocol-method-implementation"
+- (UIBarButtonItem *)toolbarBackButton { return nil; }
+#pragma clang diagnostic pop
 
 @end
