073815481aeb62cee0059db83eb59fc57637ed43
Peter Steinberger <me@petersteinberger.com>
2012-03-28 20:42:31 -0700

    Add new example for Storyboarding
    Former-commit-id: a199c665ab5e54b80a499aa836e47d44c193bdd2

---
+++ Examples/StoryboardExample/PSTableViewController.h
@@ -0,0 +1,12 @@
+//
+//  PSTableViewController.h
+//  StoryboardExample
+//
+//  Copyright (c) 2012 Peter Steinberger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSTableViewController : UITableViewController
+
+@end
