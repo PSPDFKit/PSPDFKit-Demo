694a7057e7711dda2a441047bf2c6f18d6bb2285
Peter Steinberger <me@petersteinberger.com>
2012-03-28 20:42:31 -0700

    Add new example for Storyboarding

---
+++ Examples/StoryboardExample/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  StoryboardExample
+//
+//  Copyright (c) 2012 Peter Steinberger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PSAppDelegate.h"
+
+int main(int argc, char *argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PSAppDelegate class]));
+    }
+}
