4999d291ab4add72ed5690d5fd800d9e0b1e7372
Peter Steinberger <me@petersteinberger.com>
2012-03-28 22:29:05 -0700

    1.9.11 - Support for Storyboarding, new aspectRatioVariance for PSPDFDocument, big fixes.
    Former-commit-id: 2257faa9984c0c3c0d0ff48ffd779239a8f3b8a4

--- PSPDFKit.framework/Versions/A/Headers/PSPDFPageView.h
+++ PSPDFKit.framework/Versions/A/Headers/PSPDFPageView.h
@@ -9,14 +9,14 @@
 #import <UIKit/UIKit.h>
 #import "PSPDFKitGlobal.h"
 
-@class PSPDFScrollView, PSPDFTilingView, PSPDFDocument;
+@class PSPDFScrollView, PSPDFTilingView, PSPDFDocument, PSPDFViewController;
 
 /// Compound view for a single pdf page. Will not be re-used for different pages.
 /// You can add your own views on top of the UIView (e.g. custom annotations)
 @interface PSPDFPageView : UIView 
 
 /// configure page container with data.
-- (void)displayDocument:(PSPDFDocument *)document page:(NSUInteger)page pageRect:(CGRect)pageRect scale:(CGFloat)scale;
+- (void)displayDocument:(PSPDFDocument *)document page:(NSUInteger)page pageRect:(CGRect)pageRect scale:(CGFloat)scale pdfController:(PSPDFViewController *)pdfController;
 
 /// destroys and removes CATiledLayer. Call prior deallocating.
 /// Don't set removeFromView to YES if destroy is *not* on the main thread.
@@ -26,7 +26,12 @@
 - (void)setBackgroundImage:(UIImage *)image animated:(BOOL)animated;
 
 /// Access parent PSPDFScrollView if available. (zoom controller)
-@property(nonatomic, ps_weak, readonly) PSPDFScrollView *scrollView;
+/// Note: this only lets you access the scrollView if it's in the view hiararchy.
+/// If we use pageCurl mode, we have a global scrollView which can be accessed with pdfController.pagingScrollView
+- (PSPDFScrollView *)scrollView;
+
+/// Access pdfController
+@property(nonatomic, ps_weak, readonly) PSPDFViewController *pdfController;
 
 /// Page that is displayed. Readonly.
 @property(nonatomic, assign, readonly) NSUInteger page;
