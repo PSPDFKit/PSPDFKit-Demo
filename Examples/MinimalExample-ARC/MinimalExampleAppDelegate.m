4b747740f2cd1913903ee16e418b8509f3d53562
Peter Steinberger <me@petersteinberger.com>
2012-04-01 04:14:03 -0700

    update example

--- Examples/MinimalExample-ARC/MinimalExampleAppDelegate.m
+++ Examples/MinimalExample-ARC/MinimalExampleAppDelegate.m
@@ -7,6 +7,12 @@
 
 #import "MinimalExampleAppDelegate.h"
 
+// if you use PSPDFKit.framework
+#import <PSPDFKit/PSPDFKit.h>
+
+// if you use PSPDFKit-lib.xcodeproj
+//#import "PSPDFKit.h"
+
 @implementation MinimalExampleAppDelegate
 
 @synthesize window = _window;
@@ -17,16 +23,15 @@
     NSString *path = [[[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Samples"] stringByAppendingPathComponent:@"PSPDFKit.pdf"];
     PSPDFDocument *document = [PSPDFDocument PDFDocumentWithUrl:[NSURL fileURLWithPath:path]];
     
-    //kPSPDFKitDebugScrollViews = YES;
-        
     PSPDFViewController *pdfController = [[PSPDFViewController alloc] initWithDocument:document];
-    pdfController.pageCurlEnabled = YES;
-	pdfController.pageMode = PSPDFPageModeDouble;
+     pdfController.pageCurlEnabled = YES;
+    [pdfController scrollToPage:[pdfController landscapePage:5] animated:NO];
+    //	pdfController.pageMode = PSPDFPageModeDouble;
 
     UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:pdfController];
     self.window.rootViewController = navController;
     
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor blackColor];
     [self.window makeKeyAndVisible];
     return YES;
 }
