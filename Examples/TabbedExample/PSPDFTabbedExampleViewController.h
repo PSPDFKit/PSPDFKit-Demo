3a14c976f65d748d1bba2db37ff94122b12505c3
Peter Steinberger <me@petersteinberger.com>
2012-05-01 12:23:02 +0200

    don't manually include header here
    Former-commit-id: eebc50ce3bb9f1006065f2ba39adacb1c9a5ee48

--- Examples/TabbedExample/PSPDFTabbedExampleViewController.h
+++ Examples/TabbedExample/PSPDFTabbedExampleViewController.h
@@ -5,8 +5,6 @@
 //  Copyright (c) 2012 Peter Steinberger. All rights reserved.
 //
 
-#import "PSPDFTabbedViewController.h"
-
 @interface PSPDFTabbedExampleViewController : PSPDFTabbedViewController <PSPDFTabbedViewControllerDelegate>
 
 @end
