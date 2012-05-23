63d423de047d3e1ff6672a7cad1519b6057c1f7e
Peter Steinberger <me@petersteinberger.com>
2012-05-23 20:12:53 +0200

    UIResponder is new in iOS5.

--- Examples/EmbeddedExample/AppDelegate.h
+++ Examples/EmbeddedExample/AppDelegate.h
@@ -10,7 +10,7 @@
 #define kMacbookAirFileName @"macbook_air_users_guide.pdf"
 #define kPSPDFKitExample @"PSPDFKit.pdf"
 
-@interface AppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
 
 @property (nonatomic, strong) IBOutlet UIWindow *window;
 @property (nonatomic, strong) IBOutlet UITabBarController *tabBarController;
