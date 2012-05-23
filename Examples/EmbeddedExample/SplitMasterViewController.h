aa4f5cb69122b0c26943e8d862524d955d356247
Peter Steinberger <me@petersteinberger.com>
2012-05-23 20:13:03 +0200

    cleanup
    Former-commit-id: a2986258264a654629887ed6492b814b14d0bcf7

--- Examples/EmbeddedExample/SplitMasterViewController.h
+++ Examples/EmbeddedExample/SplitMasterViewController.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2011-2012 Peter Steinberger. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 @interface SplitMasterViewController : UIViewController <UISplitViewControllerDelegate, PSPDFViewControllerDelegate> {
     PSPDFViewController *pdfController_;
     UIPopoverController *masterPopoverController_;
