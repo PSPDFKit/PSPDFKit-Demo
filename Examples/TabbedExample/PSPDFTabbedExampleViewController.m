eec4a940f4efd415495a063374c219322a79aec0
Peter Steinberger <me@petersteinberger.com>
2012-05-24 15:26:35 +0200

    persistence is now a feature of PSPDFTabbedViewController

--- Examples/TabbedExample/PSPDFTabbedExampleViewController.m
+++ Examples/TabbedExample/PSPDFTabbedExampleViewController.m
@@ -11,63 +11,20 @@
 @implementation PSPDFTabbedExampleViewController
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-#pragma mark - Private
-
-#define kPSPDFTabbedDocumentsPersistKey @"kPSPDFTabbedDocumentsPersistKey"
-
-- (void)restoreState {
-    NSData *archiveData = [[NSUserDefaults standardUserDefaults] objectForKey:kPSPDFTabbedDocumentsPersistKey];
-    NSDictionary *persistDict = nil;
-    if (archiveData) {
-        NSLog(@"restoring state.");
-        @try {
-            // This method raises an NSInvalidArchiveOperationException if data is not a valid archive.
-            persistDict = [NSKeyedUnarchiver unarchiveObjectWithData:archiveData];
-        }
-        @catch (NSException *exception) {
-            NSLog(@"Failed to restore persist state: %@", exception);
-            [[NSUserDefaults standardUserDefaults] removeObjectForKey:kPSPDFTabbedDocumentsPersistKey];
-        }
-    }
-    if (persistDict) {
-        self.documents = [persistDict objectForKey:NSStringFromSelector(@selector(documents))];
-        self.visibleDocument = [persistDict objectForKey:NSStringFromSelector(@selector(visibleDocument))];
-    }
-}
-
-- (void)persistState {
-    NSLog(@"persisting state.");
-    NSMutableDictionary *persistDict = [NSMutableDictionary dictionaryWithCapacity:2];
-    if (self.documents) {
-        [persistDict setObject:self.documents forKey:NSStringFromSelector(@selector(documents))];
-    }
-    if (self.visibleDocument) {
-        [persistDict setObject:self.visibleDocument forKey:NSStringFromSelector(@selector(visibleDocument))];
-    }
-    NSData *archive = [NSKeyedArchiver archivedDataWithRootObject:persistDict];
-    [[NSUserDefaults standardUserDefaults] setObject:archive forKey:kPSPDFTabbedDocumentsPersistKey];
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
 #pragma mark - NSObject
 
 - (id)initWithDocuments:(NSArray *)documents pdfViewController:(PSPDFViewController *)pdfViewController {
     if ((self = [super initWithDocuments:documents pdfViewController:pdfViewController])) {
         self.pdfViewController.pageCurlEnabled = YES;
+
+        // enable automatic peristance and retore the last state
+        self.enableAutomaticStatePersistance = YES;
         [self restoreState];
 
         PSPDFAddDocumentsBarButtonItem *addDocumentsButton = [[PSPDFAddDocumentsBarButtonItem alloc] initWithPDFViewController:self.pdfViewController];
         self.pdfViewController.leftBarButtonItems = [NSArray arrayWithObject:addDocumentsButton];
-
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(persistState) name:UIApplicationDidEnterBackgroundNotification object:nil];
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(persistState) name:UIApplicationWillTerminateNotification object:nil];
     }
     return self;
-}
-
-- (void)dealloc {
-    [self persistState];
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
