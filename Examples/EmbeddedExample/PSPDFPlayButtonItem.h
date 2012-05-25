f5f47aae97475ea677a760a0e9bc4cb1559f1237
Peter Steinberger <me@petersteinberger.com>
2012-05-25 18:56:24 +0200

    remove import

--- Examples/EmbeddedExample/PSPDFPlayButtonItem.h
+++ Examples/EmbeddedExample/PSPDFPlayButtonItem.h
@@ -5,8 +5,6 @@
 //  Copyright (c) 2012 Peter Steinberger. All rights reserved.
 //
 
-#import "PSPDFBarButtonItem.h"
-
 #define kPSPDFSlideshowDuration 2.0f
 
 // Adds a play/pause button that can enable a slideshow mode for pdf documents.
