891fd690bfa195102923b1b81ac91483b7fc5f8c
Peter Steinberger <me@petersteinberger.com>
2012-07-07 03:07:23 +0200

    modernize code
    Former-commit-id: bdd7a27770d210e4d477fca1e5b25ca27af18e76

--- Examples/EmbeddedExample/IntelligentSplitViewController.m
+++ Examples/EmbeddedExample/IntelligentSplitViewController.m
@@ -1,11 +1,9 @@
 //
-//  IntelligentSplitViewController.m
-//  From TexLege by Gregory S. Combs
+//  SplitMasterViewController.m
+//  EmbeddedExample
 //
-//  Released under the Creative Commons Attribution 3.0 Unported License
-//  Please see the included license page for more information.
-//
-//  In a nutshell, you can use this, just attribute this to me in your "thank you" notes or about box.
+//  Copyright (c) 2011-2012 Peter Steinberger. All rights reserved.
+//  Original code by Gregory S. Combs, Creative Commons Attribution 3.0 Unported License.
 //
 
 #import "IntelligentSplitViewController.h"
@@ -13,17 +11,20 @@
 
 @implementation IntelligentSplitViewController
 
-- (id) init {
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - NSObject
+
+- (id)init {
 	if ((self = [super init])) {
 		[[NSNotificationCenter defaultCenter] addObserver:self
 												 selector:@selector(willRotate:)
 													 name:UIApplicationWillChangeStatusBarOrientationNotification object:nil];
-		
+
 		[[NSNotificationCenter defaultCenter] addObserver:self
 												 selector:@selector(didRotate:)
 													 name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
 
-        // disable, as this breaks our gesture recognizers for pageCurl.
+        // iOS 5.1: Disable, as this breaks our gesture recognizers for pageCurl.
         // TODO: find a way to hook into their gesture recognizers to create a dependency.
 #if __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_5_0
         if ([self respondsToSelector:@selector(setPresentsWithGesture:)]) {
@@ -35,72 +36,57 @@
 }
 
 - (void)dealloc {
-	@try {
-		[[NSNotificationCenter defaultCenter] removeObserver:self];
-	}
-	@catch (NSException * e) {
-		NSLog(@"IntelligentSplitViewController DE-OBSERVING CRASHED: %@ ... error:%@", self.title, [e description]);
-	}
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - UIViewController
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     return YES;
 }
 
 - (void)willRotate:(id)sender {
-	if (![self isViewLoaded]) // we haven't even loaded up yet, let's turn away from this place
-		return;
-		  
+	if (![self isViewLoaded] || !sender) return;
+
 	NSNotification *notification = sender;
-	if (!notification)
-		return;
-	
 	UIInterfaceOrientation toOrientation = [[notification.userInfo valueForKey:UIApplicationStatusBarOrientationUserInfoKey] integerValue];
 
 	UITabBarController *tabBar = self.tabBarController;
 	BOOL notModal = (!tabBar.modalViewController);
 	BOOL isSelectedTab = [self.tabBarController.selectedViewController isEqual:self];
-	
+
 	NSTimeInterval duration = [[UIApplication sharedApplication] statusBarOrientationAnimationDuration];
 
-	if (!isSelectedTab || !notModal)  { 
+	if (!isSelectedTab || !notModal)  {
 		// Looks like we're not "visible" ... propogate rotation info
 		[super willRotateToInterfaceOrientation:toOrientation duration:duration];
-		
+
 		UIViewController *master = [self.viewControllers objectAtIndex:0];
 		NSObject *theDelegate = (NSObject *)self.delegate;
 
-		
-#define YOU_DONT_FEEL_QUEAZY_ABOUT_THIS_BECAUSE_IT_PASSES_THE_APP_STORE 1		
-		
-#if YOU_DONT_FEEL_QUEAZY_ABOUT_THIS_BECAUSE_IT_PASSES_THE_APP_STORE
-		UIBarButtonItem *button = [super valueForKey:[NSString stringWithFormat:@"%@ttonItem", @"_barBu"]];
-		
-#else //YOU_DO_FEEL_QUEAZY_AND_FOR_SOME_REASON_YOU_PREFER_THE_LESSER_EVIL_____FRIGHTENING_STUFF
-		UIBarButtonItem *button = [[[[[self.viewControllers objectAtIndex:1] 
-									  viewControllers] objectAtIndex:0] 
-									navigationItem] rightBarButtonItem];
-#endif
-		
+        // careful... uses obfuscated private API.
+        UIBarButtonItem *button = [super valueForKey:[NSString stringWithFormat:@"%@ttonItem", @"_barBu"]];
+
 		if (UIInterfaceOrientationIsPortrait(toOrientation)) {
-			if (theDelegate && [theDelegate respondsToSelector:@selector(splitViewController:willHideViewController:withBarButtonItem:forPopoverController:)]) {
+			if ([theDelegate respondsToSelector:@selector(splitViewController:willHideViewController:withBarButtonItem:forPopoverController:)]) {
 
 				@try {
-					UIPopoverController *popover = [super valueForKey:@"_hiddenPopoverController"];
+					UIPopoverController *popover = [super valueForKey:[NSString stringWithFormat:@"%@denPopoverController", @"_hid"]];
 					objc_msgSend(theDelegate, @selector(splitViewController:willHideViewController:withBarButtonItem:forPopoverController:), self, master, button, popover);
 				}
-				@catch (NSException * e) {
-					NSLog(@"There was a nasty error while notifyng splitviewcontrollers of an orientation change: %@", [e description]);
+				@catch (NSException *e) {
+					PSPDFLogError(@"There was a nasty error while notifyng splitviewcontrollers of an orientation change: %@", [e description]);
 				}
 			}
 		}
 		else if (UIInterfaceOrientationIsLandscape(toOrientation)) {
-			if (theDelegate && [theDelegate respondsToSelector:@selector(splitViewController:willShowViewController:invalidatingBarButtonItem:)]) {
+			if ([theDelegate respondsToSelector:@selector(splitViewController:willShowViewController:invalidatingBarButtonItem:)]) {
 				@try {
 					objc_msgSend(theDelegate, @selector(splitViewController:willShowViewController:invalidatingBarButtonItem:), self, master, button);
 				}
 				@catch (NSException * e) {
-					NSLog(@"There was a nasty error while notifyng splitviewcontrollers of an orientation change: %@", [e description]);
+					PSPDFLogError(@"There was a nasty error while notifing splitviewcontrollers of an orientation change: %@", [e description]);
 				}
 			}
 		}
@@ -108,19 +94,16 @@
 }
 
 - (void)didRotate:(id)sender {
-	if (![self isViewLoaded]) // we haven't even loaded up yet, let's turn away from this place
-		return;
+	if (![self isViewLoaded] || !sender) return;
 
 	NSNotification *notification = sender;
-	if (!notification)
-		return;
 	UIInterfaceOrientation fromOrientation = [[notification.userInfo valueForKey:UIApplicationStatusBarOrientationUserInfoKey] integerValue];
-	
+
 	UITabBarController *tabBar = self.tabBarController;
 	BOOL notModal = (!tabBar.modalViewController);
 	BOOL isSelectedTab = [self.tabBarController.selectedViewController isEqual:self];
-	
-	if (!isSelectedTab || !notModal)  { 
+
+	if (!isSelectedTab || !notModal)  {
 		// Looks like we're not "visible" ... propogate rotation info
 		[super didRotateFromInterfaceOrientation:fromOrientation];
 	}
