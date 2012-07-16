1882c1c86a7ed7aab66726b16bd6f7f01e822b6f
Peter Steinberger <me@petersteinberger.com>
2012-07-16 22:06:59 +0200

    API update

--- PSPDFKit.framework/Versions/A/Headers/PSPDFMenuItem.h
+++ PSPDFKit.framework/Versions/A/Headers/PSPDFMenuItem.h
@@ -8,11 +8,12 @@
 #import "PSPDFKitGlobal.h"
 
 // Oh UIMenuItem, why don't you have a target action pattern? Well, lets make one with blocks!
-// Note: This has one flaw, it doesn't work if the titles are equal inside on UIMenuController
+// Note: This has one flaw, it doesn't work if the titles are equal inside on UIMenuController.
 //       (but that would be a weird use case anyway)
 @interface PSPDFMenuItem : UIMenuItem
 
 // Initialize UIMenuItem with a block.
+// If you use PSPDFMenuItem but the classic initWithTitle:selector initializer, this will work and be handled just like a UIMenuItem.
 - (id)initWithTitle:(NSString *)title block:(PSPDFBasicBlock)block;
 
 // Menu Item can be enabled/disabled. (disable will hide it from the UIMenuController)
@@ -20,16 +21,5 @@
 
 // Install menu handler to an object. Can be called multiple times.
 + (void)installMenuHandlerForObject:(id)object;
-
-@end
-
-
-@interface PSPDFMenuItem (Trampoline)
-
-// Performs the action in the menu item.
-- (void)performBlock;
-
-// Custom selector to identify the MenuItem.
-@property(nonatomic, assign, readonly) SEL customSelector;
 
 @end
