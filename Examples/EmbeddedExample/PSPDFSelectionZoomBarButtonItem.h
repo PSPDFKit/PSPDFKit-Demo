e4d6849e4b4e6cb6be4bb14afd901f8af7785b48
Peter Steinberger <me@petersteinberger.com>
2012-07-21 15:11:00 +0200

    improve embedded example

---
+++ Examples/EmbeddedExample/PSPDFSelectionZoomBarButtonItem.h
@@ -0,0 +1,12 @@
+//
+//  PSPDFSelectionZoomBarButtonItem.h
+//  EmbeddedExample
+//
+//  Copyright (c) 2012 Peter Steinberger. All rights reserved.
+//
+
+#import "PSPDFBarButtonItem.h"
+
+@interface PSPDFSelectionZoomBarButtonItem : PSPDFBarButtonItem
+
+@end
