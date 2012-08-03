f9ed9ff5c804d35d7483d6a720bc035bdc2c7223
Peter Steinberger <me@petersteinberger.com>
2012-08-04 00:03:02 +0200

    revert to UIToolbar; else icons jump.

--- Examples/EmbeddedExample/PSPDFPlayButtonItem.m
+++ Examples/EmbeddedExample/PSPDFPlayButtonItem.m
@@ -8,6 +8,7 @@
 #import "PSPDFPlayButtonItem.h"
 
 @implementation PSPDFPlayButtonItem {
+    PSPDFTransparentToolbar *_toolbar;
     NSTimer *_autoplayTimer;
     BOOL _autoplay;
 }
@@ -16,22 +17,46 @@
     return [super isAvailable] && self.pdfController.viewMode == PSPDFViewModeDocument;
 }
 
-- (UIBarButtonSystemItem)systemItem {
-    return _autoplay ? UIBarButtonSystemItemPause : UIBarButtonSystemItemPlay;
+// If we would return UIBarButtonSystemItem, UIKit would auto-calculate the width and there - surprise! - is a 3px difference between play/pause, making all icons afterwards to jump.
+//- (UIBarButtonSystemItem)systemItem {
+//    return _autoplay ? UIBarButtonSystemItemPause : UIBarButtonSystemItemPlay;
+//}
+
+// a UIToolbar is used instead of an UIButton to get the automatic shadows on UIBarButtonItem icons.
+- (UIToolbar *)toolbar {
+    if (!_toolbar) {
+        _toolbar = [[PSPDFTransparentToolbar alloc] initWithFrame:CGRectMake(0, 0, 22, 44)];
+        _toolbar.autoresizingMask = UIViewAutoresizingFlexibleHeight;
+        _toolbar.barStyle = self.pdfController.navigationController.navigationBar.barStyle;
+        _toolbar.tintColor = self.pdfController.tintColor;
+        [self updatePlayButton];
+    }
+    return _toolbar;
 }
 
-- (void)action:(id)sender {
+- (UIView *)customView {
+    return self.toolbar;
+}
+
+- (void)updatePlayButton {
+    UIBarButtonSystemItem systemItem = _autoplay ? UIBarButtonSystemItemPause : UIBarButtonSystemItemPlay;
+    UIBarButtonItem *flexibleSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:NULL];
+    UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:systemItem target:self action:@selector(playPauseAction:)];
+    [self.toolbar setItems:@[flexibleSpace, barButtonItem, flexibleSpace] animated:YES];
+}
+
+- (void)playPauseAction:(id)sender {
     [PSPDFBarButtonItem dismissPopoverAnimated:NO];
-    
+
     if (!_autoplay) {
         _autoplay = YES;
         _autoplayTimer = [NSTimer scheduledTimerWithTimeInterval:kPSPDFSlideshowDuration target:self.pdfController selector:@selector(advanceToNextPage) userInfo:nil repeats:YES];
+        [self updatePlayButton];
     }else {
         _autoplay = NO;
         [_autoplayTimer invalidate];
+        [self updatePlayButton];
     }
-
-    [self.pdfController updateBarButtonItem:self animated:YES];
 }
 
 @end
