ea22a04b34e3d5bc8d65ea323ff58b08bde86da3
Peter Steinberger <me@petersteinberger.com>
2012-08-07 16:51:10 +0200

    add example how to customize and intercept calls to PSPDFBookmarkParser
    Former-commit-id: c4165ed72d2f9a36a55884b60907900a4e04aa90

---
+++ Examples/PSPDFCatalog/Subclassing/PSCBookmarkParser.h
@@ -0,0 +1,13 @@
+//
+//  PSCBookmarkParser.h
+//  PSPDFCatalog
+//
+//  Copyright (c) 2012 PSPDFKit. All rights reserved.
+//
+
+#import "PSPDFBookmarkParser.h"
+
+/// Example how to subclass the bookmark parser to relay the bookmark data.
+@interface PSCBookmarkParser : PSPDFBookmarkParser
+
+@end
