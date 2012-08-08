bb91f411570d2144792e40fa6659ec5a8a9d1333
Peter Steinberger <me@petersteinberger.com>
2012-08-08 23:33:36 +0200

    improve bookmark parser subclass
    Former-commit-id: 2f05a8ee8f2b967f16462af569945a271323c1d9

--- Examples/PSPDFCatalog/Subclassing/PSCBookmarkParser.h
+++ Examples/PSPDFCatalog/Subclassing/PSCBookmarkParser.h
@@ -5,8 +5,6 @@
 //  Copyright (c) 2012 PSPDFKit. All rights reserved.
 //
 
-#import "PSPDFBookmarkParser.h"
-
 /// Example how to subclass the bookmark parser to relay the bookmark data.
 @interface PSCBookmarkParser : PSPDFBookmarkParser
 
--- Examples/PSPDFCatalog/Subclassing/PSCBookmarkParser.m
+++ Examples/PSPDFCatalog/Subclassing/PSCBookmarkParser.m
@@ -9,6 +9,21 @@
 
 @implementation PSCBookmarkParser
 
+- (BOOL)addBookmarkForPage:(NSUInteger)page {
+    NSLog(@"Add Bookmark: %d", page);
+    return [super addBookmarkForPage:page];
+}
+
+- (BOOL)removeBookmarkForPage:(NSUInteger)page {
+    NSLog(@"Remove Bookmark: %d", page);
+    return [super removeBookmarkForPage:page];
+}
+
+// block bookmark loading
+- (NSArray *)loadBookmarks {
+    return [NSArray array];
+}
+
 - (void)saveBookmarks {
     dispatch_async(dispatch_get_main_queue(), ^{
         [[[UIAlertView alloc] initWithTitle:@"Bookmark Subclass Message" message:[NSString stringWithFormat:@"Intercepted bookmark saving; current bookmarks are: %@", self.bookmarks] delegate:nil cancelButtonTitle:PSPDFLocalize(@"Ok") otherButtonTitles:nil] show];
