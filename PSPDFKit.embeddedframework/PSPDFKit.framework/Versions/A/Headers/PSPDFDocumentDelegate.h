fd3fe953f24383e81c7569b1a5be91aa764af80a
Peter Steinberger <me@petersteinberger.com>
2012-08-09 13:02:12 +0200

    update API

--- PSPDFKit.embeddedframework/PSPDFKit.framework/Versions/A/Headers/PSPDFDocumentDelegate.h
+++ PSPDFKit.embeddedframework/PSPDFKit.framework/Versions/A/Headers/PSPDFDocumentDelegate.h
@@ -10,6 +10,8 @@
 
 @protocol PSPDFDocumentDelegate <NSObject>
 
+@optional
+
 /// Allow resolving custom path tokens (Documents, Bundle are automatically resolved; you can add e.g. Book and resolve this here). Will only get called for unknown tokens.
 - (NSString *)pdfDocument:(PSPDFDocument *)document resolveCustomAnnotationPathToken:(NSString *)pathToken; // return nil if unknown.
 
