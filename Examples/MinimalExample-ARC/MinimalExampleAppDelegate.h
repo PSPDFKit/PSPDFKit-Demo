404ab7989bd2d46ad193f8b98234f5d882401b32
Peter Steinberger <me@petersteinberger.com>
2012-08-11 12:37:45 +0200

    move header include

--- Examples/MinimalExample-ARC/MinimalExampleAppDelegate.h
+++ Examples/MinimalExample-ARC/MinimalExampleAppDelegate.h
@@ -5,15 +5,6 @@
 //  Copyright 2011-2012 Peter Steinberger. All rights reserved.
 //
 
-// remove the ifdef in your project
-#ifndef PSPDFKIT_USE_SOURCE
-// if you use PSPDFKit.framework
-#import <PSPDFKit/PSPDFKit.h>
-#else
-// if you use PSPDFKit-lib.xcodeproj
-#import "PSPDFKit.h"
-#endif
-
 @interface MinimalExampleAppDelegate : UIResponder <UIApplicationDelegate, PSPDFViewControllerDelegate>
 
 @property(strong, nonatomic) PSPDFViewController *pdfController;
