076960ac58bf6dd638945a860febf5b8254b3373
Peter Steinberger <me@petersteinberger.com>
2012-08-15 23:06:40 +0200

    add new examples

---
+++ Examples/PSPDFCatalog/Subclassing/PSCExampleAnnotationViewController.h
@@ -0,0 +1,13 @@
+//
+//  PSCExampleAnnotationViewController.h
+//  PSPDFCatalog-static
+//
+//  Created by Peter Steinberger on 8/15/12.
+//  Copyright (c) 2012 PSPDFKit. All rights reserved.
+//
+
+#import "PSPDFViewController.h"
+
+@interface PSCExampleAnnotationViewController : PSPDFViewController
+
+@end
