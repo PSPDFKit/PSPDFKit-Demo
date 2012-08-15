435057f4db45bed4bb61bd07d792e587278908c5
Peter Steinberger <me@petersteinberger.com>
2012-08-15 23:06:40 +0200

    add new examples
    Former-commit-id: 076960ac58bf6dd638945a860febf5b8254b3373

---
+++ Examples/PSPDFCatalog/Subclassing/PSCCustomDrawingViewController.h
@@ -0,0 +1,13 @@
+//
+//  PSCCustomDrawingViewController.h
+//  PSPDFCatalog-static
+//
+//  Created by Peter Steinberger on 8/15/12.
+//  Copyright (c) 2012 PSPDFKit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PSCCustomDrawingViewController : NSObject
+
+@end
