c8298bf1da5fbf813ee833b8e16b0fdcfe2b95fc
Peter Steinberger <me@petersteinberger.com>
2012-08-15 23:07:07 +0200

    add new classes

--- Examples/PSPDFCatalog/Subclassing/PSCExampleAnnotationViewController.m
+++ Examples/PSPDFCatalog/Subclassing/PSCExampleAnnotationViewController.m
@@ -1,38 +1,46 @@
 //
 //  PSCExampleAnnotationViewController.m
-//  PSPDFCatalog-static
+//  PSPDFCatalog
 //
-//  Created by Peter Steinberger on 8/15/12.
 //  Copyright (c) 2012 PSPDFKit. All rights reserved.
 //
 
 #import "PSCExampleAnnotationViewController.h"
+#import "PSCVerticalAnnotationToolbar.h"
 
 @interface PSCExampleAnnotationViewController ()
-
+@property(nonatomic, strong) PSCVerticalAnnotationToolbar *verticalToolbar;
 @end
 
 @implementation PSCExampleAnnotationViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+- (void)dealloc {
+    self.verticalToolbar.pdfController = nil; // ensure pdf controller is removed
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+
+    // create the custom toolbar and add it on top of the HUDView.
+    self.verticalToolbar = [[PSCVerticalAnnotationToolbar alloc] initWithPDFController:self];
+    self.verticalToolbar.frame = CGRectIntegral(CGRectMake(self.view.bounds.size.width-44.f, (self.view.bounds.size.height-44.f)/2, 44.f, 44.f));
+    self.verticalToolbar.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
+    [self.HUDView addSubview:self.verticalToolbar];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidUnload {
+    [super viewDidUnload];
+    self.verticalToolbar.pdfController = nil;
+    self.verticalToolbar = nil;
+}
+
+// ensure custom toolbar is hidden when we show the thumbnails
+- (void)setViewMode:(PSPDFViewMode)viewMode animated:(BOOL)animated {
+    [super setViewMode:viewMode animated:animated];
+
+    [UIView animateWithDuration:0.25 delay:0.f options:UIViewAnimationOptionAllowUserInteraction animations:^{
+        self.verticalToolbar.alpha = viewMode == PSPDFViewModeThumbnails ? 0.f : 1.f;
+    } completion:NULL];
 }
 
 @end
