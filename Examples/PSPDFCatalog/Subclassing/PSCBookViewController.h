afadc5fe2cb97fa656c2eecfc116eea722746759
Peter Steinberger <me@petersteinberger.com>
2012-09-04 01:40:36 +0200

    remove invalid import
    Former-commit-id: c8d440b27b40775591cd6bc926fb0b5d844b696f

--- Examples/PSPDFCatalog/Subclassing/PSCBookViewController.h
+++ Examples/PSPDFCatalog/Subclassing/PSCBookViewController.h
@@ -5,8 +5,6 @@
 //  Copyright (c) 2012 PSPDFKit. All rights reserved.
 //
 
-#import "PSPDFViewController.h"
-
 /**
     Example for a Book-style layout that adds custom margins around each PSPDFPageView.
  */
