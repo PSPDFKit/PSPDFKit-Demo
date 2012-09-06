1d34bce06fdadea7034bd1d61cc1c1e48fa9f358
Peter Steinberger <me@petersteinberger.com>
2012-09-06 14:11:54 +0200

    remove experimental code
    Former-commit-id: a1e82b949ada23b90230d91e9d1d1fd3aedfe1d9

--- Examples/PSPDFCatalog/PSCatalogViewController.m
+++ Examples/PSPDFCatalog/PSCatalogViewController.m
@@ -142,26 +142,7 @@
             PSPDFViewController *controller = [[PSPDFViewController alloc] initWithDocument:document];
             controller.rightBarButtonItems = @[controller.annotationButtonItem, controller.emailButtonItem, controller.searchButtonItem, controller.outlineButtonItem, controller.viewModeButtonItem];
             return controller;
-        }]];
-
-
-        // Currently broken.
-         /// And even a CGDocumentProvider (can be used for encryption)
-         [documentTests addContent:[[PSContent alloc] initWithTitle:@"Encrypted CGDocumentProvider" block:^{
-
-         NSURL *encryptedPDF = [NSURL fileURLWithPath:[[[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Samples"] stringByAppendingPathComponent:@"output.pdf.aes"]];
-
-         // Note: For shipping apps, you need to protect this string better, making it harder for hacker to simply disassemble and receive the key from the binary. Or add an internet service that fetches the key from an SSL-API. But then there's still the slight risk of memory dumping with an attached gdb. Or screenshots. Security is never 100% perfect; but using AES makes it way harder to get the PDF. You can even combine AES and a PDF password.
-         // Also, be sure to disable the cache in PSPDFCache or your document will end up unencrypted in single images on the disk.
-             NSString *passphrase = @"afghadöghdgdhfgöhapvuenröaoeruhföaeiruaerub";
-             NSString *salt = @"ducrXn9WaRdpaBfMjDTJVjUf3FApA6gtim0e61LeSGWV9sTxB0r26mPs59Lbcexn";
-
-             PSPDFAESCryptoDataProvider *cryptoWrapper = [[PSPDFAESCryptoDataProvider alloc] initWithURL:encryptedPDF passphrase:passphrase salt:salt];
-
-         PSPDFDocument *document = [PSPDFDocument PDFDocumentWithDataProvider:cryptoWrapper.dataProviderRef];
-         return [[PSPDFViewController alloc] initWithDocument:document];
-         }]];
-              
+        }]];              
 
         PSCSectionDescriptor *annotationSection = [[PSCSectionDescriptor alloc] initWithTitle:@"Annotation Tests" footer:@"PSPDFKit supports all common PDF annotations, including Highlighing, Underscore, Strikeout, Comment and Ink."];
 
