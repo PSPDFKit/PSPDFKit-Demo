f5c09553d2fdfa081c0d23503d8051385052968a
Peter Steinberger <me@petersteinberger.com>
2012-09-22 10:29:09 +0200

    clean up app delegate; add simple example to directly push PSPDFViewController

--- Examples/PSPDFCatalog/PSCAppDelegate.m
+++ Examples/PSPDFCatalog/PSCAppDelegate.m
@@ -15,13 +15,12 @@
 #error "Compile this file with ARC"
 #endif
 
-
 @implementation PSCAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     // print version of the catalog example and PSPDFKit.
-    NSString *appVersion = [[[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"] stringByReplacingOccurrencesOfString:@"@" withString:@""] stringByReplacingOccurrencesOfString:@"\"" withString:@""];
+    NSString *appVersion = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
     NSLog(@"Starting Catalog Example %@ with %@", appVersion, PSPDFVersionString());
 
 
@@ -33,17 +32,22 @@
                                      @"%d of %d" : @"Page %d of %d",
                                      @"Outline"  : @"Table of Contents"
                                    }});
-    
-    // enable to change animations (e.g. enable on iPad1)
-    kPSPDFAnimateOption = PSPDFAnimateEverywhere;
 
     // change log level to be more verbose.
     kPSPDFLogLevel = PSPDFLogLevelInfo;
 
-    // Create catalog controller
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+#if 0
+    // Directly push a PSPDFViewController
+    NSURL *samplesURL = [[[NSBundle mainBundle] resourceURL] URLByAppendingPathComponent:@"Samples"];
+    PSPDFDocument *document = [PSPDFDocument PDFDocumentWithURL:[samplesURL URLByAppendingPathComponent:kHackerMagazineExample]];
+    PSPDFViewController *viewController = [[PSPDFViewController alloc] initWithDocument:document];
+    self.catalog = [[UINavigationController alloc] initWithRootViewController:viewController];
+#else
+    // Create catalog controller
     PSCatalogViewController *catalogController = [[PSCatalogViewController alloc] initWithStyle:UITableViewStyleGrouped];
     self.catalog = [[UINavigationController alloc] initWithRootViewController:catalogController];
+#endif
     self.window.rootViewController = self.catalog;
     [self.window makeKeyAndVisible];
 
@@ -54,22 +58,21 @@
     // Perform non-important actions on the next runloop, for faster app-starting.
     dispatch_async(dispatch_get_main_queue(), ^{
 
-        // HockeyApp is a *great* service to manage crashes. It's well worth the money.
-        // If you use it, download your own, current version of the HockeySDK.
+        // HockeyApp (http://hockeyapp.net) is a *great* service to manage crashes. It's well worth the money.
+        // If you use it, download your own current version of the HockeySDK!
         // This version has been modified to work w/o a dSYM (but will not show line numbers)
-        // http://hockeyapp.net
         [[BITHockeyManager sharedHockeyManager] configureWithIdentifier:@"fa73e1f8f3806bcb3466c5ab16d70768" delegate:nil];
-        [BITHockeyManager sharedHockeyManager].crashManager.crashManagerStatus = BITCrashManagerStatusAutoSend;
+        [[BITHockeyManager sharedHockeyManager] crashManager].crashManagerStatus = BITCrashManagerStatusAutoSend;
         [[BITHockeyManager sharedHockeyManager] startManager];
 
-        // Localytics helps me to track PSPDFKit-DEMO downloads.
+        // Localytics helps me to track PSPDFCatalog launches
         // Remove this or replace with your own Localytics ID if you are using PSPDFKitExample as a template for your own app.
+#ifndef PSPDF_USE_SOURCE
         [[LocalyticsSession sharedLocalyticsSession] startSession:@"3b7cb4552ea954d48d68f0e-3451f502-de39-11e1-4ab8-00ef75f32667"];
-
+#else
         // useful for debugging
-        #ifdef DEBUG
         [PSPDFHangDetector startHangDetector];
-        #endif
+#endif
     });
     return YES;
 }
