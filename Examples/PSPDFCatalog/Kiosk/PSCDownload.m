0125fd34348139b894ca9b094ccc9743878d8f98
Peter Steinberger <me@petersteinberger.com>
2012-09-24 20:36:27 +0200

    rename headers

--- Examples/PSPDFCatalog/Kiosk/PSCDownload.m
+++ Examples/PSPDFCatalog/Kiosk/PSCDownload.m
@@ -1,6 +1,6 @@
 //
 //  PSCDownload.m
-//  PSPDFKitExample
+//  PSPDFCatalog
 //
 //  Copyright 2011-2012 Peter Steinberger. All rights reserved.
 //
--- Examples/PSPDFCatalog/Kiosk/PSCKioskPDFViewController.h
+++ Examples/PSPDFCatalog/Kiosk/PSCKioskPDFViewController.h
@@ -1,6 +1,6 @@
 //
 //  PSCKioskPDFViewController.h
-//  PSPDFKitExample
+//  PSPDFCatalog
 //
 //  Copyright 2011-2012 Peter Steinberger. All rights reserved.
 //
