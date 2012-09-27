6cfd9746a8b00a20beda8cb4f949c526b333d20b
Peter Steinberger <me@petersteinberger.com>
2012-09-28 01:10:01 +0200

    move out clear tab button

--- Examples/PSPDFCatalog/TabbedExample/PSCTabbedExampleViewController.m
+++ Examples/PSPDFCatalog/TabbedExample/PSCTabbedExampleViewController.m
@@ -7,6 +7,7 @@
 
 #import "PSCTabbedExampleViewController.h"
 #import "PSCAddDocumentsBarButtonItem.h"
+#import "PSCClearTabsButtonItem.h"
 #import <objc/runtime.h>
 
 @implementation PSCTabbedExampleViewController
@@ -24,12 +25,12 @@ const char *clearAllActionSheetToken;
         self.enableAutomaticStatePersistance = YES;
 
         // on iPhone, we want a backButton here.
-        UIBarButtonItem *clearAllButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemTrash target:self action:@selector(clearAll:)];
+        PSCClearTabsButtonItem *clearTabsButton = [[PSCClearTabsButtonItem alloc] initWithPDFViewController:self.pdfViewController];
         if (PSIsIpad()) {
             PSCAddDocumentsBarButtonItem *addDocumentsButton = [[PSCAddDocumentsBarButtonItem alloc] initWithPDFViewController:self.pdfViewController];
-            self.pdfViewController.leftBarButtonItems = @[addDocumentsButton, clearAllButton];
+            self.pdfViewController.leftBarButtonItems = @[addDocumentsButton, clearTabsButton];
         }else {
-            self.pdfViewController.leftBarButtonItems = @[clearAllButton];
+            self.pdfViewController.leftBarButtonItems = @[clearTabsButton];
             self.navigationItem.leftItemsSupplementBackButton = YES;
         }
 
@@ -42,32 +43,6 @@ const char *clearAllActionSheetToken;
         }
     }
     return self;
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////
-#pragma mark - Private
-
-- (void)clearAll:(id)sender {
-    // ensure we correctly shwow/hide the sheet
-    PSPDFActionSheet *actionSheet = objc_getAssociatedObject(sender, clearAllActionSheetToken);
-    if (actionSheet) {
-        [actionSheet dismissWithClickedButtonIndex:-1 animated:YES];
-    }else {
-        actionSheet = [[PSPDFActionSheet alloc] initWithTitle:nil];
-        [actionSheet setDestructiveButtonWithTitle:PSPDFLocalize(@"Clear All Tabs") block:^{
-            [self removeDocuments:self.documents animated:YES];
-        }];
-        [actionSheet setCancelButtonWithTitle:PSPDFLocalize(@"Cancel") block:NULL];
-        actionSheet.destroyBlock = ^{
-            objc_removeAssociatedObjects(sender);
-        };
-        objc_setAssociatedObject(sender, clearAllActionSheetToken, actionSheet, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-        if (PSIsIpad()) {
-            [actionSheet showFromBarButtonItem:sender animated:YES];
-        }else {
-            [actionSheet showInView:self.view];
-        }
-    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
