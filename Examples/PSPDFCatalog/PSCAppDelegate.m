1f8d7bbee1c561fa7af77e1e48f58052079e92e5
Peter Steinberger <me@petersteinberger.com>
2012-10-03 03:17:46 +0200

    use hockey only for demo

--- Examples/PSPDFCatalog/PSCAppDelegate.m
+++ Examples/PSPDFCatalog/PSCAppDelegate.m
@@ -30,7 +30,6 @@
     PSPDFSetLocalizationDictionary(@{@"en" :
                                    @{@"Go to %@" : @"%@",
                                      @"%d of %d" : @"Page %d of %d",
-                                     @"Outline"  : @"Table of Contents"
                                    }});
 
     // change log level to be more verbose.
@@ -60,17 +59,17 @@
     // Perform non-important actions on the next runloop, for faster app-starting.
     dispatch_async(dispatch_get_main_queue(), ^{
 
+        // Localytics helps me to track PSPDFCatalog launches
+        // Remove this or replace with your own Localytics ID if you are using PSPDFCatalog as a template for your own app.
+#ifndef PSPDF_USE_SOURCE
+        [[LocalyticsSession sharedLocalyticsSession] startSession:@"3b7cb4552ea954d48d68f0e-3451f502-de39-11e1-4ab8-00ef75f32667"];
+
         // HockeyApp (http://hockeyapp.net) is a *great* service to manage crashes. It's well worth the money.
         // If you use it, download your own current version of the HockeySDK!
         // This version has been modified to work w/o a dSYM (but will not show line numbers)
         [[BITHockeyManager sharedHockeyManager] configureWithIdentifier:@"fa73e1f8f3806bcb3466c5ab16d70768" delegate:nil];
         [[BITHockeyManager sharedHockeyManager] crashManager].crashManagerStatus = BITCrashManagerStatusAutoSend;
         [[BITHockeyManager sharedHockeyManager] startManager];
-
-        // Localytics helps me to track PSPDFCatalog launches
-        // Remove this or replace with your own Localytics ID if you are using PSPDFCatalog as a template for your own app.
-#ifndef PSPDF_USE_SOURCE
-        [[LocalyticsSession sharedLocalyticsSession] startSession:@"3b7cb4552ea954d48d68f0e-3451f502-de39-11e1-4ab8-00ef75f32667"];
 #else
         // useful for debugging
         [PSPDFHangDetector startHangDetector];
