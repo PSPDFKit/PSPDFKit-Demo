438de57364d93084ee0075a1d29bd3724b302f03
Peter Steinberger <me@petersteinberger.com>
2012-10-04 13:36:03 +0200

    add class comments for settings controller

--- Examples/PSPDFCatalog/Kiosk/PSCSettingsController.h
+++ Examples/PSPDFCatalog/Kiosk/PSCSettingsController.h
@@ -7,6 +7,12 @@
 
 #define kGlobalVarChangeNotification @"kGlobalVarChangeNotification"
 
+/**
+ The settings controller is meant to show off the flexibility of PSPDFKit.
+
+ It's not intended to be used in shipping apps.
+ Make smart choices for the user and don't over-burdem them with settings.
+ */
 @interface PSCSettingsController : UITableViewController
 
 @property (nonatomic, ps_weak) UIViewController *owningViewController;
