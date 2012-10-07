449b9f19753be308b7f45d28ec37e3ff5af143d7
Peter Steinberger <me@petersteinberger.com>
2012-10-07 22:44:14 +0200

    expose auto playing property

--- Examples/PSPDFCatalog/Common/PSCPlayButtonItem.h
+++ Examples/PSPDFCatalog/Common/PSCPlayButtonItem.h
@@ -10,4 +10,6 @@
 // Adds a play/pause button that can enable a slideshow mode for pdf documents.
 @interface PSCPlayButtonItem : PSPDFBarButtonItem
 
+@property (nonatomic, assign, getter=isAutoplaying) BOOL autoplaying;
+
 @end
