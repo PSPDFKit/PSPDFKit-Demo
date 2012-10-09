2f9537dc2f5fc37b9f15742a68098934473c8e0d
Peter Steinberger <me@petersteinberger.com>
2012-10-09 18:06:24 +0200

    ensure we pop to the root view before showing a document
    Former-commit-id: 6f2cb846bec5200b725ec2b3743fa6eaaf7e4087

--- Examples/PSPDFCatalog/PSCAppDelegate.m
+++ Examples/PSPDFCatalog/PSCAppDelegate.m
@@ -89,6 +89,7 @@
     if ([launchPDFURL isFileURL] && [[NSFileManager defaultManager] fileExistsAtPath:[launchPDFURL path]]) {
         PSPDFDocument *document = [PSPDFDocument PDFDocumentWithURL:launchPDFURL];
         PSPDFViewController *pdfController = [[PSPDFViewController alloc] initWithDocument:document];
+        [self.catalog popToRootViewControllerAnimated:NO];
         [self.catalog pushViewController:pdfController animated:NO];
         return YES;
     }
