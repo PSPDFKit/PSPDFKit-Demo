8dae80034c767d3d26ca9ef55f3b73018e6f34ee
Peter Steinberger <me@petersteinberger.com>
2012-10-10 15:09:35 +0200

    add basic example for full-text search

---
+++ Examples/PSPDFCatalog/Common/PSCFullTextSearchOperation.h
@@ -0,0 +1,41 @@
+//
+//  PSCFullTextSearchOperation.h
+//  PSPDFCatalog
+//
+//  Copyright (c) 2012 PSPDFKit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class PSCFullTextSearchOperation;
+
+/// Full-Text Search delegate. Use completionBlock for the final update.
+@protocol PSCFullTextSearchOperationDelegate <NSObject>
+
+/// New search results have been added.
+- (void)fullTextSearchOperationDidUpdateResults:(PSCFullTextSearchOperation *)operation;
+
+@end
+
+/// Sample operation how to perform full-text search across multiple documents.
+/// Note that this WILL BE SLOW for many documents. I am working on a better solution.
+/// (There's a reason why this is PSC* space and not in PSPDF)
+/// iOS5 and later.
+@interface PSCFullTextSearchOperation : NSOperation
+
+/// Designated initializer
+- (id)initWithDocuments:(NSArray *)documents searchTerm:(NSString *)searchTerm;
+
+/// Documents to search
+@property (nonatomic, copy, readonly) NSArray *documents;
+
+/// Search term.
+@property (nonatomic, copy, readonly) NSString *searchTerm;
+
+@property (nonatomic, unsafe_unretained) id<PSCFullTextSearchOperationDelegate> delegate;
+
+/// Array of documents that match for a certain search term.
+/// Access after operation has finished.
+@property (nonatomic, copy, readonly) NSArray *results;
+
+@end
---
+++ Examples/PSPDFCatalog/Common/PSCFullTextSearchOperation.m
@@ -0,0 +1,99 @@
+//
+//  PSCFullTextSearchOperation.m
+//  PSPDFCatalog
+//
+//  Copyright (c) 2012 PSPDFKit. All rights reserved.
+//
+
+#import "PSCFullTextSearchOperation.h"
+
+@interface PSCFullTextSearchOperation() <PSPDFTextSearchDelegate> {
+    dispatch_semaphore_t _searchBlockSemaphore;
+    NSMutableOrderedSet *_internalResults;
+}
+@property (nonatomic, copy) NSArray *documents;
+@property (nonatomic, copy) NSString *searchTerm;
+@property (nonatomic, copy) NSArray *results;
+@end
+
+@implementation PSCFullTextSearchOperation
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - NSObject
+
+- (id)initWithDocuments:(NSArray *)documents searchTerm:(NSString *)searchTerm {
+    if ((self = [super init])) {
+        _documents = documents;
+        _searchTerm = [searchTerm copy];
+        _internalResults = [NSMutableOrderedSet new];
+        _searchBlockSemaphore = dispatch_semaphore_create(0);
+    }
+    return self;
+}
+
+- (void)dealloc {
+    PSPDFDispatchRelease(_searchBlockSemaphore);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - NSObject
+
+// thread entry point
+- (void)main {
+    for (PSPDFDocument *document in self.documents) {
+        @autoreleasepool {
+            if (self.isCancelled) break;
+
+            // create extra textSearch class so we don't interfear with delegates
+            PSPDFTextSearch *textSearch = [[PSPDFTextSearch alloc] initWithDocument:document];
+            textSearch.delegate = self;
+
+            // because of the semaphore we need to call this from another thread.
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                [textSearch searchForString:self.searchTerm];
+            });
+
+            dispatch_semaphore_wait(_searchBlockSemaphore, DISPATCH_TIME_FOREVER); // wait until completion.
+            textSearch.delegate = nil; // just do be safe
+            [document clearCache]; // else we will eventually run out of memory
+        }
+    }
+
+    self.results = [[_internalResults array] copy];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSPDFTextSearchDelegate
+
+- (void)didUpdateSearch:(PSPDFTextSearch *)textSearch forTerm:(NSString *)searchTerm newSearchResults:(NSArray *)searchResults forPage:(NSUInteger)page {
+    if ([searchResults count]) {
+        NSLog(@"found term in %@", textSearch.document);
+        [_internalResults addObject:textSearch.document];
+
+        // update results + notify delegate
+        self.results = [[_internalResults array] copy];
+        [self.delegate fullTextSearchOperationDidUpdateResults:self];
+
+        // stop serach in current document - we already found a result
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            [textSearch cancelAllOperationsAndWait];
+        });
+        dispatch_semaphore_signal(_searchBlockSemaphore);
+    }else if(self.isCancelled) {
+        dispatch_semaphore_signal(_searchBlockSemaphore);
+    }
+}
+
+- (void)willStartSearch:(PSPDFTextSearch *)textSearch forTerm:(NSString *)searchTerm isFullSearch:(BOOL)isFullSearch {
+    NSLog(@"Full-Text search: looking in %@.", textSearch.document);
+}
+
+- (void)didFinishSearch:(PSPDFTextSearch *)textSearch forTerm:(NSString *)searchTerm searchResults:(NSArray *)searchResults isFullSearch:(BOOL)isFullSearch {
+    dispatch_semaphore_signal(_searchBlockSemaphore);
+}
+
+- (void)didCancelSearch:(PSPDFTextSearch *)textSearch forTerm:(NSString *)searchTerm isFullSearch:(BOOL)isFullSearch {
+    dispatch_semaphore_signal(_searchBlockSemaphore);
+}
+
+@end
