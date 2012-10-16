5be60119bc30d3805cd7d2f105ecc145ba433f36
Peter Steinberger <me@petersteinberger.com>
2012-10-16 17:04:51 +0200

    add go to page example

---
+++ Examples/PSPDFCatalog/Common/PSCGoToPageButtonItem.h
@@ -0,0 +1,14 @@
+//
+//  PSCGoToPageButtonItem.h
+//  PSPDFCatalog
+//
+//  Copyright (c) 2012 PSPDFKit. All rights reserved.
+//
+
+/// Allows to jump to a specific page or page label.
+@interface PSCGoToPageButtonItem : PSPDFBarButtonItem
+
+/// Match partial label string names. Defaults to YES.
+@property (nonatomic, assign) BOOL enablePartialLabelMatching;
+
+@end
