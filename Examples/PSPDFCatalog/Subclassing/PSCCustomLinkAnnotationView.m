b5685f228ff24a45c6ac5e124a92a532db1ba36b
Peter Steinberger <me@petersteinberger.com>
2012-10-19 11:11:06 +0200

    add example how to customize the link color

---
+++ Examples/PSPDFCatalog/Subclassing/PSCCustomLinkAnnotationView.m
@@ -0,0 +1,20 @@
+//
+//  PSCCustomLinkAnnotationView.m
+//  PSPDFCatalog
+//
+//  Copyright (c) 2012 PSPDFKit. All rights reserved.
+//
+
+#import "PSCCustomLinkAnnotationView.h"
+
+@implementation PSCCustomLinkAnnotationView
+
+- (id)initWithFrame:(CGRect)frame {
+    if ((self = [super initWithFrame:frame])) {
+        self.borderColor = [[UIColor redColor] colorWithAlphaComponent:0.5];
+        self.highlightBackgroundColor = [[UIColor redColor] colorWithAlphaComponent:0.5f];
+    }
+    return self;
+}
+
+@end
