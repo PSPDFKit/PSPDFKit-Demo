4c72f03a82c52b57295fec9c378ddd441b40b79e
Peter Steinberger <me@petersteinberger.com>
2012-11-01 01:02:18 +0100

    add new bottom toolbar example

---
+++ Examples/PSPDFCatalog/Subclassing/PSCBottomToolbarViewController.h
@@ -0,0 +1,10 @@
+//
+//  PSCBottomToolbarViewController.h
+//  PSPDFCatalog
+//
+//  Copyright (c) 2012 PSPDFKit. All rights reserved.
+//
+
+@interface PSCBottomToolbarViewController : PSPDFViewController
+
+@end
