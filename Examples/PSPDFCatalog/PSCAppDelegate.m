6442b2fac8777350423351ad8c6649f94a743aab
Peter Steinberger <me@petersteinberger.com>
2012-11-24 11:53:53 +0100

    upgrade to HockeySDK 3.0
    Former-commit-id: 5e145436f98c09fcdd912d46b6786f40c1bab356

--- Examples/PSPDFCatalog/PSCAppDelegate.m
+++ Examples/PSPDFCatalog/PSCAppDelegate.m
@@ -7,13 +7,15 @@
 
 #import "PSCAppDelegate.h"
 #import "PSCatalogViewController.h"
-#import "BITHockeyManager.h"
-#import "BITCrashManager.h"
+#import "HockeySDK.h"
 #import "LocalyticsSession.h"
 
 #if !__has_feature(objc_arc)
 #error "Compile this file with ARC"
 #endif
+
+@interface PSCAppDelegate () <BITHockeyManagerDelegate, BITUpdateManagerDelegate, BITCrashManagerDelegate>
+@end
 
 @implementation PSCAppDelegate
 
@@ -62,17 +64,18 @@
 
     // Perform non-important actions on the next runloop, for faster app-starting.
     dispatch_async(dispatch_get_main_queue(), ^{
+
+        // HockeyApp (http://hockeyapp.net) is a *great* service to manage crashes. It's well worth the money.
+#ifndef CONFIGURATION_Debug
+        [[BITHockeyManager sharedHockeyManager] configureWithIdentifier:@"fa73e1f8f3806bcb3466c5ab16d70768" delegate:nil];
+        [[BITHockeyManager sharedHockeyManager] crashManager].crashManagerStatus = BITCrashManagerStatusAutoSend;
+        [[BITHockeyManager sharedHockeyManager] startManager];
+#endif
+
         // Localytics helps me to track PSPDFCatalog launches
         // Remove this or replace with your own Localytics ID if you are using PSPDFCatalog as a template for your own app.
 #ifndef PSPDF_USE_SOURCE
         [[LocalyticsSession sharedLocalyticsSession] startSession:@"3b7cb4552ea954d48d68f0e-3451f502-de39-11e1-4ab8-00ef75f32667"];
-
-        // HockeyApp (http://hockeyapp.net) is a *great* service to manage crashes. It's well worth the money.
-        // If you use it, download your own current version of the HockeySDK!
-        // This version has been modified to work w/o a dSYM (but will not show line numbers)
-        [[BITHockeyManager sharedHockeyManager] configureWithIdentifier:@"fa73e1f8f3806bcb3466c5ab16d70768" delegate:nil];
-        [[BITHockeyManager sharedHockeyManager] crashManager].crashManagerStatus = BITCrashManagerStatusAutoSend;
-        [[BITHockeyManager sharedHockeyManager] startManager];
 #else
         // useful for debugging
         [PSPDFHangDetector startHangDetector];
