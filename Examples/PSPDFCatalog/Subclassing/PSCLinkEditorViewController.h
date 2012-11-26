d70cb4b2c5edd9ea85774b882cca74897d8f9c36
Peter Steinberger <me@petersteinberger.com>
2012-11-26 14:32:11 +0100

    add link editor example and a few new tests

---
+++ Examples/PSPDFCatalog/Subclassing/PSCLinkEditorViewController.h
@@ -0,0 +1,11 @@
+//
+//  PSCLinkEditorViewController.h
+//  PSPDFCatalog
+//
+//  Copyright (c) 2012 Peter Steinberger. All rights reserved.
+//
+
+// Allows Link Editing within PDF documents.
+@interface PSCLinkEditorViewController : PSPDFViewController <PSPDFViewControllerDelegate>
+
+@end
