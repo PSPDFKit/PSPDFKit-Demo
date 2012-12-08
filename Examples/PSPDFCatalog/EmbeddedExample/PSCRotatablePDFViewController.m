a9a6065ab73173afd10b026fa73c0ec92e384c28
Peter Steinberger <me@petersteinberger.com>
2012-12-08 23:47:55 +0100

    add rotate gesture to the rotation example

--- Examples/PSPDFCatalog/EmbeddedExample/PSCRotatablePDFViewController.m
+++ Examples/PSPDFCatalog/EmbeddedExample/PSCRotatablePDFViewController.m
@@ -7,6 +7,8 @@
 
 #import "PSCRotatablePDFViewController.h"
 
+@interface PSPDFRotatableScrollView : PSPDFScrollView @end
+
 @implementation PSCRotatablePDFViewController
 
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -18,6 +20,10 @@
     // prevents page flashing when there's cached content available at the cost of slight main thread blocking.
     self.renderingMode = PSPDFPageRenderingModeFullPageBlocking;
 
+    // Optional: Allow rotation via a gesture.
+    self.overrideClassNames = @{(id)[PSPDFScrollView class] : [PSPDFRotatableScrollView class]};
+
+    // Add manual rotation button.
     UIBarButtonItem *rotate = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemReply target:self action:@selector(rotateAction:)];
     self.leftBarButtonItems = @[rotate];
     self.navigationItem.leftItemsSupplementBackButton = YES;
@@ -31,7 +37,7 @@
     [[PSPDFCache sharedCache] removeCacheForDocument:self.document deleteDocument:NO error:NULL];
 
     // rotate 90 degree counter-clock-wise (and make sure we don't set something >= 360)
-    BOOL rotateAll = YES;
+    BOOL rotateAll = NO;
     if (rotateAll) {
         for (NSUInteger pageIndex=0; pageIndex < [self.document pageCount]; pageIndex++) {
             PSPDFPageInfo *pageInfo = [self.document pageInfoForPage:pageIndex];
@@ -41,11 +47,62 @@
         PSPDFPageInfo *pageInfo = [self.document pageInfoForPage:self.page];
         pageInfo.pageRotation = PSPDFNormalizeRotation(pageInfo.pageRotation - 90);
     }
+    
     // request an immediate rendering of the current page, will block the main thread but prevent flashing.
     [[PSPDFCache sharedCache] renderAndCacheImageForDocument:self.document page:self.page size:PSPDFSizeNative error:NULL];
 
     // reload the controller
     [self reloadData];
+}
+
+@end
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSPDFRotatableScrollView
+
+@implementation PSPDFRotatableScrollView
+
+- (id)initWithFrame:(CGRect)frame {
+    if ((self = [super initWithFrame:frame])) {
+        // Add page rotation gesture.
+        UIRotationGestureRecognizer *rotationGesture = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(handleRotation:)];
+        rotationGesture.delegate = self;
+        [self addGestureRecognizer:rotationGesture];
+    }
+    return self;
+}
+
+- (void)handleRotation:(UIRotationGestureRecognizer *)gestureRecognizer {
+    if (gestureRecognizer.state == UIGestureRecognizerStateEnded) {
+        // reset view
+        gestureRecognizer.view.transform = CGAffineTransformIdentity;
+
+        // Invalidate the cache.
+        // TODO: this could be made more fine grained. (just update the current page)
+        [[PSPDFCache sharedCache] removeCacheForDocument:self.document deleteDocument:NO error:NULL];
+
+        // Get rotation and snap to the closest position.
+        PSPDFPageInfo *pageInfo = [self.document pageInfoForPage:self.page];
+        CGFloat degrees = PSPDFRadiansToDegrees(atan2f(self.transform.b, self.transform.a));
+        pageInfo.pageRotation = PSPDFNormalizeRotation(pageInfo.pageRotation+degrees);
+        PSCLog(@"Snap rotation to: %d", pageInfo.pageRotation);
+
+        // request an immediate rendering, will block the main thread but prevent flashing.
+        [[PSPDFCache sharedCache] renderAndCacheImageForDocument:self.document page:self.page size:PSPDFSizeNative error:NULL];
+
+        // Reload the controller. (this is efficient and will re-use views)
+        [self.pdfController reloadData];
+    }else {
+        // Transform the current view.
+        gestureRecognizer.view.transform = CGAffineTransformRotate(gestureRecognizer.view.transform, gestureRecognizer.rotation);
+        gestureRecognizer.rotation = 0;
+    }
+}
+
+// Allow zooming and rotating at the same time.
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    if ([gestureRecognizer isKindOfClass:[UIRotationGestureRecognizer class]]) return YES;
+    return [super gestureRecognizer:gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:otherGestureRecognizer];
 }
 
 @end
