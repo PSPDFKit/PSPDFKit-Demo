6849cbb37fc1a01ad04b6d140161d49b48be88bc
Peter Steinberger <me@petersteinberger.com>
2012-12-10 14:36:26 +0100

    add new tint test files
    Former-commit-id: d4489e5dc17c3a2009b416ede5af4c52e63b13f8

--- Examples/PSPDFCatalog/Common/PSCTintColorSelectionBarButtonItem.h
+++ Examples/PSPDFCatalog/Common/PSCTintColorSelectionBarButtonItem.h
@@ -1,12 +1,9 @@
 //
 //  PSCTintColorSelectionBarButtonItem.h
-//  PSPDFCatalog-static
+//  PSPDFCatalog
 //
-//  Created by Peter Steinberger on 12/9/12.
 //  Copyright (c) 2012 Peter Steinberger. All rights reserved.
 //
-
-#import "PSPDFBarButtonItem.h"
 
 @interface PSCTintColorSelectionBarButtonItem : PSPDFBarButtonItem
 
--- Examples/PSPDFCatalog/Subclassing/PSCTintablePDFViewController.m
+++ Examples/PSPDFCatalog/Subclassing/PSCTintablePDFViewController.m
@@ -1,13 +1,40 @@
 //
 //  PSCTintablePDFViewController.m
-//  PSPDFCatalog-static
+//  PSPDFCatalog
 //
-//  Created by Peter Steinberger on 12/10/12.
 //  Copyright (c) 2012 Peter Steinberger. All rights reserved.
 //
 
 #import "PSCTintablePDFViewController.h"
+#import "PSCTintColorSelectionBarButtonItem.h"
 
 @implementation PSCTintablePDFViewController
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSPDFViewController
+
+- (void)commonInitWithDocument:(PSPDFDocument *)document {
+    [super commonInitWithDocument:document];
+    self.statusBarStyleSetting = PSPDFStatusBarDefault;
+    self.tintColor = [UIColor colorWithRed:0.092 green:0.608 blue:0.000 alpha:1.000];
+    PSCTintColorSelectionBarButtonItem *tintColorButtonItem = [[PSCTintColorSelectionBarButtonItem alloc] initWithPDFViewController:self];
+    UIBarButtonItem *alertButtonTest = [[UIBarButtonItem alloc] initWithTitle:@"Alert" style:UIBarButtonItemStyleBordered target:self action:@selector(testAlert)];
+
+    self.useBorderedToolbarStyle = YES;
+    self.leftBarButtonItems = @[self.closeButtonItem, tintColorButtonItem, alertButtonTest];
+    self.rightBarButtonItems = @[self.searchButtonItem, self.viewModeButtonItem];
+    self.linkAction = PSPDFLinkActionAlertView; // touch a link to test tint.
+
+    // both default, but set explicitely here for demonstration purposes
+    self.shouldTintPopovers = YES;
+    self.shouldTintAlertView = YES;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - Private
+
+- (void)testAlert {
+    [[[PSPDFAlertView alloc] initWithTitle:@"AlertView Test" message:@"This alert is a test. Notice the custom tintColor with PSPDFAlertView." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] showWithTintColor:self.tintColor];
+}
 
 @end
