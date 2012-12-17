fb77bad9268aa8d4e2e1d2555da751f8f2798202
Peter Steinberger <me@petersteinberger.com>
2012-12-17 23:10:02 +0100

    PSPDFKit 2.6.3 - Greatly improves FreeText annotations. Edit color, font style and size. Also includes sharper annotation selection and some general bug fixes.

---
+++ PSPDFKit.embeddedframework/PSPDFKit.framework/Versions/A/Headers/PSPDFBrightnessSlider.h
@@ -0,0 +1,33 @@
+//
+//  PSPDFBrightnessSlider.h
+//  PSPDFKit
+//
+//  Copyright (c) 2012 Peter Steinberger. All rights reserved.
+//  Based on work of Ryan Sullivan. BSD licensed.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef enum {
+    PSPDFThumbImageStyleDefault = 0,
+    PSPDFThumbImageStyleHourGlass,
+    PSPDFThumbImageStyleArrowLoop,
+}PSPDFThumbImageStyle;
+
+typedef enum {
+    PSPDFSliderBackgroundStyleDefault = 0,
+    PSPDFSliderBackgroundStyleGrayscale,
+    PSPDFSliderBackgroundStyleColorfull,
+} PSPDFSliderBackgroundStyle;
+
+@class PSPDFColorPickerView;
+
+@interface PSPDFBrightnessSlider : UISlider
+
+@property (nonatomic, assign) PSPDFThumbImageStyle thumbImageStyle;
+@property (nonatomic, assign) PSPDFSliderBackgroundStyle backgroundStyle;
+@property (nonatomic, weak) IBOutlet PSPDFColorPickerView* colorPicker;
+
+- (void)updateBackground;
+
+@end
