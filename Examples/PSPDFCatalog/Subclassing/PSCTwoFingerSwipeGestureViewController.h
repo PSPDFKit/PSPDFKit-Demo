0b5392e20371dbb53eb7af0d92c895f3c73b2f64
Peter Steinberger <me@petersteinberger.com>
2012-12-18 15:56:55 +0100

    add custom swipe gestures example

---
+++ Examples/PSPDFCatalog/Subclassing/PSCTwoFingerSwipeGestureViewController.h
@@ -0,0 +1,11 @@
+//
+//  PSCTwoFingerSwipeGestureViewController.h
+//  PSPDFCatalog
+//
+//  Created by Peter Steinberger on 12/18/12.
+//  Copyright (c) 2012 Peter Steinberger. All rights reserved.
+//
+
+@interface PSCTwoFingerSwipeGestureViewController : PSPDFViewController
+
+@end
