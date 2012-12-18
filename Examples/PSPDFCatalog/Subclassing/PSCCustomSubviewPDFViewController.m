4aae3e57e88e63942f9f26486088629eae4b9d01
Peter Steinberger <me@petersteinberger.com>
2012-12-18 14:36:23 +0100

    add new custom subview example, render all annotations as overlay

---
+++ Examples/PSPDFCatalog/Subclassing/PSCCustomSubviewPDFViewController.m
@@ -0,0 +1,64 @@
+//
+//  PSCCustomSubviewPDFViewController.m
+//  PSPDFCatalog
+//
+//  Copyright (c) 2012 Peter Steinberger. All rights reserved.
+//
+
+#import "PSCCustomSubviewPDFViewController.h"
+
+@interface PSCOverlayFileAnnotationProvider : PSPDFFileAnnotationProvider @end
+
+@implementation PSCCustomSubviewPDFViewController
+
+- (void)commonInitWithDocument:(PSPDFDocument *)document {
+    [super commonInitWithDocument:document];
+    self.delegate = self;
+    self.rightBarButtonItems = @[self.annotationButtonItem, self.viewModeButtonItem];
+
+    // register our custom annotation provider as subclass.
+    document.overrideClassNames = @{(id)[PSPDFFileAnnotationProvider class] : [PSCOverlayFileAnnotationProvider class]};
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSPDFViewControllerDelegate
+
+- (void)pdfViewController:(PSPDFViewController *)pdfController didLoadPageView:(PSPDFPageView *)pageView {
+    // adds a custom view above every page, to demonstrate that the annotations will render ABOVE that view.
+    UIView *customView = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 300, 300)];
+    customView.backgroundColor = [UIColor colorWithRed:1.000 green:0.846 blue:0.088 alpha:0.900];
+    customView.layer.cornerRadius = 10.f;
+    customView.layer.borderColor = [UIColor colorWithRed:1.000 green:0.846 blue:0.088 alpha:1.000].CGColor;
+    customView.layer.borderWidth = 2.f;
+    [pageView insertSubview:customView belowSubview:pageView.annotationContainerView];
+}
+
+@end
+
+@implementation PSCOverlayFileAnnotationProvider
+
+- (NSArray *)annotationsForPage:(NSUInteger)page pageRef:(CGPDFPageRef)pageRef {
+    NSArray *annotations = [super annotationsForPage:page pageRef:pageRef];
+
+    // make all annotations overlay annotations (they will be rendered in their own views instead of within the page image)
+    for (PSPDFAnnotation *annotation in annotations) {
+        // Making highlights as overlay really really doesn't look good. (since they are multiplicated into the page content, this is not possible with regular UIView composition, so you'd completely overlap the text, unless you make them semi-transparent)
+        if (![annotation isKindOfClass:[PSPDFHighlightAnnotation class]]) {
+            annotation.overlay = YES;
+        }
+    }
+    return annotations;
+}
+
+// Set annotations to render as overlay right after they are inserted.
+- (BOOL)addAnnotations:(NSArray *)annotations forPage:(NSUInteger)page {
+    for (PSPDFAnnotation *annotation in annotations) {
+        if (![annotation isKindOfClass:[PSPDFHighlightAnnotation class]]) {
+            annotation.overlay = YES;
+        }
+    }
+    return [super addAnnotations:annotations forPage:page];
+}
+
+@end
+
