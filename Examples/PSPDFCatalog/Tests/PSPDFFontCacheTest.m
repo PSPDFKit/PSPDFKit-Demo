9cdde6b9ea779d3d1987668233f32db415ac7c30
Peter Steinberger <me@petersteinberger.com>
2012-12-24 17:50:22 +0100

    add new test case

---
+++ Examples/PSPDFCatalog/Tests/PSPDFFontCacheTest.m
@@ -0,0 +1,44 @@
+//
+//  PSPDFFontCacheTest.h
+//  PSPDFCatalog
+//
+//  Copyright (c) 2012 Peter Steinberger. All rights reserved.
+//
+
+#import "PSPDFFontCacheTest.h"
+
+@interface PSPDFFontCacheTest () <PSPDFTextSearchDelegate>
+@property (nonatomic, strong) NSString *path;
+@property (nonatomic, strong) PSPDFDocument *doc;
+@end
+
+@implementation PSPDFFontCacheTest
+
+static PSPDFFontCacheTest *instance = nil;
+
++ (void)runWithDocumentAtPath:(NSString *)path {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [PSPDFFontCacheTest new];
+    });
+
+    [instance runWithDocumentAtPath:path];
+}
+
+- (void)runWithDocumentAtPath:(NSString *)path {
+  self.path = path;
+  self.doc = [[PSPDFDocument alloc] initWithURL:[NSURL fileURLWithPath:path]];
+  PSPDFTextSearch *search = [[PSPDFTextSearch alloc] initWithDocument:self.doc];
+  search.delegate = self;
+  [search searchForString:@"test"];
+}
+
+- (void)didFinishSearch:(PSPDFTextSearch *)textSearch forTerm:(NSString *)searchTerm
+          searchResults:(NSArray *)searchResults isFullSearch:(BOOL)isFullSearch {
+  NSLog(@"%@\n\n\n\n\n\n\n\n\n\n\n\n-----------------------------------------------------------------------------------------------------------------------", [self.doc textParserForPage:9].text);
+  
+  self.doc = nil;
+  [self performSelector:@selector(runWithDocumentAtPath:) withObject:self.path afterDelay:2];
+}
+
+@end
