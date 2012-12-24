5cfba96db2383e51d24f860dc8cff67a54605c70
Peter Steinberger <me@petersteinberger.com>
2012-12-24 17:50:22 +0100

    add new test case

---
+++ Examples/PSPDFCatalog/Tests/PSPDFFontCacheTest.h
@@ -0,0 +1,14 @@
+//
+//  PSPDFFontCacheTest.h
+//  PSPDFCatalog
+//
+//  Copyright (c) 2012 Peter Steinberger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PSPDFFontCacheTest : NSObject
+
++ (void)runWithDocumentAtPath:(NSString *)path;
+
+@end
