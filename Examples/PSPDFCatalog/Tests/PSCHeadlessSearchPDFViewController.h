ee89b5895504c6944e74cfb857f7475c77cfb95b
Peter Steinberger <me@petersteinberger.com>
2013-01-06 23:29:14 +0100

    remove invalid import

--- Examples/PSPDFCatalog/Tests/PSCHeadlessSearchPDFViewController.h
+++ Examples/PSPDFCatalog/Tests/PSCHeadlessSearchPDFViewController.h
@@ -5,8 +5,6 @@
 //  Copyright (c) 2013 Peter Steinberger. All rights reserved.
 //
 
-#import "PSPDFViewController.h"
-
 /// Example how to have a "persistent" search mode.
 @interface PSCHeadlessSearchPDFViewController : PSPDFViewController <PSPDFViewControllerDelegate>
 
