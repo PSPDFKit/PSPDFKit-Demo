f9c7c1894617a76379ef1929c2f8c56f583a8e88
Peter Steinberger <me@petersteinberger.com>
2013-01-12 17:06:48 +0100

    remove unneeded import

--- Examples/PSPDFCatalog/Annotations/PSCSaveAsPDFViewController.h
+++ Examples/PSPDFCatalog/Annotations/PSCSaveAsPDFViewController.h
@@ -5,8 +5,6 @@
 //  Copyright (c) 2013 Peter Steinberger. All rights reserved.
 //
 
-#import "PSPDFViewController.h"
-
 /// This class will ask the user as soon as the first annotation has been added/modified
 /// where the annotation should be saved, and optionally copies the file to a new location.
 @interface PSCSaveAsPDFViewController : PSPDFViewController
