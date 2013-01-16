4d65d4a840482e0ae90c51cfb9694d38978d7ada
Peter Steinberger <me@petersteinberger.com>
2013-01-16 19:30:17 +0100

    fixes analyzer warnings

--- Examples/PSPDFCatalog/Vendor/RNCryptor/RNCryptor.m
+++ Examples/PSPDFCatalog/Vendor/RNCryptor/RNCryptor.m
@@ -100,7 +100,7 @@ const uint8_t kRNCryptorFileVersion = 1;
 {
   NSMutableData *derivedKey = [NSMutableData dataWithLength:keySettings.keySize];
 
-  int result = CCKeyDerivationPBKDF(keySettings.PBKDFAlgorithm,         // algorithm
+  __unused int result = CCKeyDerivationPBKDF(keySettings.PBKDFAlgorithm,         // algorithm
                                     password.UTF8String,                // password
                                     password.length,                    // passwordLength
                                     salt.bytes,                         // salt
@@ -121,7 +121,7 @@ const uint8_t kRNCryptorFileVersion = 1;
 {
   NSMutableData *data = [NSMutableData dataWithLength:length];
 
-  int result = SecRandomCopyBytes(NULL, length, data.mutableBytes);
+  __unused int result = SecRandomCopyBytes(NULL, length, data.mutableBytes);
   NSAssert(result == 0, @"Unable to generate random bytes: %d", errno);
 
   return data;
