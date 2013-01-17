d79e773280e44f5421cda9e171cd59be186a64b1
Peter Steinberger <me@petersteinberger.com>
2013-01-17 15:05:37 +0100

    PSPDFKit 2.7.1 - Note annotation flattening, continuous scrolling improvements, better text selection and bug fixes.

--- PSPDFKit.embeddedframework/PSPDFKit.framework/Versions/A/Headers/PSPDFExtendedPopoverController.h
+++ PSPDFKit.embeddedframework/PSPDFKit.framework/Versions/A/Headers/PSPDFExtendedPopoverController.h
@@ -16,6 +16,9 @@
 /// e.g. Used to improve animation of the keyboard dismissing.
 @interface PSPDFExtendedPopoverController : UIPopoverController
 
+/// Action that is invoked before the popover hides. (programmatially or via user action)
+@property (nonatomic, copy) dispatch_block_t popoverWillDismissAction;
+
 /// Call to manually invoke delegate sending.
 /// Due to the nature of the callback we currently don't actually know the animated state.
 - (void)notifyContentControllerAboutDismissalAnimated:(BOOL)animated;
