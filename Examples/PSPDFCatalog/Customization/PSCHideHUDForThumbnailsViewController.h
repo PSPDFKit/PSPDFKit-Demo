bee8c49565ae8f88d4c108692677b33a49ba7cd9
Peter Steinberger <me@petersteinberger.com>
2013-02-13 19:17:04 +0100

    Fixes the build without source
    Former-commit-id: ad6514f236659089579c35045ab080be279c1e46

--- Examples/PSPDFCatalog/Customization/PSCHideHUDForThumbnailsViewController.h
+++ Examples/PSPDFCatalog/Customization/PSCHideHUDForThumbnailsViewController.h
@@ -5,8 +5,6 @@
 //  Copyright (c) 2013 Peter Steinberger. All rights reserved.
 //
 
-#import "PSPDFViewController.h"
-
 @interface PSCHideHUDForThumbnailsViewController : PSPDFViewController <PSPDFViewControllerDelegate>
 
 @end
