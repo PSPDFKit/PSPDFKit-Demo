7d0c5c4acedbc3956c35bc21e0dcf2fee4b28d21
Peter Steinberger <me@petersteinberger.com>
2013-02-21 22:11:24 +0100

    Add new example

---
+++ Examples/PSPDFCatalog/Subclassing/PSCCustomDefaultZoomScaleViewController.m
@@ -0,0 +1,38 @@
+//
+//  PSCCustomDefaultZoomScaleViewController.m
+//  PSPDFCatalog-static
+//
+//  Created by Peter Steinberger on 2/21/13.
+//  Copyright (c) 2013 Peter Steinberger. All rights reserved.
+//
+
+#import "PSCCustomDefaultZoomScaleViewController.h"
+
+@interface PSCCustomDefaultZoomScaleViewController ()
+
+@end
+
+@implementation PSCCustomDefaultZoomScaleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
