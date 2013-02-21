ec1636443d8e02172ff62820a0b6af251332254d
Peter Steinberger <me@petersteinberger.com>
2013-02-21 22:11:24 +0100

    Add new example
    Former-commit-id: 7d0c5c4acedbc3956c35bc21e0dcf2fee4b28d21

---
+++ Examples/PSPDFCatalog/Subclassing/PSCCustomDefaultZoomScaleViewController.h
@@ -0,0 +1,13 @@
+//
+//  PSCCustomDefaultZoomScaleViewController.h
+//  PSPDFCatalog-static
+//
+//  Created by Peter Steinberger on 2/21/13.
+//  Copyright (c) 2013 Peter Steinberger. All rights reserved.
+//
+
+#import "PSPDFViewController.h"
+
+@interface PSCCustomDefaultZoomScaleViewController : PSPDFViewController
+
+@end
