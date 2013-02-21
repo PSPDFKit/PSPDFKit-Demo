4c1c14de26b76041812db915130389c4d04ba27d
Peter Steinberger <me@petersteinberger.com>
2013-02-21 22:11:59 +0100

    Update example

--- Examples/PSPDFCatalog/Subclassing/PSCCustomDefaultZoomScaleViewController.h
+++ Examples/PSPDFCatalog/Subclassing/PSCCustomDefaultZoomScaleViewController.h
@@ -1,13 +1,10 @@
 //
 //  PSCCustomDefaultZoomScaleViewController.h
-//  PSPDFCatalog-static
+//  PSPDFCatalog
 //
-//  Created by Peter Steinberger on 2/21/13.
 //  Copyright (c) 2013 Peter Steinberger. All rights reserved.
 //
 
-#import "PSPDFViewController.h"
-
-@interface PSCCustomDefaultZoomScaleViewController : PSPDFViewController
+@interface PSCCustomDefaultZoomScaleViewController : PSPDFViewController <PSPDFViewControllerDelegate>
 
 @end
