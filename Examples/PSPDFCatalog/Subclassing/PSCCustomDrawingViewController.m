0fb980d7d0be4ffcac5dec110c39a3874cbefecf
Peter Steinberger <me@petersteinberger.com>
2013-03-05 17:38:12 +0000

    Update example for new API

--- Examples/PSPDFCatalog/Subclassing/PSCCustomDrawingViewController.m
+++ Examples/PSPDFCatalog/Subclassing/PSCCustomDrawingViewController.m
@@ -7,34 +7,44 @@
 
 #import "PSCCustomDrawingViewController.h"
 
+@interface PSCDocumentDrawer : NSObject <PSPDFDocumentDelegate> @end
+
+@interface PSCCustomDrawingViewController ()
+@property (nonatomic, strong) PSCDocumentDrawer *documentDrawer;
+@end
+
 @implementation PSCCustomDrawingViewController
 
 - (id)initWithDocument:(PSPDFDocument *)document {
     if ((self = [super initWithDocument:document])) {
         self.delegate = self;
+        self.documentDrawer = [[PSCDocumentDrawer alloc] init];
+        document.delegate = self.documentDrawer;
     }
     return self;
 }
 
+@end
+
+@implementation PSCDocumentDrawer
+
 ///////////////////////////////////////////////////////////////////////////////////////////
-#pragma mark - PSPDFViewControllerDelegate
+#pragma mark - PSPDFDocumentDelegate
 
-- (void)pdfViewController:(PSPDFViewController *)pdfController didRenderPage:(NSUInteger)page inContext:(CGContextRef)context withSize:(CGSize)fullSize clippedToRect:(CGRect)clipRect withAnnotations:(NSArray *)annotations options:(NSDictionary *)options {
+- (void)pdfDocument:(PSPDFDocument *)document didRenderPage:(NSUInteger)page inContext:(CGContextRef)context withSize:(CGSize)fullSize clippedToRect:(CGRect)clipRect withAnnotations:(NSArray *)annotations options:(NSDictionary *)options {
 
-    // don't render text for cached pages
-    if (options[kPSPDFCachedRenderRequest]) {
-        return;
-    }
+    // Don't render text for cached pages.
+    if (options[kPSPDFCachedRenderRequest]) return;
 
-    // setup graphics context for current PDF page
-    PSPDFPageInfo *pageInfo = [pdfController.document pageInfoForPage:page];
+    // Setup graphics context for current PDF page.
+    PSPDFPageInfo *pageInfo = [document pageInfoForPage:page];
     [PSPDFPageRenderer setupGraphicsContext:context inRectangle:clipRect pageInfo:pageInfo];
 
-    // flip drawing
+    // Flip drawing.
     CGContextTranslateCTM(context, 0, pageInfo.rotatedPageRect.size.height);
     CGContextScaleCTM(context, 1, -1);
 
-    // set up drawing
+    // Set up drawing.
     NSString *fontName = @"Helvetica-Bold";
     CGFloat fontSize = 24.f;
     NSString *overlayText = @"Example overlay";
@@ -43,11 +53,11 @@
     CGAffineTransform xform = CGAffineTransformMake(1.0, 0.0, 0.0, -1.0, 0.0, 0.0);
     CGContextSetTextMatrix(context, xform);
 
-    // calculate the font box to center it
+    // Calculate the font box to center it.
     CGSize boundingBox = [overlayText sizeWithFont:[UIFont fontWithName:fontName size:fontSize]];
     CGContextSetTextPosition(context, roundf((pageInfo.rotatedPageRect.size.width-boundingBox.width)/2), roundf((pageInfo.rotatedPageRect.size.height-boundingBox.height)/2));
 
-    // finally draw text
+    // Finally draw text.
     CGContextShowText(context, [overlayText cStringUsingEncoding:NSUTF8StringEncoding], [overlayText length]);
 }
 
