b8b6b899b90ee3156fb3043f77724acf61756e78
Peter Steinberger <me@petersteinberger.com>
2013-03-06 10:49:25 +0000

    Remove old API references
    Former-commit-id: 01fa1816aa87103c10450339e13db58be74c2446

--- Examples/PSPDFCatalog/Customization/PSCCustomToolbarController.m
+++ Examples/PSPDFCatalog/Customization/PSCCustomToolbarController.m
@@ -127,9 +127,4 @@
     }
 }
 
-- (BOOL)pdfViewController:(PSPDFViewController *)pdfController didTapOnPageView:(PSPDFPageView *)pageView info:(PSPDFPageInfo *)pageInfo coordinates:(PSPDFPageCoordinates *)pageCoordinates {
-    NSLog(@"didTapOnPageView: %d coordinates: %@", pageView.page, pageCoordinates);
-    return NO;
-}
-
 @end
