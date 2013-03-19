65144dd4dd27710c7cf81bdc03d2b2c95f55012d
Peter Steinberger <me@petersteinberger.com>
2013-03-19 16:18:26 +0100

    Add new example

---
+++ Examples/PSPDFCatalog/Subclassing/PSCAnnotationTrailerCaptureDocument.h
@@ -0,0 +1,13 @@
+//
+//  PSCAnnotationTrailerCaptureDocument.h
+//  PSPDFCatalog-static
+//
+//  Created by Peter Steinberger on 3/19/13.
+//  Copyright (c) 2013 Peter Steinberger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSCAnnotationTrailerCaptureDocument : PSDPFDocument
+
+@end
---
+++ Examples/PSPDFCatalog/Subclassing/PSCAnnotationTrailerCaptureDocument.m
@@ -0,0 +1,13 @@
+//
+//  PSCAnnotationTrailerCaptureDocument.m
+//  PSPDFCatalog-static
+//
+//  Created by Peter Steinberger on 3/19/13.
+//  Copyright (c) 2013 Peter Steinberger. All rights reserved.
+//
+
+#import "PSCAnnotationTrailerCaptureDocument.h"
+
+@implementation PSCAnnotationTrailerCaptureDocument
+
+@end
