e206daad147a2484b0835a64933740b51b643a18
Peter Steinberger <me@petersteinberger.com>
2013-03-19 17:04:25 +0100

    Add new example
    Former-commit-id: 00e0bb37119ae75c6920864928fd030e0edfcaca

--- Examples/PSPDFCatalog/Subclassing/PSCAnnotationTrailerCaptureDocument.h
+++ Examples/PSPDFCatalog/Subclassing/PSCAnnotationTrailerCaptureDocument.h
@@ -1,13 +1,12 @@
 //
 //  PSCAnnotationTrailerCaptureDocument.h
-//  PSPDFCatalog-static
+//  PSPDFCatalog
 //
-//  Created by Peter Steinberger on 3/19/13.
 //  Copyright (c) 2013 Peter Steinberger. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface PSCAnnotationTrailerCaptureDocument : PSDPFDocument
+@interface PSCAnnotationTrailerCaptureDocument : PSPDFDocument
 
 @end
