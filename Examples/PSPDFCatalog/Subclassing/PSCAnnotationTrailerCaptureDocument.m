00e0bb37119ae75c6920864928fd030e0edfcaca
Peter Steinberger <me@petersteinberger.com>
2013-03-19 17:04:25 +0100

    Add new example

--- Examples/PSPDFCatalog/Subclassing/PSCAnnotationTrailerCaptureDocument.m
+++ Examples/PSPDFCatalog/Subclassing/PSCAnnotationTrailerCaptureDocument.m
@@ -1,13 +1,21 @@
 //
 //  PSCAnnotationTrailerCaptureDocument.m
-//  PSPDFCatalog-static
+//  PSPDFCatalog
 //
-//  Created by Peter Steinberger on 3/19/13.
 //  Copyright (c) 2013 Peter Steinberger. All rights reserved.
 //
 
 #import "PSCAnnotationTrailerCaptureDocument.h"
 
 @implementation PSCAnnotationTrailerCaptureDocument
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSPDFDocumentProviderDelegate
+
+// You can transmit the annotation trailer over to your server and re-attach the annotations there.
+- (void)documentProvider:(PSPDFDocumentProvider *)documentProvider didAppendData:(NSData *)data {
+    // Transmit it as *binary* data, not as string - the data output might also contain UTF16 parts and binary parts, the conversion here is just for debugging purposes, and the ascii conversion will break once it finds encoded stream data.
+    NSLog(@"Annotation Trailer attached: %@", [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding]);
+}
 
 @end
