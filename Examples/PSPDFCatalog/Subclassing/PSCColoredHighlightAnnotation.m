0b46a9a2ad75aa89c19f1df356aeba5e95c9d922
Peter Steinberger <me@petersteinberger.com>
2013-04-10 19:32:52 +0200

    Add new example
    Former-commit-id: 36cd1aa5ddc3d0359c0682e5aa9c7a2ec96ded07

---
+++ Examples/PSPDFCatalog/Subclassing/PSCColoredHighlightAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  PSCColoredHighlightAnnotation.m
+//  PSPDFCatalog-static
+//
+//  Created by Peter Steinberger on 4/10/13.
+//  Copyright (c) 2013 Peter Steinberger. All rights reserved.
+//
+
+#import "PSCColoredHighlightAnnotation.h"
+
+@implementation PSCColoredHighlightAnnotation
+
+@end
