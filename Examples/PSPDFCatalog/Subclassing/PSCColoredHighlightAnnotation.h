36cd1aa5ddc3d0359c0682e5aa9c7a2ec96ded07
Peter Steinberger <me@petersteinberger.com>
2013-04-10 19:32:52 +0200

    Add new example

---
+++ Examples/PSPDFCatalog/Subclassing/PSCColoredHighlightAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  PSCColoredHighlightAnnotation.h
+//  PSPDFCatalog-static
+//
+//  Created by Peter Steinberger on 4/10/13.
+//  Copyright (c) 2013 Peter Steinberger. All rights reserved.
+//
+
+#import "PSPDFHighlightAnnotation.h"
+
+@interface PSCColoredHighlightAnnotation : PSPDFHighlightAnnotation
+
+@end
