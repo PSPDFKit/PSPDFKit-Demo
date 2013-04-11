2bf5468d9eff586e7a17766e80ee1d59ec44e061
Peter Steinberger <me@petersteinberger.com>
2013-04-11 14:57:41 +0200

    Remove unneeded import
    Former-commit-id: a4a88f120cb511d62013eea157f4058de7fea3e5

--- Examples/PSPDFCatalog/Subclassing/PSCColoredHighlightAnnotation.h
+++ Examples/PSPDFCatalog/Subclassing/PSCColoredHighlightAnnotation.h
@@ -5,8 +5,6 @@
 //  Copyright (c) 2013 Peter Steinberger. All rights reserved.
 //
 
-#import "PSPDFHighlightAnnotation.h"
-
 // Colors new highlight annotations blue.
 @interface PSCColoredHighlightAnnotation : PSPDFHighlightAnnotation
 
