0ff4bbabb5c8429411e299372b3e4437777ed90d
Peter Steinberger <me@petersteinberger.com>
2013-04-18 23:09:14 +0200

    Add new multi user annotation example

---
+++ Examples/PSPDFCatalog/Subclassing/PSCMultipleUsersPDFViewController.h
@@ -0,0 +1,9 @@
+//
+//  PSCMultipleUsersPDFViewController.h
+//  PSPDFCatalog
+//
+//  Copyright (c) 2013 Peter Steinberger. All rights reserved.
+//
+
+@interface PSCMultipleUsersPDFViewController : PSPDFViewController
+@end
