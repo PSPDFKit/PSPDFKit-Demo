4581ac619cd360e481264b5bd3cf6c6ea8e40f04
Peter Steinberger <me@petersteinberger.com>
2013-05-14 12:40:45 +0200

    Add code to better simulate stress tests

--- Examples/PSPDFCatalog/Customization/PSCSplitPDFViewController.h
+++ Examples/PSPDFCatalog/Customization/PSCSplitPDFViewController.h
@@ -5,6 +5,10 @@
 //  Copyright (c) 2011-2013 Peter Steinberger. All rights reserved.
 //
 
+// Enable this to simulate improper document usage and test high-load sitations.
+// Only useful for development.
+#define kPSPDFEnableDocumentStressTest 0
+
 @interface PSCSplitPDFViewController : PSPDFViewController <UISplitViewControllerDelegate, PSPDFViewControllerDelegate>
 
 @property (nonatomic, strong, readonly) UIPopoverController *masterPopoverController;
