447c14171ee948b864915d5e48de3792cf2b9231
Peter Steinberger <me@petersteinberger.com>
2013-05-22 18:26:37 +0200

    Update file headers
    Former-commit-id: 57f257ba07b08efcfe35576cbe31d17c44d72e4b [formerly 9b6259587462c437bb95cc34e7367bc618a905e8]
    Former-commit-id: 3838e585509fb837bd2b5c7eba2bd73770c75e91

--- Examples/PSPDFCatalog/Tests/PSCTextParserTest.h
+++ Examples/PSPDFCatalog/Tests/PSCTextParserTest.h
@@ -2,7 +2,12 @@
 //  PSCTextParserTest.h
 //  PSPDFCatalog
 //
-//  Copyright (c) 2013 Peter Steinberger. All rights reserved.
+//  Copyright (c) 2013 PSPDFKit GmbH. All rights reserved.
+//
+//  THIS SOURCE CODE AND ANY ACCOMPANYING DOCUMENTATION ARE PROTECTED BY AUSTRIAN COPYRIGHT LAW
+//  AND MAY NOT BE RESOLD OR REDISTRIBUTED. USAGE IS BOUND TO THE PSPDFKIT LICENSE AGREEMENT.
+//  UNAUTHORIZED REPRODUCTION OR DISTRIBUTION IS SUBJECT TO CIVIL AND CRIMINAL PENALTIES.
+//  This notice may not be removed from this file.
 //
 
 #import <UIKit/UIKit.h>
