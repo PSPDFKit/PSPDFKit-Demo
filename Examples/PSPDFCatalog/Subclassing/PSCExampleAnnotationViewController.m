9b6259587462c437bb95cc34e7367bc618a905e8
Peter Steinberger <me@petersteinberger.com>
2013-05-22 18:26:37 +0200

    Update file headers

--- Examples/PSPDFCatalog/Subclassing/PSCExampleAnnotationViewController.m
+++ Examples/PSPDFCatalog/Subclassing/PSCExampleAnnotationViewController.m
@@ -2,7 +2,12 @@
 //  PSCExampleAnnotationViewController.m
 //  PSPDFCatalog
 //
-//  Copyright (c) 2012-2013 Peter Steinberger. All rights reserved.
+//  Copyright (c) 2012-2013 PSPDFKit GmbH. All rights reserved.
+//
+//  THIS SOURCE CODE AND ANY ACCOMPANYING DOCUMENTATION ARE PROTECTED BY AUSTRIAN COPYRIGHT LAW
+//  AND MAY NOT BE RESOLD OR REDISTRIBUTED. USAGE IS BOUND TO THE PSPDFKIT LICENSE AGREEMENT.
+//  UNAUTHORIZED REPRODUCTION OR DISTRIBUTION IS SUBJECT TO CIVIL AND CRIMINAL PENALTIES.
+//  This notice may not be removed from this file.
 //
 
 #import "PSCExampleAnnotationViewController.h"
