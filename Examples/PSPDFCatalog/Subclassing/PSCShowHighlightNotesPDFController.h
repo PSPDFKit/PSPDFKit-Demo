8ec57e92cf22242005caec18698ccc207ed21398
Peter Steinberger <me@petersteinberger.com>
2013-06-14 22:36:32 +0100

    Add new example
    Former-commit-id: be928e72593b398e81a411c2f05e1ad2de7c34bb

---
+++ Examples/PSPDFCatalog/Subclassing/PSCShowHighlightNotesPDFController.h
@@ -0,0 +1,13 @@
+//
+//  PSCShowHighlightNotesPDFController.h
+//  PSPDFCatalog-static
+//
+//  Created by Peter Steinberger on 14/06/13.
+//  Copyright (c) 2013 Peter Steinberger. All rights reserved.
+//
+
+#import "PSPDFViewController.h"
+
+@interface PSCShowHighlightNotesPDFController : PSPDFViewController <PSPDFViewControllerDelegate>
+
+@end
