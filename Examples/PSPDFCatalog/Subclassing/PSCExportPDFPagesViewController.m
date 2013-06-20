c9cd1661467dbdb393cd107f2258d0a7ff71c0f9
Peter Steinberger <me@petersteinberger.com>
2013-06-20 20:35:23 +0200

    Add new export actions example

---
+++ Examples/PSPDFCatalog/Subclassing/PSCExportPDFPagesViewController.m
@@ -0,0 +1,38 @@
+//
+//  PSCExportPDFPagesViewController.m
+//  PSPDFCatalog-static
+//
+//  Created by Peter Steinberger on 20/06/13.
+//  Copyright (c) 2013 PSPDFKit GmbH. All rights reserved.
+//
+
+#import "PSCExportPDFPagesViewController.h"
+
+@interface PSCExportPDFPagesViewController ()
+
+@end
+
+@implementation PSCExportPDFPagesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
