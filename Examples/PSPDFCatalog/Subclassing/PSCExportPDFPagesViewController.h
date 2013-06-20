64d5016ebb1a558f4b5615f0e0f321c2df1d1c2f
Peter Steinberger <me@petersteinberger.com>
2013-06-20 20:35:23 +0200

    Add new export actions example
    Former-commit-id: bd93550b76cb19d868a8afbfda0823b1cbbcf87d [formerly c9cd1661467dbdb393cd107f2258d0a7ff71c0f9]
    Former-commit-id: 83eafb5d52db2f51a66260346772659e890cc14d

---
+++ Examples/PSPDFCatalog/Subclassing/PSCExportPDFPagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  PSCExportPDFPagesViewController.h
+//  PSPDFCatalog-static
+//
+//  Created by Peter Steinberger on 20/06/13.
+//  Copyright (c) 2013 PSPDFKit GmbH. All rights reserved.
+//
+
+#import "PSPDFViewController.h"
+
+@interface PSCExportPDFPagesViewController : PSPDFViewController
+
+@end
