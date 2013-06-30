535b4c7f42889e65e6791536d5fc9a4caa50ae8c
Peter Steinberger <me@petersteinberger.com>
2013-06-30 16:33:14 +0200

    Fixes a check for iOS7

--- Examples/PSPDFCatalog/Kiosk/PSCSettingsController.m
+++ Examples/PSPDFCatalog/Kiosk/PSCSettingsController.m
@@ -183,7 +183,7 @@ __attribute__((constructor)) static void setupDefaults(void) {
     UIGraphicsEndImageContext();
     // Don't tint on iOS7
 #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 70000
-    if ([UIImage instanceMethodForSelector:@selector(imageWithRenderingMode:)]) {
+    if ([UIImage instancesRespondToSelector:@selector(imageWithRenderingMode:)]) {
         renderedImage = [renderedImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     }
 #endif
