d04d395b7d3d160a0ccd668d1f5bf1d019541be0
Peter Steinberger <me@petersteinberger.com>
2013-07-05 16:42:28 +0200

    Add Core Data Example (WIP)

---
+++ Examples/PSPDFCatalog/AnnotationProviders/CoreDataBackend/PSCCoreDataAnnotationProvider.h
@@ -0,0 +1,26 @@
+//
+//  PSCCoreDataAnnotationProvider.h
+//  PSPDFCatalog
+//
+//  Copyright (c) 2013 PSPDFKit GmbH. All rights reserved.
+//
+//  THIS SOURCE CODE AND ANY ACCOMPANYING DOCUMENTATION ARE PROTECTED BY AUSTRIAN COPYRIGHT LAW
+//  AND MAY NOT BE RESOLD OR REDISTRIBUTED. USAGE IS BOUND TO THE PSPDFKIT LICENSE AGREEMENT.
+//  UNAUTHORIZED REPRODUCTION OR DISTRIBUTION IS SUBJECT TO CIVIL AND CRIMINAL PENALTIES.
+//  This notice may not be removed from this file.
+//
+
+#import <Foundation/Foundation.h>
+
+// This is an example how you could store your annotations with CoreData in the simplest possible way.
+// Note: It's not a great idea performance-wise to use NSKeyedArchiver for data serialization.
+@interface PSCCoreDataAnnotationProvider : NSObject <PSPDFAnnotationProvider>
+
+// Designated initializer.
+- (id)initWithDocumentProvider:(PSPDFDocumentProvider *)documentProvider;
+
+// Associated documentProvider.
+@property (nonatomic, weak) PSPDFDocumentProvider *documentProvider;
+
+
+@end
