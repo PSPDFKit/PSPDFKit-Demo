7c5d952a67d6d529fa308d202b1b4c292ee6d3b8
Peter Steinberger <me@petersteinberger.com>
2013-07-07 15:36:21 +0200

    Clean up
    Former-commit-id: 0728bab30eaef2965d97f2974e369e2531f17a0a

--- Examples/PSPDFCatalog/PSCAppDelegate.m
+++ Examples/PSPDFCatalog/PSCAppDelegate.m
@@ -11,32 +11,23 @@
 #import "PSCAppDelegate.h"
 #import "PSCatalogViewController.h"
 #import <DropboxSDK/DropboxSDK.h>
-#import <objc/message.h>
+
 #ifdef HOCKEY_ENABLED
 #import <HockeySDK/HockeySDK.h>
-#endif
-#if !__has_feature(objc_arc)
-#error "Compile this file with ARC"
+@interface PSCAppDelegate () <BITHockeyManagerDelegate, BITUpdateManagerDelegate, BITCrashManagerDelegate> @end
 #endif
 
-@interface PSCAppDelegate ()
-#ifdef HOCKEY_ENABLED
-<BITHockeyManagerDelegate, BITUpdateManagerDelegate, BITCrashManagerDelegate>
-#endif
-@end
 @implementation PSCAppDelegate
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - UIApplicationDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     // Example how to localize strings in PSPDFKit.
     // See PSPDFKit.bundle/en.lproj/PSPDFKit.strings for all available strings.
-    // You can also replace the strings in the PSPDFKit.bundle, but then make sure you merge your changes anytime the bundle is updated.
-    // If you have created custom translations, feel free to submit them to peter@pspdfkit.com for inclusion.
-    PSPDFSetLocalizationDictionary(@{@"en" :
-                                         @{@"%d of %d" : @"Page %d of %d",
-                                           @"%d-%d of %d" : @"Pages %d-%d of %d",
-                                           }
-                                     });
+    PSPDFSetLocalizationDictionary(@{@"en" : @{@"%d of %d" : @"Page %d of %d",
+                                              @"%d-%d of %d" : @"Pages %d-%d of %d"}});
 
     // You can also customize localization with a block.
     // If you return nil, the default PSPDFKit language system will be used.
@@ -52,19 +43,16 @@
     // Change log level to be more verbose.
 #ifdef DEBUG
     PSPDFLogLevel = PSPDFLogLevelMaskInfo|PSPDFLogLevelMaskWarning|PSPDFLogLevelMaskError;
-
-    // Clear cache, better debugging
     //[PSPDFCache.sharedCache clearCache];
+    //NSLog(@"Cache: %@", NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)[0]);
 #endif
 
     // Enable if you're having memory issues.
     //PSPDFLowMemoryMode = YES;
 
-    // enable global Undo/Redo
-    application.applicationSupportsShakeToEdit = YES;
-
-    self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
-
+    // Set your license key here. PSPDFKit is commercial software.
+    // Each PSPDFKit license is bound to a specific app bundle id.
+    // Visit http://customers.pspdfkit.com to get your license key.
     PSPDFSetLicenseKey("fdfqSCYNKUWegznURyw849L+jz3BPdzGQQr6ZsJuarQb7wlVlmLm1SmsvfHv5YX"
                        "uMn1iCqH4965ooaE96ltFB58pDIJRwOzYOUZdNaYr9C0Vut0c+cwGv0xJaiS"
                        "ltSPvtlQL0XOp6kp35MvGtQGVTunJLXl3N4vEnllJjlcpBCWjqw7xnL8WMgY"
@@ -74,39 +62,31 @@
                        "yT4ZwYIQ6ML00AFljeW2lP0/zVJ2IpFEh0EbUu+cZKBXbOqdwSpA4oZloK5v"
                        "MWNqPFun6bx0nj2XBB+hSAHUuoizOH5/MGkoUHRj3JF59rt3xm7HGfk9q6kh"
                        "KGLiNZK6Nlk=");
-    
-#if 0
-    // Directly push a PSPDFViewController
-    NSURL *samplesURL = [[[NSBundle mainBundle] resourceURL] URLByAppendingPathComponent:@"Samples" isDirectory:NO];
-    PSPDFDocument *document = [PSPDFDocument documentWithURL:[samplesURL URLByAppendingPathComponent:kHackerMagazineExample isDirectory:YES]];
-    PSPDFViewController *viewController = [[PSPDFViewController alloc] initWithDocument:document];
-    self.catalog = [[UINavigationController alloc] initWithRootViewController:viewController];
-#else
+
     // Create catalog controller
     PSCatalogViewController *catalogController = [[PSCatalogViewController alloc] initWithStyle:UITableViewStyleGrouped];
     // PSPDFNavigationController is a simple subclass that forwards iOS6 rotation methods.
     self.catalog = [[PSPDFNavigationController alloc] initWithRootViewController:catalogController];
-#endif
+    self.window  = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
     self.window.rootViewController = self.catalog;
     [self.window makeKeyAndVisible];
+
+    // Enable global Undo/Redo
+    application.applicationSupportsShakeToEdit = YES;
 
     // Opened with the Open In... feature?
     [self handleOpenURL:launchOptions[UIApplicationLaunchOptionsURLKey]];
 
-    // print current cache dir
-    NSString *cacheFolder = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)[0];
-    NSLog(@"Cache Folder: %@", cacheFolder);
-
-    // Perform non-important actions on the next runloop, for faster app-starting.
-    dispatch_async(dispatch_get_main_queue(), ^{
-        // HockeyApp (http://hockeyapp.net) is a *great* service to manage crashes and distribute beta builds. It's well worth the money.
+    // HockeyApp (http://hockeyapp.net) is a *great* service to manage crashes and distribute beta builds. It's well worth the money.
 #if !defined(CONFIGURATION_Debug) && defined(PSPDF_USE_SOURCE) && defined(HOCKEY_ENABLED)
+    dispatch_async(dispatch_get_main_queue(), ^{
         NSLog(@"This example project uses HockeyApp for crash reports and Localytics for user statistics. Make sure to either remove that or change the identifiers before shipping your app, if you use PSPDFCatalog/PSPDFKiosk as the foundation of your application.");
         [[BITHockeyManager sharedHockeyManager] configureWithIdentifier:@"fa73e1f8f3806bcb3466c5ab16d70768" delegate:nil];
         [[BITHockeyManager sharedHockeyManager] crashManager].crashManagerStatus = BITCrashManagerStatusAutoSend;
         [[BITHockeyManager sharedHockeyManager] startManager];
-#endif
     });
+#endif
+
     return YES;
 }
 
@@ -118,9 +98,7 @@
 - (BOOL)handleOpenURL:(NSURL *)launchURL {
     // Add Dropbox hook
     if ([[DBSession sharedSession] handleOpenURL:launchURL]) {
-        if (DBSession.sharedSession.isLinked) {
-            PSCLog(@"App linked successfully!");
-        }
+        if (DBSession.sharedSession.isLinked) PSCLog(@"App linked successfully!");
         return YES;
     }
 
