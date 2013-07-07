754e8ee0733bb3105b122ee3458cef7d6db8c9ba
Peter Steinberger <me@petersteinberger.com>
2013-07-07 12:44:23 +0200

    Relicense example code
    Former-commit-id: bbe60ebd45d9b3529d3fa926faec1d4e8b509e3b [formerly 5c3b49b8d547c80b04bfb1f377c12406cf64ddf5]
    Former-commit-id: 0b5958bf3c3655efcb581198c8599598f07b01d7

--- Examples/PSPDFCatalog/Subclassing/PSCExportPDFPagesViewController.h
+++ Examples/PSPDFCatalog/Subclassing/PSCExportPDFPagesViewController.h
@@ -4,11 +4,10 @@
 //
 //  Copyright (c) 2013 PSPDFKit GmbH. All rights reserved.
 //
-//  THIS SOURCE CODE AND ANY ACCOMPANYING DOCUMENTATION ARE PROTECTED BY AUSTRIAN COPYRIGHT LAW
-//  AND MAY NOT BE RESOLD OR REDISTRIBUTED. USAGE IS BOUND TO THE PSPDFKIT LICENSE AGREEMENT.
-//  UNAUTHORIZED REPRODUCTION OR DISTRIBUTION IS SUBJECT TO CIVIL AND CRIMINAL PENALTIES.
-//  This notice may not be removed from this file.
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
 //
+
 // This example shows how to track selection in the thumbnail view controller and allow to export single/multiple pages.
 @interface PSCExportPDFPagesViewController : PSPDFViewController
 
