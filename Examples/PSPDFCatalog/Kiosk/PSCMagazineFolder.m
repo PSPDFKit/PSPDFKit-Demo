0b5958bf3c3655efcb581198c8599598f07b01d7
Peter Steinberger <me@petersteinberger.com>
2013-07-07 12:44:23 +0200

    Relicense example code
    Former-commit-id: 5c3b49b8d547c80b04bfb1f377c12406cf64ddf5

--- Examples/PSPDFCatalog/Kiosk/PSCMagazineFolder.m
+++ Examples/PSPDFCatalog/Kiosk/PSCMagazineFolder.m
@@ -4,10 +4,8 @@
 //
 //  Copyright 2011-2013 PSPDFKit GmbH. All rights reserved.
 //
-//  THIS SOURCE CODE AND ANY ACCOMPANYING DOCUMENTATION ARE PROTECTED BY AUSTRIAN COPYRIGHT LAW
-//  AND MAY NOT BE RESOLD OR REDISTRIBUTED. USAGE IS BOUND TO THE PSPDFKIT LICENSE AGREEMENT.
-//  UNAUTHORIZED REPRODUCTION OR DISTRIBUTION IS SUBJECT TO CIVIL AND CRIMINAL PENALTIES.
-//  This notice may not be removed from this file.
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
 //
 
 #import "PSCMagazineFolder.h"
