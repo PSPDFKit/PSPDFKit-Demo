3e67fbed3ccebfc218ba5c36e98d8ba1ca125a7f
Peter Steinberger <me@petersteinberger.com>
2013-08-01 21:05:48 +0200

    Add example how to customize default stamps
    Former-commit-id: 595672c80739823240044d2c027c7a858f504694 [formerly 27300ee22c1ed68495e7d28b69d908dba1ad7ff1]
    Former-commit-id: c8aa381eecf0f5e35046e3f92c0bbaa1d06cc08f

---
+++ Examples/PSPDFCatalog/Helper/NSObject+PSCDeallocationBlock.h
@@ -0,0 +1,18 @@
+//
+//  NSObject+PSCDeallocationBlock.h
+//  PSPDFCatalog
+//
+//  Copyright (c) 2012-2013 PSPDFKit GmbH. All rights reserved.
+//
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (PSCDeallocationBlock)
+
+// Register block to be called when `self` is deallocated.
+- (void)psc_addDeallocBlock:(dispatch_block_t)block;
+
+@end
