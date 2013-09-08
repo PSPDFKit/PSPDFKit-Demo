4bc216c297c434a486de939ff3196d49c5d52590
Peter Steinberger <me@petersteinberger.com>
2013-09-08 22:38:47 +0200

    Add new example infrastructure and add new toolbar example

---
+++ Examples/PSPDFCatalog/ExampleBuilder/PSCExample.h
@@ -0,0 +1,34 @@
+//
+//  PSCExample.h
+//  PSPDFCatalog
+//
+//  Copyright (c) 2012-2013 PSPDFKit GmbH. All rights reserved.
+//
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, PSCExampleCategory) {
+    PSCExampleCategoryAnnotations
+};
+
+extern NSString *PSPDFStringFromExampleCategory(PSCExampleCategory category);
+
+// Base class for examples.
+@interface PSCExample : NSObject
+
+// The example title. Mandatory.
+@property (nonatomic, copy) NSString *title;
+
+// The example description. Optional.
+@property (nonatomic, copy) NSString *description;
+
+// The category for this example.
+@property (nonatomic, assign) PSCExampleCategory category;
+
+// Builds the sample and returns a new view controller that will then be pushed.
+- (UIViewController *)invoke;
+
+@end
---
+++ Examples/PSPDFCatalog/ExampleBuilder/PSCExampleManager.h
@@ -0,0 +1,22 @@
+//
+//  PSCExampleManager.h
+//  PSPDFCatalog
+//
+//  Copyright (c) 2012-2013 PSPDFKit GmbH. All rights reserved.
+//
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
+//
+
+#import <Foundation/Foundation.h>
+#import "PSCExample.h"
+
+@interface PSCExampleManager : NSObject
+
+// Singleton
++ (instancetype)defaultManager;
+
+// Get all examples.
+@property (nonatomic, copy, readonly) NSArray *allExamples;
+
+@end
