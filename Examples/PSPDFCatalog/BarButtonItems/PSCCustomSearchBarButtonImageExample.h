a3a25e53ed74b5a52642d0abd55600f437bee418
Peter Steinberger <me@petersteinberger.com>
2013-09-09 22:48:36 +0200

    Add new example

---
+++ Examples/PSPDFCatalog/BarButtonItems/PSCCustomSearchBarButtonImageExample.h
@@ -0,0 +1,17 @@
+//
+//  PSCCustomSearchBarButtonImage.h
+//  PSPDFKit
+//
+//  Copyright (c) 2012-2013 PSPDFKit GmbH. All rights reserved.
+//
+//  THIS SOURCE CODE AND ANY ACCOMPANYING DOCUMENTATION ARE PROTECTED BY AUSTRIAN COPYRIGHT LAW
+//  AND MAY NOT BE RESOLD OR REDISTRIBUTED. USAGE IS BOUND TO THE PSPDFKIT LICENSE AGREEMENT.
+//  UNAUTHORIZED REPRODUCTION OR DISTRIBUTION IS SUBJECT TO CIVIL AND CRIMINAL PENALTIES.
+//  This notice may not be removed from this file.
+//
+
+#import "PSCExample.h"
+
+@interface PSCCustomSearchBarButtonImageExample : PSCExample
+
+@end
