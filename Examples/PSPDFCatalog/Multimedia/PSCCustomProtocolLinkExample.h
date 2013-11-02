0c00f3acb7e23dfb4bb68d7242f0e965a78c3c80
Peter Steinberger <me@petersteinberger.com>
2013-11-02 09:58:40 +0100

    Add custom protocol example

---
+++ Examples/PSPDFCatalog/Multimedia/PSCCustomProtocolLinkExample.h
@@ -0,0 +1,15 @@
+//
+//  PSCCustomProtocolLinkExample.h
+//  PSPDFCatalog
+//
+//  Copyright (c) 2012-2013 PSPDFKit GmbH. All rights reserved.
+//
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
+//
+
+#import "PSCExample.h"
+
+@interface PSCCustomProtocolLinkExample : PSCExample
+
+@end
