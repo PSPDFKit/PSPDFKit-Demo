6ed9dd7c79d37e951bc0532f7cd52dbd92633a33
Peter Steinberger <me@petersteinberger.com>
2013-11-10 10:33:29 +0100

    Code Style
    Former-commit-id: ac712791726677e8f8212c788ff7512f7faf1c3d [formerly a4b8e25e480b9d37e8eb5f40fbea611209623a32]
    Former-commit-id: 1639ca1dffcb86c2e2cb5e526aa2b6f35f6f8e16

--- Examples/PSPDFCatalog/main.m
+++ Examples/PSPDFCatalog/main.m
@@ -16,6 +16,6 @@
 
 int main(int argc, char *argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PSCAppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass(PSCAppDelegate.class));
     }
 }
