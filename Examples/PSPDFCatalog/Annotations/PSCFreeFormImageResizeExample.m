a5ae07b3daaa0d4ee07a84f209352746669aecb9
Peter Steinberger <me@petersteinberger.com>
2013-11-18 11:36:07 +0100

    Add new example for free form image resizing

---
+++ Examples/PSPDFCatalog/Annotations/PSCFreeFormImageResizeExample.m
@@ -0,0 +1,51 @@
+//
+//  PSCFreeFormImageResizeExample.m
+//  PSPDFCatalog
+//
+//  Copyright (c) 2011-2013 PSPDFKit GmbH. All rights reserved.
+//
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
+//
+
+#import "PSCFreeFormImageResizeExample.h"
+#import "PSCAssetLoader.h"
+
+@interface PSCFreeFormResizeStampAnnotation : PSPDFStampAnnotation @end
+
+@implementation PSCFreeFormImageResizeExample
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSCExample
+
+- (id)init {
+    if (self = [super init]) {
+        self.title = @"Free Form Image Resize";
+        self.category = PSCExampleCategoryViewCustomization;
+    }
+    return self;
+}
+
+- (UIViewController *)invokeWithDelegate:(id<PSCExampleRunner>)delegate {
+    // Set up the document.
+    PSPDFDocument *document = [PSCAssetLoader sampleDocumentWithName:kHackerMagazineExample];
+    [document overrideClass:PSPDFStampAnnotation.class withClass:PSCFreeFormResizeStampAnnotation.class];
+
+    // And also the controller.
+    PSPDFViewController *pdfController = [[PSPDFViewController alloc] initWithDocument:document];
+    return pdfController;
+}
+
+@end
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSCFreeFormResizeStampAnnotation
+
+@implementation PSCFreeFormResizeStampAnnotation
+
+// Never maintain aspect ratio. By default this would return YES for images.
+- (BOOL)shouldMaintainAspectRatio {
+    return NO;
+}
+
+@end
