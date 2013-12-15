8b1d50897f911f3f00f848e197c4ffa0ef5b3bfa
Peter Steinberger <me@petersteinberger.com>
2013-12-15 17:49:54 +0100

    Fix warnings in AFNetworking
    Former-commit-id: 49b7042f48bf3313c919433d3b3d4c162da55ace

--- Examples/PSPDFCatalog/Vendor/AFNetworking/AFHTTPRequestOperation.h
+++ Examples/PSPDFCatalog/Vendor/AFNetworking/AFHTTPRequestOperation.h
@@ -34,11 +34,6 @@
 ///------------------------------------------------
 
 /**
- The last HTTP response received by the operation's connection.
- */
-@property (readonly, nonatomic, strong) NSHTTPURLResponse *response;
-
-/**
  Responses sent from the server in data tasks created with `dataTaskWithRequest:success:failure:` and run using the `GET` / `POST` / et al. convenience methods are automatically validated and serialized by the response serializer. By default, this property is set to an AFHTTPResoinse serializer, which uses the raw data as its response object. The serializer validates the status code to be in the `2XX` range, denoting success. If the response serializer generates an error in `-responseObjectForResponse:data:error:`, the `failure` callback of the session task or request operation will be executed; otherwise, the `success` callback will be executed.
 
  @warning `responseSerializer` must not be `nil`. Setting a response serializer will clear out any cached value
