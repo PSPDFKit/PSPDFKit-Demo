093530e9a08cdd1e4d882b4e1db2794a3504147d
Peter Steinberger <me@petersteinberger.com>
2013-12-20 09:13:01 +0100

    Add new example

---
+++ Examples/PSPDFCatalog/Customization/PSCSignatureControllerCustomizationExample.h
@@ -0,0 +1,13 @@
+//
+//  PSCSignatureControllerCustomizationExample.h
+//  PSPDFCatalog
+//
+//  Created by Peter Steinberger on 20/12/13.
+//  Copyright (c) 2013 PSPDFKit. All rights reserved.
+//
+
+#import "PSCExample.h"
+
+@interface PSCSignatureControllerCustomizationExample : PSCExample
+
+@end
