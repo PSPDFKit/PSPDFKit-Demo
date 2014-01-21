b772c3dd0615fbe7329e47f2298ed3d6bca66163
Peter Steinberger <me@petersteinberger.com>
2014-01-21 20:03:45 +0100

    add commented out example code
    Former-commit-id: 33f884e88436c0a2cfbcc9696c0c8ea48e03b5b1

--- Examples/PSPDFCatalog/TabbedExample/PSCTabbedExampleViewController.m
+++ Examples/PSPDFCatalog/TabbedExample/PSCTabbedExampleViewController.m
@@ -50,7 +50,19 @@ const char *clearAllActionSheetToken;
             NSArray *documents = [PSPDFDocumentPickerController documentsFromDirectory:@"/Bundle/Samples" includeSubdirectories:YES];
             self.documents = [documents filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(id evaluatedObject, NSDictionary *bindings) {
                 return arc4random_uniform(2) > 0; // returns 0 or 1 randomly.
-            }]] ;
+            }]];
+
+            // AES test
+            /*
+            NSURL *samplesURL = [NSBundle.mainBundle.resourceURL URLByAppendingPathComponent:@"Samples"];
+            NSURL *encryptedPDF = [samplesURL URLByAppendingPathComponent:@"aes-encrypted.pdf.aes"];
+            NSString *passphrase = @"afghadöghdgdhfgöhapvuenröaoeruhföaeiruaerub";
+            NSString *salt = @"ducrXn9WaRdpaBfMjDTJVjUf3FApA6gtim0e61LeSGWV9sTxB0r26mPs59Lbcexn";
+            PSPDFAESCryptoDataProvider *cryptoWrapper = [[PSPDFAESCryptoDataProvider alloc] initWithURL:encryptedPDF passphrase:passphrase salt:salt rounds:PSPDFDefaultPBKDFNumberOfRounds];
+            PSPDFDocument *document = [PSPDFDocument documentWithDataProvider:cryptoWrapper.dataProvider];
+            document.UID = [encryptedPDF lastPathComponent]; // manually set an UID for encrypted documents.
+            self.documents = @[document];
+             */
         }
     }
     return self;
