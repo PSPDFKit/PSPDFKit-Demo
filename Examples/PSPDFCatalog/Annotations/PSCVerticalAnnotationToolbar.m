a6d70093c6e74ab5ae454a4038e1919a311568c5
Matej Bukovinski <matej@bukovinski.com>
2014-01-24 13:34:13 +0100

    Updated for latest annotation toolbar changes.
    Former-commit-id: 793ca491eac27512b1929e42cf7c7a054aeb6949

--- Examples/PSPDFCatalog/Annotations/PSCVerticalAnnotationToolbar.m
+++ Examples/PSPDFCatalog/Annotations/PSCVerticalAnnotationToolbar.m
@@ -41,12 +41,12 @@
 
         // draw button
         if ([pdfController.document.editableAnnotationTypes containsObject:PSPDFAnnotationStringFreeText]) {
-        UIButton *freetextButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        UIImage *freeTextImage = PSPDFBundleImage(@"freetext");
-        [freetextButton setImage:freeTextImage forState:UIControlStateNormal];
-        [freetextButton addTarget:self action:@selector(freetextButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
-        [self addSubview:freetextButton];
-        self.freeTextButton = freetextButton;
+			UIButton *freetextButton = [UIButton buttonWithType:UIButtonTypeCustom];
+			UIImage *freeTextImage = PSPDFBundleImage(@"freetext");
+			[freetextButton setImage:freeTextImage forState:UIControlStateNormal];
+			[freetextButton addTarget:self action:@selector(freetextButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+			[self addSubview:freetextButton];
+			self.freeTextButton = freetextButton;
         }
 
     }
@@ -97,7 +97,7 @@
     }else {
         pdfController.HUDViewMode = PSPDFHUDViewModeAutomatic;
         // remove toolbar
-        [self.toolbar finishDrawingAnimated:YES saveAnnotation:NO];
+		[self.toolbar hideAndRemoveToolbarAnimated:YES completion:nil];
     }
 }
 
