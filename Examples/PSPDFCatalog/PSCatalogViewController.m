6a28227bb1e800bfaa77e111106e2dea43491e10
Peter Steinberger <me@petersteinberger.com>
2014-01-24 16:24:14 +0100

    update test
    Former-commit-id: dfae7dfcaab435f68d735b7128e0120c6ecf5d14

--- Examples/PSPDFCatalog/PSCatalogViewController.m
+++ Examples/PSPDFCatalog/PSCatalogViewController.m
@@ -125,7 +125,7 @@ static NSString *const PSCLastIndexPath = @"PSCLastIndexPath";
     // Playground is convenient for testing.
     [appSection addContent:[PSContent contentWithTitle:@"PSPDFViewController Playground" block:^{
         PSPDFDocument *document = [PSPDFDocument documentWithURL:hackerMagURL];
-        //PSPDFDocument *document = [PSPDFDocument documentWithURL:[samplesURL URLByAppendingPathComponent:@"stamps2.pdf"]];
+        //PSPDFDocument *document = [PSPDFDocument documentWithURL:[samplesURL URLByAppendingPathComponent:@"Testcase_Form_YesNo.pdf"]];
         //PSPDFDocument *document = [PSPDFDocument documentWithURL:[samplesURL URLByAppendingPathComponent:@"Testcase_Wartungsformular_2.pdf"]];
         //PSPDFDocument *document = [PSPDFDocument documentWithURL:[samplesURL URLByAppendingPathComponent:@"OoPdfFormExample.pdf"]];
         //PSPDFDocument *document = [PSPDFDocument documentWithURL:[samplesURL URLByAppendingPathComponent:@"W8_Formular.pdf"]];
