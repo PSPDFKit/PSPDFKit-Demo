c5fb824a9be7ca1eb06ff6f252137fc812f6f2d9
Peter Steinberger <me@petersteinberger.com>
2014-02-17 16:44:10 +0100

    Improve sidebar example

--- Examples/PSPDFCatalog/Interfaces/PSCSidebarExample.m
+++ Examples/PSPDFCatalog/Interfaces/PSCSidebarExample.m
@@ -10,6 +10,15 @@
 
 #import "PSCSidebarExample.h"
 #import "PSCAssetLoader.h"
+#import "UIBarButtonItem+PSCBlockSupport.h"
+
+@interface PSPDFSidebarViewController : UIViewController <PSPDFDocumentPickerControllerDelegate, UISplitViewControllerDelegate>
+- (id)initWithDocument:(PSPDFDocument *)document;
+@property (nonatomic, strong) PSPDFDocument *document;
+@property (nonatomic, strong) PSPDFViewController *pdfController;
+@property (nonatomic, strong) PSPDFContainerViewController *sidebarController;
+@property (nonatomic, strong) UIBarButtonItem *pickerButtonItem;
+@end
 
 @implementation PSCSidebarExample
 
@@ -30,36 +39,108 @@
     PSPDFDocument *document = [PSCAssetLoader sampleDocumentWithName:kHackerMagazineExample];
     document.annotationSaveMode = PSPDFAnnotationSaveModeDisabled;
 
-    // Set up the PDF controller
-    PSPDFViewController *pdfController = [[PSPDFViewController alloc] initWithDocument:document];
-    pdfController.pageMode = PSPDFPageModeSingle;
-    pdfController.fitToWidthEnabled = YES;
-    pdfController.rightBarButtonItems = @[pdfController.annotationButtonItem, pdfController.searchButtonItem, pdfController.activityButtonItem, pdfController.viewModeButtonItem];
-    UINavigationController *navPDFController = [[UINavigationController alloc] initWithRootViewController:pdfController];
-
-    // Set up the container for annotations/outline/bookmarks.
-    PSPDFOutlineViewController *outlineController = [[PSPDFOutlineViewController alloc] initWithDocument:document delegate:pdfController];
-    PSPDFBookmarkViewController *bookmarkController = [[PSPDFBookmarkViewController alloc] initWithDocument:document delegate:pdfController];
-    PSPDFAnnotationTableViewController *annotationController = [[PSPDFAnnotationTableViewController alloc] initWithDocument:document delegate:pdfController];
-    PSPDFSearchViewController *searchController = [[PSPDFSearchViewController alloc] initWithDocument:document delegate:pdfController];
-    searchController.pinSearchBarToHeader = YES;
-    PSPDFContainerViewController *containerController = [[PSPDFContainerViewController alloc] initWithControllers:@[outlineController, annotationController, bookmarkController, searchController] titles:nil delegate:nil];
-    containerController.shouldShowCloseButton = NO;
-    containerController.shouldAnimateChanges = NO;
-    UINavigationController *navContainer = [[UINavigationController alloc] initWithRootViewController:containerController];
-
-    // Create the split view controller
-    UISplitViewController *splitController = [[UISplitViewController alloc] init];
-    splitController.viewControllers = @[navContainer, navPDFController];
-
-    // Use a dummy to present, as the split controller doesn't like to be presented modally.
-    UIViewController *dummyController = [[UIViewController alloc] init];
-    [dummyController addChildViewController:splitController];
-    [dummyController.view addSubview:splitController.view];
-    [splitController didMoveToParentViewController:dummyController];
-
-    [delegate.currentViewController presentViewController:dummyController animated:YES completion:NULL];
+    PSPDFSidebarViewController *sidebarController = [[PSPDFSidebarViewController alloc] initWithDocument:document];
+    [delegate.currentViewController presentViewController:sidebarController animated:YES completion:NULL];
     return nil;
+}
+
+@end
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSPDFSidebarViewController
+
+@implementation PSPDFSidebarViewController
+
+- (id)initWithDocument:(PSPDFDocument *)document {
+    if (self = [super init]) {
+        // Set up the PDF controller
+        PSPDFViewController *pdfController = [[PSPDFViewController alloc] initWithDocument:document];
+        pdfController.pageMode = PSPDFPageModeSingle;
+        pdfController.fitToWidthEnabled = YES;
+
+        // Set up the document picker button
+        PSPDFDocumentPickerController *documentPicker = [[PSPDFDocumentPickerController alloc] initWithDirectory:@"/Bundle/Samples" includeSubdirectories:YES library:PSPDFLibrary.defaultLibrary delegate:self];
+        __weak PSPDFViewController *weakPDFController = pdfController;
+        UIBarButtonItem *pickerButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Document" style:UIBarButtonItemStyleBordered block:^(id sender) {
+            [weakPDFController presentModalOrInPopover:documentPicker embeddedInNavigationController:YES withCloseButton:NO animated:YES sender:sender options:nil];
+        }];
+        self.pickerButtonItem = pickerButtonItem;
+
+        pdfController.leftBarButtonItems = @[pdfController.closeButtonItem, pickerButtonItem];
+        pdfController.barButtonItemsAlwaysEnabled = @[pickerButtonItem];
+        pdfController.rightBarButtonItems = @[pdfController.annotationButtonItem, pdfController.searchButtonItem, pdfController.activityButtonItem, pdfController.viewModeButtonItem];
+        self.pdfController = pdfController;
+        UINavigationController *navPDFController = [[UINavigationController alloc] initWithRootViewController:pdfController];
+
+        // Set up the container for annotations/outline/bookmarks/search.
+        PSPDFOutlineViewController *outlineController = [[PSPDFOutlineViewController alloc] initWithDocument:document delegate:pdfController];
+        PSPDFBookmarkViewController *bookmarkController = [[PSPDFBookmarkViewController alloc] initWithDocument:document delegate:pdfController];
+        PSPDFAnnotationTableViewController *annotationController = [[PSPDFAnnotationTableViewController alloc] initWithDocument:document delegate:pdfController];
+        PSPDFSearchViewController *searchController = [[PSPDFSearchViewController alloc] initWithDocument:document delegate:pdfController];
+        searchController.pinSearchBarToHeader = YES;
+        PSPDFContainerViewController *containerController = [[PSPDFContainerViewController alloc] initWithControllers:@[outlineController, annotationController, bookmarkController, searchController] titles:nil delegate:nil];
+        containerController.shouldShowCloseButton = NO;
+        containerController.shouldAnimateChanges = NO;
+        self.sidebarController = containerController;
+        UINavigationController *navContainer = [[UINavigationController alloc] initWithRootViewController:containerController];
+
+        // Create the split view controller
+        UISplitViewController *splitController = [UISplitViewController new];
+        splitController.delegate = self;
+        splitController.viewControllers = @[navContainer, navPDFController];
+        
+        // Use a dummy to present, as the split controller doesn't like to be presented modally.
+        [self addChildViewController:splitController];
+        [self.view addSubview:splitController.view];
+        [splitController didMoveToParentViewController:self];
+    }
+    return self;
+}
+
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    self.pdfController.splitViewController.view.frame = self.view.bounds;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - Properties
+
+- (PSPDFDocument *)document {
+    return self.pdfController.document;
+}
+
+// Update document everywhere.
+- (void)setDocument:(PSPDFDocument *)document {
+    self.pdfController.document = document;
+    // All controllers allow updating the document. If you add other controllers, customize this part to check for the selector.
+    [self.sidebarController.viewControllers setValue:document forKey:@"document"];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSPDFDocumentPickerControllerDelegate
+
+- (void)documentPickerController:(PSPDFDocumentPickerController *)documentPickerController didSelectDocument:(PSPDFDocument *)document page:(NSUInteger)pageIndex searchString:(NSString *)searchString {
+
+    // Set new document and dismiss.
+    self.document = document;
+    [self.pdfController dismissPopoverAnimated:YES class:documentPickerController.class completion:NULL];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - UISplitViewControllerDelegate
+
+- (void)splitViewController:(UISplitViewController *)svc willHideViewController:(UIViewController *)aViewController withBarButtonItem:(UIBarButtonItem *)barButtonItem forPopoverController:(UIPopoverController *)pc {
+    barButtonItem.title = @"Sidebar";
+    self.pdfController.leftBarButtonItems = @[self.pdfController.closeButtonItem, self.pickerButtonItem, barButtonItem];
+}
+
+- (void)splitViewController:(UISplitViewController *)svc willShowViewController:(UIViewController *)aViewController invalidatingBarButtonItem:(UIBarButtonItem *)barButtonItem {
+    self.pdfController.leftBarButtonItems = @[self.pdfController.closeButtonItem, self.pickerButtonItem];
+}
+
+- (void)splitViewController:(UISplitViewController *)svc popoverController:(UIPopoverController *)pc willPresentViewController:(UIViewController *)aViewController {
+    // Dismiss any other popovers.
+    [self.pdfController dismissPopoverAnimated:YES class:Nil completion:NULL];
 }
 
 @end
