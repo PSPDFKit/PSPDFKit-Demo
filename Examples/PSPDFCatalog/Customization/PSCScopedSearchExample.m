afbd78a4521f20bec537cae3e7d4802f428cfbfe
Peter Steinberger <me@petersteinberger.com>
2014-02-18 12:21:04 +0100

    Add search scope example

---
+++ Examples/PSPDFCatalog/Customization/PSCScopedSearchExample.m
@@ -0,0 +1,66 @@
+//
+//  PSCScopedSearchExample.m
+//  PSPDFCatalog
+//
+//  Copyright (c) 2014 PSPDFKit GmbH. All rights reserved.
+//
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
+//
+
+#import "PSCScopedSearchExample.h"
+#import "PSCAssetLoader.h"
+
+@interface PSCScopedSearchViewController : PSPDFSearchViewController
+@end
+
+@interface PSCScopedPDFViewController : PSPDFViewController
+@end
+
+@implementation PSCScopedSearchExample
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSCExample
+
+- (id)init {
+    if (self = [super init]) {
+        self.title = @"Add scope to PSPDFSearchViewController";
+        self.contentDescription = @"Allows more fine-grained search control with a custom scope bar.";
+        self.category = PSCExampleCategoryControllerCustomization;
+    }
+    return self;
+}
+
+- (UIViewController *)invokeWithDelegate:(id<PSCExampleRunnerDelegate>)delegate {
+    PSPDFDocument *document = [PSCAssetLoader sampleDocumentWithName:kHackerMagazineExample];
+    PSPDFViewController *pdfController = [[PSCScopedPDFViewController alloc] initWithDocument:document];
+    [pdfController overrideClass:PSPDFSearchViewController.class withClass:PSCScopedSearchViewController.class];
+    return pdfController;
+}
+
+@end
+
+@implementation PSCScopedSearchViewController
+
+- (UISearchBar *)createSearchBar {
+    UISearchBar *searchBar = [super createSearchBar];
+    // Add scopes
+    searchBar.scopeButtonTitles = @[@"This page", @"Everything"];
+    searchBar.showsScopeBar = YES;
+    return searchBar;
+}
+
+@end
+
+@implementation PSCScopedPDFViewController
+
+// The PSPDFSearchViewController has its delegate set to the PSPDFViewController, so subclass and add this method.
+- (NSIndexSet *)searchViewController:(PSPDFSearchViewController *)searchController searchRangeForScope:(NSString *)scope {
+    if ([scope isEqualToString:@"This page"]) {
+        return PSPDFIndexSetFromArray(self.visiblePageNumbers.array);
+    }else {
+        return nil; // all pages
+    }
+}
+
+@end
