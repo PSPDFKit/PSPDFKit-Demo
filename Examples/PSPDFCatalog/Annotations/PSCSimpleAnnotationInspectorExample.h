c2a2f25c2de93bca85a37f8a80ed4462893ea126
Peter Steinberger <me@petersteinberger.com>
2014-02-18 11:03:42 +0100

    Add new inspector example

---
+++ Examples/PSPDFCatalog/Annotations/PSCSimpleAnnotationInspectorExample.h
@@ -0,0 +1,14 @@
+//
+//  PSCSimpleAnnotationInspectorExample.h
+//  PSPDFCatalog
+//
+//  Copyright (c) 2014 PSPDFKit GmbH. All rights reserved.
+//
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
+//
+
+#import "PSCExample.h"
+
+@interface PSCSimpleAnnotationInspectorExample : PSCExample
+@end
---
+++ Examples/PSPDFCatalog/Annotations/PSCSimpleAnnotationInspectorExample.m
@@ -0,0 +1,87 @@
+//
+//  PSCSimpleAnnotationInspectorExample.m
+//  PSPDFCatalog
+//
+//  Copyright (c) 2014 PSPDFKit GmbH. All rights reserved.
+//
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
+//
+
+#import "PSCSimpleAnnotationInspectorExample.h"
+#import "PSCAssetLoader.h"
+
+@interface PSCSimpleAnnotationStyleViewController : PSPDFAnnotationStyleViewController
+@end
+
+@interface PSCSimpleAnnotationInspectorExample () <PSPDFViewControllerDelegate>
+@end
+
+@implementation PSCSimpleAnnotationInspectorExample
+
+- (id)init {
+    if (self = [super init]) {
+        self.title = @"Simple Annotation Inspector";
+        self.contentDescription = @"Shows how to customize the annotation inspector to hide certain properties, making it simpler.";
+        self.category = PSCExampleCategoryPDFAnnotations;
+        self.priority = 30;
+    }
+    return self;
+}
+
+- (UIViewController *)invokeWithDelegate:(id<PSCExampleRunnerDelegate>)delegate {
+    PSPDFDocument *document = [PSCAssetLoader sampleDocumentWithName:kHackerMagazineExample];
+    document.annotationSaveMode = PSPDFAnnotationSaveModeDisabled;
+    PSPDFViewController *pdfController = [[PSPDFViewController alloc] initWithDocument:document];
+
+    // This property can be used as well to customize what options are offered in the text menu.
+    // We customize this again later in the callback, but here's your chance to e.g. enable Wikipedia.
+    pdfController.allowedMenuActions = PSPDFTextSelectionMenuActionSearch|PSPDFTextSelectionMenuActionDefine;
+
+    // We use the delegate to customize the menu items.
+    pdfController.delegate = self;
+
+    // Overrides the inspector with our own subclass to dynamically modify what properties we want to show.
+    [pdfController overrideClass:PSPDFAnnotationStyleViewController.class withClass:PSCSimpleAnnotationStyleViewController.class];
+    return pdfController;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSPDFViewControllerDelegate
+
+// Limit menu options for highlight annotations (they don't use the annotation inspector)
+- (NSArray *)pdfViewController:(PSPDFViewController *)pdfController shouldShowMenuItems:(NSArray *)menuItems atSuggestedTargetRect:(CGRect)rect forAnnotations:(NSArray *)annotations inRect:(CGRect)annotationRect onPageView:(PSPDFPageView *)pageView {
+    return [menuItems filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(PSPDFMenuItem *menuItem, NSDictionary *bindings) {
+        // Make sure to also enable individual colors, as this callback will be also called for sub-menus.
+        return [@[PSPDFAnnotationMenuRemove, PSPDFAnnotationMenuColor, PSPDFAnnotationMenuOpacity, PSPDFAnnotationMenuColorWhite, PSPDFAnnotationMenuColorYellow, PSPDFAnnotationMenuColorGreen, PSPDFAnnotationMenuColorBlue] containsObject:menuItem.identifier];
+    }]];
+}
+
+// Limit the menu options when text is selected.
+- (NSArray *)pdfViewController:(PSPDFViewController *)pdfController shouldShowMenuItems:(NSArray *)menuItems atSuggestedTargetRect:(CGRect)rect forSelectedText:(NSString *)selectedText inRect:(CGRect)textRect onPageView:(PSPDFPageView *)pageView {
+    return [menuItems filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(PSPDFMenuItem *menuItem, NSDictionary *bindings) {
+        return [@[PSPDFTextMenuCopy, PSPDFTextMenuDefine, PSPDFAnnotationMenuHighlight] containsObject:menuItem.identifier];
+    }]];
+}
+
+@end
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSCSimpleAnnotationStyleViewController
+
+@implementation PSCSimpleAnnotationStyleViewController
+
+- (NSArray *)propertiesForAnnotations:(NSArray *)annotations {
+    NSArray *sections = [super propertiesForAnnotations:annotations];
+
+    // Allow only a smaller list of known properties in the inspector popover.
+    NSMutableArray *newSections = [NSMutableArray array];
+    for (NSArray *properties in sections) {
+        [newSections addObject:[properties filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(NSString *property, NSDictionary *bindings2) {
+            return [@[PROPERTY(color), PROPERTY(alpha), PROPERTY(lineWidth), PROPERTY(fontSize)] containsObject:property];
+        }]]];
+    }
+    return newSections;
+}
+
+@end
