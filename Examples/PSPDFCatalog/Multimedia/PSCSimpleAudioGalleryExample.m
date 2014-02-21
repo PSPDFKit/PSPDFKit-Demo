722a0b2abf65d81d815a5f91339193a6450ac090
Peter Steinberger <me@petersteinberger.com>
2014-02-21 09:17:25 +0100

    Add audio example

--- Examples/PSPDFCatalog/Multimedia/PSCSimpleAudioGalleryExample.m
+++ Examples/PSPDFCatalog/Multimedia/PSCSimpleAudioGalleryExample.m
@@ -1,8 +1,8 @@
 //
-//  PSCSimpleImageExample.m
+//  PSCSimpleAudioGalleryExample.m
 //  PSPDFCatalog
 //
-//  Copyright (c) 2012-2014 PSPDFKit GmbH. All rights reserved.
+//  Copyright (c) 2014 PSPDFKit GmbH. All rights reserved.
 //
 //  The PSPDFKit Sample applications are licensed with a modified BSD license.
 //  Please see License for details. This notice may not be removed from this file.
@@ -11,15 +11,15 @@
 #import "PSCExample.h"
 #import "PSCAssetLoader.h"
 
-@interface PSCSimpleImageExample : PSCExample @end
-@implementation PSCSimpleImageExample
+@interface PSCSimpleAudioGalleryExample : PSCExample @end
+@implementation PSCSimpleAudioGalleryExample
 
 - (id)init {
     if (self = [super init]) {
-        self.title = @"Simple Image Annotation";
-        self.contentDescription = @"Add inline images to the PDF.";
+        self.title = @"Simple Audio Annotation";
+        self.contentDescription = @"Add an mp3 to the PDF.";
         self.category = PSCExampleCategoryMultimedia;
-        self.priority = 400;
+        self.priority = 500;
     }
     return self;
 }
@@ -29,21 +29,12 @@
     document.annotationSaveMode = PSPDFAnnotationSaveModeDisabled;
 
     // Dynamically add gallery annotation.
-    PSPDFLinkAnnotation *galleryAnnotation = [[PSPDFLinkAnnotation alloc] initWithURLString:@"pspdfkit://https://pbs.twimg.com/media/Bfw58-LIYAAVYdW.jpg"];
+    PSPDFLinkAnnotation *galleryAnnotation = [[PSPDFLinkAnnotation alloc] initWithURLString:@"pspdfkit://[type:audio]http://api.soundcloud.com/tracks/82065282/download?client_id=20e3de80e29b918c89e5000a8489cc27"];
     CGRect pageRect = [document pageInfoForPage:0].rotatedPageRect;
     CGPoint center = CGPointMake(CGRectGetMidX(pageRect), CGRectGetMidY(pageRect));
-    CGSize size = CGSizeMake(400.f, 300.f);
+    CGSize size = CGSizeMake(400.f, 100.f);
     galleryAnnotation.boundingBox = CGRectMake(center.x - size.width / 2.f, center.y - size.height / 2.f, size.width, size.height);
     [document addAnnotations:@[galleryAnnotation]];
-
-    // Add simple image annotation
-    PSPDFLinkAnnotation *imageAnnotation = [[PSPDFLinkAnnotation alloc] initWithLinkAnnotationType:PSPDFLinkAnnotationImage];
-    imageAnnotation.fillColor = [UIColor clearColor];
-    imageAnnotation.URL = [NSURL fileURLWithPath:[NSBundle.mainBundle.bundlePath stringByAppendingPathComponent:@"alternative_note_image@2x.png"]];
-
-    imageAnnotation.boundingBox = (CGRect) {CGPointMake(3.f, 200.f), CGSizeMake(25.f, 25.f)};
-    imageAnnotation.page = 0;
-    [document addAnnotations:@[imageAnnotation]];
 
     PSPDFViewController *pdfController = [[PSPDFViewController alloc] initWithDocument:document];
     return pdfController;
