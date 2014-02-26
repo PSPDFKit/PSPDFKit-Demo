6fc4af4c2e53108c1559932fc064ea4d12ac7d71
Vic Smith <vic.gbsmith@gmail.com>
2014-02-26 16:20:20 +0000

    Changed to use new signature manager API.

--- Examples/PSPDFCatalog/Forms/PSCFormExamples.m
+++ Examples/PSPDFCatalog/Forms/PSCFormExamples.m
@@ -12,11 +12,8 @@
 #import "PSCAppDelegate.h"
 #import "PSCAssetLoader.h"
 
-static void PSPDFFormExampleAddTrustedCertificates() {
-    NSURL *samplesURL = [NSBundle.mainBundle.resourceURL URLByAppendingPathComponent:@"Samples"];
-    NSData *cert = [NSData dataWithContentsOfFile:[[samplesURL URLByAppendingPathComponent:@"JohnAppleseed.p7c"] path]];
-    [PSPDFDigitalSignatureManager.sharedManager addCertificate:cert error:nil];
-}
+#import "PSPDFDigitalCertificate.h"
+#import "PSPDFDigitalSigningIdentity.h"
 
 static PSPDFViewController *PSPDFFormExampleInvokeWithFilename(NSString *filename) {
     NSURL *samplesURL = [NSBundle.mainBundle.resourceURL URLByAppendingPathComponent:@"Samples"];
@@ -24,45 +21,77 @@ static PSPDFViewController *PSPDFFormExampleInvokeWithFilename(NSString *filenam
     return [[PSPDFViewController alloc] initWithDocument:document];
 }
 
-static PSPDFViewController *PSPDFFormExampleViewControllerForDocument(PSPDFDocument *document) {
-    PSPDFViewController *pdfController = [[PSPDFViewController alloc] initWithDocument:document];
-    pdfController.rightBarButtonItems = @[pdfController.searchButtonItem, pdfController.outlineButtonItem, pdfController.annotationButtonItem, pdfController.viewModeButtonItem];
-    pdfController.additionalBarButtonItems = @[pdfController.openInButtonItem, pdfController.bookmarkButtonItem, pdfController.brightnessButtonItem, pdfController.printButtonItem, pdfController.emailButtonItem];
-    return pdfController;
-}
-
-@interface PSCFormExampleSignatureDelegate : NSObject <PSPDFDigitalSignatureRevisionDelegate>
+@interface PSCFormExampleSignatureDelegate : NSObject <PSPDFDigitalSignatureManagerDelegate, PSPDFDigitalSignatureRevisionDelegate, PSPDFDigitalSignatureSigningDelegate>
 + (PSCFormExampleSignatureDelegate *)sharedDelegate;
+@property (atomic, strong) NSArray *certificates;
+@property (atomic, strong) NSArray *identities;
 @end
 
 // Use a singleton to control reactions to signature related events.
 @implementation PSCFormExampleSignatureDelegate
+
 + (PSCFormExampleSignatureDelegate *)sharedDelegate {
     static PSCFormExampleSignatureDelegate *delegate = nil;
     static dispatch_once_t pred;
     dispatch_once(&pred, ^{delegate = [PSCFormExampleSignatureDelegate new];});
     return delegate;
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSPDFDigitalSignatureRevisionDelegate and PSPDFDigitalSignatureSigningDelegate
+
 - (void)pdfRevisionRequested:(PSPDFDocument *)document verificationHandler:(id<PSPDFDigitalSignatureVerificationHandler>)handler {
-    PSPDFViewController *controller = PSPDFFormExampleViewControllerForDocument(document);
+    NSString *date = [NSDateFormatter localizedStringFromDate:handler.signature.timeSigned dateStyle:NSDateFormatterMediumStyle timeStyle:NSDateFormatterMediumStyle];
+    NSString *title = [NSString stringWithFormat:@"%@ (%@ - %@)", handler.documentProvider.document.title, date, handler.signature.name];
+    [self showDocument:document withTitle:title];
+}
+
+- (void)pdfSigned:(PSPDFDocument *)document signingHandler:(id<PSPDFDigitalSignatureSigningHandler>)handler {
+    [self showDocument:document withTitle:@"Test"];
+    
+}
+
+- (void)showDocument:(PSPDFDocument *)document withTitle:(NSString *)title {
+    PSPDFViewController *controller = [self viewControllerForDocument:document];
     controller.rightBarButtonItems = @[controller.searchButtonItem, controller.outlineButtonItem, controller.viewModeButtonItem];
     
-    NSString *date = [NSDateFormatter localizedStringFromDate:handler.signature.timeSigned dateStyle:NSDateFormatterMediumStyle timeStyle:NSDateFormatterMediumStyle];
-    document.title = [NSString stringWithFormat:@"%@ (%@ - %@)", handler.documentProvider.document.title, date, handler.signature.name];
-    document.annotationSaveMode = PSPDFAnnotationSaveModeDisabled;
+    if (title) document.title = title;
     
     PSCAppDelegate *appDelegate = UIApplication.sharedApplication.delegate;
     [appDelegate.catalog pushViewController:controller animated:YES];
 }
+
+- (PSPDFViewController *)viewControllerForDocument:(PSPDFDocument *)document {
+    PSPDFViewController *pdfController = [[PSPDFViewController alloc] initWithDocument:document];
+    pdfController.rightBarButtonItems = @[pdfController.searchButtonItem, pdfController.outlineButtonItem, pdfController.annotationButtonItem, pdfController.viewModeButtonItem];
+    pdfController.additionalBarButtonItems = @[pdfController.openInButtonItem, pdfController.bookmarkButtonItem, pdfController.brightnessButtonItem, pdfController.printButtonItem, pdfController.emailButtonItem];
+    return pdfController;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSPDFDigitalSignatureManagerDelegate
+
+- (NSArray *)trustedCertificates {
+    if (!self.certificates) {
+        NSURL *samplesURL = [NSBundle.mainBundle.resourceURL URLByAppendingPathComponent:@"Samples"];
+        NSData *cert = [NSData dataWithContentsOfFile:[[samplesURL URLByAppendingPathComponent:@"JohnAppleseed.p7c"] path]];
+        self.certificates = @[[PSPDFDigitalCertificate certificateFromData:cert]];
+    }
+    return self.certificates;
+}
+- (NSArray *)signingIdentities {
+    if (!self.identities) {
+        NSURL *samplesURL = [NSBundle.mainBundle.resourceURL URLByAppendingPathComponent:@"Samples"];
+        self.identities = @[[PSPDFDigitalSigningIdentity signingIdentityWithFilePath:[[samplesURL URLByAppendingPathComponent:@"JohnAppleseed.p12"] path] name:@"John Appleseed"]];
+    }
+    return self.identities;
+}
+- (BOOL)useAdobeCA {return YES;}
+- (NSArray *)revisionDelegates {return @[self];}
+- (NSArray *)signingDelegates {return @[self];}
+- (NSArray *)signingHandlers {return @[];}
+- (NSArray *)verificationHandlers {return @[];}
 @end
-
-static void PSPDFFormExampleRegisterForRevisionCallbacks() {
-    [PSPDFDigitalSignatureManager.sharedManager registerForReceivingRequestsToViewRevisions:PSCFormExampleSignatureDelegate.sharedDelegate];
-}
-
-static void PSPDFFormExampleDeregisterForRevisionCallbacks() {
-    [PSPDFDigitalSignatureManager.sharedManager deregisterFromReceivingRequestsToViewRevisions:PSCFormExampleSignatureDelegate.sharedDelegate];
-}
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 #pragma mark - PSCFormExample
@@ -74,18 +103,17 @@ static void PSPDFFormExampleDeregisterForRevisionCallbacks() {
         self.title = @"Example of a PDF Interactive Form";
         self.category = PSCExampleCategoryForms;
         self.priority = 20;
-        PSPDFFormExampleAddTrustedCertificates();
-        PSPDFFormExampleRegisterForRevisionCallbacks();
+        PSPDFDigitalSignatureManager.sharedManager.delegate = PSCFormExampleSignatureDelegate.sharedDelegate;
     }
     return self;
 }
 
 - (void)dealloc {
-    PSPDFFormExampleDeregisterForRevisionCallbacks();
+    PSPDFDigitalSignatureManager.sharedManager.delegate = nil;
 }
 
 - (UIViewController *)invokeWithDelegate:(id<PSCExampleRunner>)delegate {
-    return PSPDFFormExampleInvokeWithFilename(@"W8_Formular.pdf");
+    return PSPDFFormExampleInvokeWithFilename(@"Form_example.pdf");
 }
 @end
 
@@ -99,14 +127,13 @@ static void PSPDFFormExampleDeregisterForRevisionCallbacks() {
         self.title = @"Example of a PDF Interactive Form with a Digital Signature";
         self.category = PSCExampleCategoryForms;
         self.priority = 10;
-        PSPDFFormExampleAddTrustedCertificates();
-        PSPDFFormExampleRegisterForRevisionCallbacks();
+        PSPDFDigitalSignatureManager.sharedManager.delegate = PSCFormExampleSignatureDelegate.sharedDelegate;
     }
     return self;
 }
 
 - (void)dealloc {
-    PSPDFFormExampleDeregisterForRevisionCallbacks();
+    PSPDFDigitalSignatureManager.sharedManager.delegate = nil;
 }
 
 - (UIViewController *)invokeWithDelegate:(id<PSCExampleRunner>)delegate {
