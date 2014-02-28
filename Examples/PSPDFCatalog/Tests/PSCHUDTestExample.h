16e7339ee56791fb35ae51bd2c485bbdbdc98ccb
Peter Steinberger <me@petersteinberger.com>
2014-02-28 23:09:28 +0100

    rename test

--- Examples/PSPDFCatalog/Tests/PSCHUDTestExample.h
+++ Examples/PSPDFCatalog/Tests/PSCHUDTestExample.h
@@ -1,5 +1,5 @@
 //
-//  PSCHUDTestExmple.h
+//  PSCHUDTestExample.h
 //  PSPDFCatalog
 //
 //  Copyright (c) 2014 PSPDFKit GmbH. All rights reserved.
@@ -10,6 +10,6 @@
 
 #import "PSCExample.h"
 
-@interface PSCHUDTestExmple : PSCExample
+@interface PSCHUDTestExample : PSCExample
 
 @end
--- Examples/PSPDFCatalog/Tests/PSCHUDTestExample.m
+++ Examples/PSPDFCatalog/Tests/PSCHUDTestExample.m
@@ -1,5 +1,5 @@
 //
-//  PSCHUDTestExmple.m
+//  PSCHUDTestExample.m
 //  PSPDFCatalog
 //
 //  Copyright (c) 2014 PSPDFKit GmbH. All rights reserved.
@@ -8,9 +8,9 @@
 //  Please see License for details. This notice may not be removed from this file.
 //
 
-#import "PSCHUDTestExmple.h"
+#import "PSCHUDTestExample.h"
 
-@implementation PSCHUDTestExmple
+@implementation PSCHUDTestExample
 
 - (id)init {
     if (self = [super init]) {
@@ -30,42 +30,42 @@
     PSPDFStatusHUDItem *item = [PSPDFStatusHUDItem progressWithText:@"Hello"];
     item.title = @"Title";
     item.subtitle = @"Subtitle";
-    item.progress = 0.7;
+    item.progress = 0.7f;
     [item pushAnimated:YES];
-    [self performSelector:@selector(showError:) withObject:item afterDelay:1.0];
+    [self performSelector:@selector(showError:) withObject:item afterDelay:1.f];
 }
 
 - (void)showError:(PSPDFStatusHUDItem *)oldItem {
     PSPDFStatusHUDItem *item = [PSPDFStatusHUDItem errorWithText:@"Omg!"];
     item.title = @"Error";
     [item pushAnimated:YES];
-    [self performSelector:@selector(showSuccess:) withObject:item afterDelay:1.0];
+    [self performSelector:@selector(showSuccess:) withObject:item afterDelay:1.f];
 }
 
 - (void)showSuccess:(PSPDFStatusHUDItem *)oldItem {
     PSPDFStatusHUDItem *item = [PSPDFStatusHUDItem successWithText:@"Omg!"];
     item.title = @"Success";
     [item pushAnimated:YES];
-    [self performSelector:@selector(switchToBlackStyle:) withObject:item afterDelay:1.0];
+    [self performSelector:@selector(switchToBlackStyle:) withObject:item afterDelay:1.f];
 }
 
 - (void)switchToBlackStyle:(PSPDFStatusHUDItem *)oldItem {
     [oldItem setHUDStyle:PSPDFStatusHUDStyleBlack];
-    [self performSelector:@selector(switchToClearStyle:) withObject:oldItem afterDelay:1.0];
+    [self performSelector:@selector(switchToClearStyle:) withObject:oldItem afterDelay:1.f];
 }
 
 - (void)switchToClearStyle:(PSPDFStatusHUDItem *)oldItem {
     [oldItem setHUDStyle:PSPDFStatusHUDStyleClear];
-    [self performSelector:@selector(switchToGradientStyle:) withObject:oldItem afterDelay:1.0];
+    [self performSelector:@selector(switchToGradientStyle:) withObject:oldItem afterDelay:1.f];
 }
 
 - (void)switchToGradientStyle:(PSPDFStatusHUDItem *)oldItem {
     [oldItem setHUDStyle:PSPDFStatusHUDStyleGradient];
-    [self performSelector:@selector(popAll) withObject:nil afterDelay:2.0];
+    [self performSelector:@selector(popAll) withObject:nil afterDelay:2.f];
 }
 
 - (void)popAll {
-    [self performSelector:@selector(updateHud) withObject:nil afterDelay:2.0];
+    [self performSelector:@selector(updateHud) withObject:nil afterDelay:2.f];
 }
 
 @end
