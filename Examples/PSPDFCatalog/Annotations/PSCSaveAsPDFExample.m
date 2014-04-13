3beffdabf064d06768b22ef93d866f8bc4705f85
Peter Steinberger <steipete@gmail.com>
2014-04-13 15:05:25 +0200

    Improve the save as PDF example. Asks for saving before the controller is dismissed.

--- Examples/PSPDFCatalog/Annotations/PSCSaveAsPDFExample.m
+++ Examples/PSPDFCatalog/Annotations/PSCSaveAsPDFExample.m
@@ -16,7 +16,8 @@
 /// where the annotation should be saved, and optionally copies the file to a new location.
 @interface PSCSaveAsPDFViewController : PSPDFViewController
 @property (nonatomic, assign) BOOL hasUserBeenAskedAboutSaveLocation;
-@property (nonatomic, strong) PSPDFAlertView *annotationSaveAlertView;
+@property (nonatomic, strong) PSPDFActionSheet *annotationSaveActionSheet;
+@property (nonatomic, weak) id saveActionSender;
 @end
 
 @interface PSCAnnotationsSaveAsForAnnotationEditingExample : PSCExample @end
@@ -38,9 +39,9 @@
 - (UIViewController *)invokeWithDelegate:(id<PSCExampleRunnerDelegate>)delegate {
     NSURL *samplesURL = [NSBundle.mainBundle.resourceURL URLByAppendingPathComponent:@"Samples"];
     NSURL *documentURL = [samplesURL URLByAppendingPathComponent:kHackerMagazineExample];
-    NSURL *writableDocumentURL = PSCCopyFileURLToDocumentFolderAndOverride(documentURL, YES);
-    PSPDFDocument *linkDocument = [PSPDFDocument documentWithURL:writableDocumentURL];
-    return [[PSCSaveAsPDFViewController alloc] initWithDocument:linkDocument];
+    NSURL *writableDocumentURL = PSCCopyFileURLToDocumentFolderAndOverride(documentURL, NO);
+    PSPDFDocument *document = [PSPDFDocument documentWithURL:writableDocumentURL];
+    return [[PSCSaveAsPDFViewController alloc] initWithDocument:document];
 }
 
 @end
@@ -53,6 +54,9 @@
 - (void)commonInitWithDocument:(PSPDFDocument *)document {
     [super commonInitWithDocument:document];
     self.rightBarButtonItems = @[self.annotationButtonItem, self.viewModeButtonItem];
+
+    UIBarButtonItem *closeButton = [[UIBarButtonItem alloc] initWithTitle:PSPDFLocalize(@"Close") style:UIBarButtonItemStylePlain target:self action:@selector(closeButtonPressed:)];
+    self.leftBarButtonItems = @[closeButton];
 
     // PSPDFViewController will unregister all notifications on dealloc.
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(annotationChangedNotification:) name:PSPDFAnnotationChangedNotification object:nil];
@@ -68,6 +72,21 @@
 ///////////////////////////////////////////////////////////////////////////////////////////
 #pragma mark - Private
 
+- (void)closeButtonPressed:(id)sender {
+    self.saveActionSender = sender;
+    self.annotationStateManager.state = nil; // Commit any annotations.
+    self.saveActionSender = nil;
+
+    UINavigationController *navigationController = self.navigationController;
+    if (!self.annotationSaveActionSheet.isVisible) {
+        [navigationController popViewControllerAnimated:YES];
+    }else {
+        [self.annotationSaveActionSheet addWillDismissBlock:^(NSInteger buttonIndex) {
+            [navigationController popViewControllerAnimated:YES];
+        }];
+    }
+}
+
 - (void)annotationChangedNotification:(NSNotification *)notification {
     [self processChangeForAnnotation:notification.object];
 }
@@ -81,71 +100,85 @@
 - (void)processChangeForAnnotation:(PSPDFAnnotation *)annotation {
     if (annotation.document == self.document) {
         if (!self.hasUserBeenAskedAboutSaveLocation) {
-            // notification might not be on main thread.
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [self askUserAboutSaveLocation];
-            });
+            // The notification might not be on main thread.
+            if (NSThread.isMainThread) {
+                [self askUserAboutSaveLocationFromSender:self.saveActionSender];
+            }else {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self askUserAboutSaveLocationFromSender:self.saveActionSender];
+                });
+            }
         }
     }
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - Document Copying Logic
+
 // This code assumes that the PDF location itself is writeable, and will fail for documents in the bundle folder.
-- (void)askUserAboutSaveLocation {
-    if (self.annotationSaveAlertView) return; // don't show multiple times
-
-    self.annotationSaveAlertView = [[PSPDFAlertView alloc] initWithTitle:@"Annotation Save Location" message:@"Would you like to save annotations into the current file, or create a copy to save the annotation changes?"];
-
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-retain-cycles"
-    [self.annotationSaveAlertView addButtonWithTitle:@"Save to this file" block:^{
+- (void)askUserAboutSaveLocationFromSender:(id)sender {
+    PSPDFActionSheet *annotationSaveActionSheet = [[PSPDFActionSheet alloc] initWithTitle:NSLocalizedString(@"Would you like to save annotations into the current file, or create a copy to save the annotation changes?", @"")];
+    [annotationSaveActionSheet setDestructiveButtonWithTitle:@"Save to this file" block:^(NSInteger buttonIndex) {
         self.hasUserBeenAskedAboutSaveLocation = YES;
         // We're all set, don't need to do more.
     }];
-    [self.annotationSaveAlertView addButtonWithTitle:@"Create Copy" block:^{
+    [annotationSaveActionSheet addButtonWithTitle:@"Save as Copy" block:^(NSInteger buttonIndex) {
         self.hasUserBeenAskedAboutSaveLocation = YES;
-
-        // Build new URL, tests for a filename that doesn't yet exist.
-        NSUInteger appendFileCount = 0;
-        NSString *newPath;
-        do {
-            newPath = self.document.fileURL.path;
-            NSString *appendSuffix = [NSString stringWithFormat:@"_annotated%@.pdf", appendFileCount == 0 ? @"" : @(appendFileCount)];
-            if ([newPath.lowercaseString hasSuffix:@".pdf"]) {
-                newPath = [newPath stringByReplacingOccurrencesOfString:@".pdf" withString:appendSuffix options:NSCaseInsensitiveSearch range:NSMakeRange(newPath.length-4, 4)];
-            }else {
-                newPath = [newPath stringByAppendingString:appendSuffix];
-            }
-            appendFileCount++;
-        }while ([NSFileManager.defaultManager fileExistsAtPath:newPath]);
-        NSURL *newURL = [NSURL fileURLWithPath:newPath];
-
-        NSError *error;
-        if (![NSFileManager.defaultManager copyItemAtURL:self.document.fileURL toURL:newURL error:&error]) {
-            NSLog(@"Failed to copy file to %@: %@", newURL.path, error.localizedDescription);
-        }else {
-            // Since the annotation has already been edited, we copy the file *before* it will be saved
-            // then save the current state and switch out the documents.
-            if (![self.document saveAnnotationsWithError:&error]) {
-                NSLog(@"Failed to save annotations: %@", error.localizedDescription);
-            }
-            NSURL *tmpURL = [newURL URLByAppendingPathExtension:@"temp"];
-            if (![NSFileManager.defaultManager moveItemAtURL:self.document.fileURL toURL:tmpURL error:&error]) {
-                NSLog(@"Failed to move file: %@", error.localizedDescription); return;
-            }
-            if (![NSFileManager.defaultManager moveItemAtURL:newURL toURL:self.document.fileURL error:&error]) {
-                NSLog(@"Failed to move file: %@", error.localizedDescription); return;
-            }
-            if (![NSFileManager.defaultManager moveItemAtURL:tmpURL toURL:newURL error:&error]) {
-                NSLog(@"Failed to move file: %@", error.localizedDescription); return;
-            }
-            // Finally update the fileURL, this will clear the current document cache.
-            PSPDFDocument *newDocument = [PSPDFDocument documentWithURL:newURL];
-            newDocument.title = self.document.title; // preserve title.
-            self.document = newDocument;
-        }
+        [self replaceDocumentWithCopy];
     }];
-#pragma clang diagnostic pop
-    [self.annotationSaveAlertView show];
+    [annotationSaveActionSheet addWillDismissBlock:^(NSInteger buttonIndex) {
+        self.annotationSaveActionSheet = nil;
+    }];
+    [annotationSaveActionSheet addCancelBlock:^(NSInteger buttonIndex) {
+        self.hasUserBeenAskedAboutSaveLocation = YES;
+    }];
+    annotationSaveActionSheet.allowsTapToDismiss = NO;
+    [annotationSaveActionSheet showWithSender:sender fallbackView:self.view animated:YES];
+    self.annotationSaveActionSheet = annotationSaveActionSheet;
+    
+    if (self.annotationSaveActionSheet.isVisible) return;
+}
+
+- (void)replaceDocumentWithCopy {
+    // Build new URL, tests for a filename that doesn't yet exist.
+    NSUInteger appendFileCount = 0;
+    NSString *newPath;
+    do {
+        newPath = self.document.fileURL.path;
+        NSString *appendSuffix = [NSString stringWithFormat:@"_annotated%@.pdf", appendFileCount == 0 ? @"" : @(appendFileCount)];
+        if ([newPath.lowercaseString hasSuffix:@".pdf"]) {
+            newPath = [newPath stringByReplacingOccurrencesOfString:@".pdf" withString:appendSuffix options:NSCaseInsensitiveSearch range:NSMakeRange(newPath.length-4, 4)];
+        }else {
+            newPath = [newPath stringByAppendingString:appendSuffix];
+        }
+        appendFileCount++;
+    }while ([NSFileManager.defaultManager fileExistsAtPath:newPath]);
+    NSURL *newURL = [NSURL fileURLWithPath:newPath];
+
+    NSError *error;
+    if (![NSFileManager.defaultManager copyItemAtURL:self.document.fileURL toURL:newURL error:&error]) {
+        NSLog(@"Failed to copy file to %@: %@", newURL.path, error.localizedDescription);
+    }else {
+        // Since the annotation has already been edited, we copy the file *before* it will be saved
+        // then save the current state and switch out the documents.
+        if (![self.document saveAnnotationsWithError:&error]) {
+            NSLog(@"Failed to save annotations: %@", error.localizedDescription);
+        }
+        NSURL *tmpURL = [newURL URLByAppendingPathExtension:@"temp"];
+        if (![NSFileManager.defaultManager moveItemAtURL:self.document.fileURL toURL:tmpURL error:&error]) {
+            NSLog(@"Failed to move file: %@", error.localizedDescription); return;
+        }
+        if (![NSFileManager.defaultManager moveItemAtURL:newURL toURL:self.document.fileURL error:&error]) {
+            NSLog(@"Failed to move file: %@", error.localizedDescription); return;
+        }
+        if (![NSFileManager.defaultManager moveItemAtURL:tmpURL toURL:newURL error:&error]) {
+            NSLog(@"Failed to move file: %@", error.localizedDescription); return;
+        }
+        // Finally update the fileURL, this will clear the current document cache.
+        PSPDFDocument *newDocument = [PSPDFDocument documentWithURL:newURL];
+        newDocument.title = self.document.title; // preserve title.
+        self.document = newDocument;
+    }
 }
 
 @end
