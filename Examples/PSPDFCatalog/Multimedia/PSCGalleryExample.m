2bbf86c5f46f7280db6ddb2c5731938d7aaef824
Peter Steinberger <steipete@gmail.com>
2014-04-17 09:54:23 +0200

    Remove unnecessary import

--- Examples/PSPDFCatalog/Multimedia/PSCGalleryExample.m
+++ Examples/PSPDFCatalog/Multimedia/PSCGalleryExample.m
@@ -10,7 +10,6 @@
 
 #import "PSCExample.h"
 #import "PSCAssetLoader.h"
-#import "PSPDFGalleryContainerView.h"
 
 @interface PSCGalleryExampleCustomEmbeddedBackgroundView : PSPDFGalleryEmbeddedBackgroundView @end
 @implementation PSCGalleryExampleCustomEmbeddedBackgroundView @end
