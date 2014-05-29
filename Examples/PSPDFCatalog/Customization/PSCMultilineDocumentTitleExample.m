62288d2f1b7eb1e7546705dcf8a66eda6d02df56
Matej Bukovinski <matej@bukovinski.com>
2014-05-29 16:05:20 +0200

    Added a PSCExample for multiline titles.

---
+++ Examples/PSPDFCatalog/Customization/PSCMultilineDocumentTitleExample.m
@@ -0,0 +1,37 @@
+//
+//  PSCMultilineDocumentTitleExample.m
+//  PSPDFCatalog
+//
+//  Copyright (c) 2011-2014 PSPDFKit GmbH. All rights reserved.
+//
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
+//
+
+#import "PSCExample.h"
+#import "PSCAssetLoader.h"
+#import "PSCKioskPDFViewController.h"
+
+@interface PSCMultilineDocumentTitleExample : PSCExample @end
+@implementation PSCMultilineDocumentTitleExample
+
+- (id)init {
+    if (self = [super init]) {
+        self.title = @"Multiline document title";
+        self.category = PSCExampleCategoryViewCustomization;
+    }
+    return self;
+}
+
+- (UIViewController *)invokeWithDelegate:(id<PSCExampleRunnerDelegate>)delegate {
+    PSPDFDocument *document = [PSCAssetLoader sampleDocumentWithName:kHackerMagazineExample];
+	document.title = @"This PDF document has a pretty long title. It should wrap into multiple lines on an iPhone.";
+    PSPDFViewController *pdfController = [[PSCKioskPDFViewController alloc] initWithDocument:document];
+
+	// The standard numberOfLines conventions apply here (0 = use as many lines as needed)
+	pdfController.HUDView.documentLabel.label.numberOfLines = 0;
+
+	return pdfController;
+}
+
+@end
