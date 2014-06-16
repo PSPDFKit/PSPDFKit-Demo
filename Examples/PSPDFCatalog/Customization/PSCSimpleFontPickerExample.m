8970886e00b61f95cb820820fa635fc2eadd9407
Peter Steinberger <steipete@gmail.com>
2014-06-15 19:56:24 -0700

    Add new font picker example

---
+++ Examples/PSPDFCatalog/Customization/PSCSimpleFontPickerExample.m
@@ -0,0 +1,88 @@
+//
+//  PSCSimpleFontPickerExample.m
+//  PSPDFCatalog
+//
+//  Copyright (c) 2014 PSPDFKit GmbH. All rights reserved.
+//
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
+//
+
+#import "PSCExample.h"
+#import "PSCAssetLoader.h"
+
+@interface PSCSimpleFontSelectorViewController : PSPDFFontSelectorViewController @end
+
+@interface PSCSimpleFontPickerExample : PSCExample <PSPDFViewControllerDelegate> @end
+@implementation PSCSimpleFontPickerExample
+
+- (id)init {
+    if (self = [super init]) {
+        self.title = @"Simplified Font Picker";
+        self.category = PSCExampleCategoryViewCustomization;
+    }
+    return self;
+}
+
+- (UIViewController *)invokeWithDelegate:(id<PSCExampleRunnerDelegate>)delegate {
+    PSPDFDocument *document = [PSCAssetLoader sampleDocumentWithName:kHackerMagazineExample];
+    document.annotationSaveMode = PSPDFAnnotationSaveModeDisabled;
+
+    // Create a sample annotation.
+    PSPDFFreeTextAnnotation *freeText = [[PSPDFFreeTextAnnotation alloc] initWithContents:@"This is a test free-text annotation."];
+    freeText.fillColor = UIColor.whiteColor;
+    freeText.fontSize = 30.f;
+    freeText.boundingBox = CGRectMake(300.f, 300.f, 150.f, 150.f);
+    [freeText sizeToFit];
+    [document addAnnotations:@[freeText]];
+
+    PSPDFViewController *pdfController = [[PSPDFViewController alloc] initWithDocument:document];
+    [pdfController overrideClass:PSPDFFontSelectorViewController.class withClass:PSCSimpleFontSelectorViewController.class];
+    pdfController.delegate = self;
+    return pdfController;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSPDFViewControllerDelegate
+
+- (void)pdfViewController:(PSPDFViewController *)pdfController didShowPageView:(PSPDFPageView *)pageView {
+    // Just a convenience helper to automatically select the free text annotation.
+    pageView.selectedAnnotations = [pdfController.document annotationsForPage:0 type:PSPDFAnnotationTypeFreeText];
+    [pageView showMenuIfSelectedAnimated:YES];
+}
+
+@end
+
+@implementation PSCSimpleFontSelectorViewController
+
++ (NSArray *)allowedFonts {
+    // Very strange, if Courier font isnt added Courier New wont appear
+    return @[@"Arial", @"Calibri", @"Times New Roman", @"Courier New", @"Helvetica", @"Comic Sans MS"]; //, @"Courier"
+}
+
++ (NSArray *)defaultBlocklist {
+    NSArray *allAvailableFonts = [UIFont familyNames];
+
+    NSMutableArray *newBlockList = [NSMutableArray array];
+    NSArray *allowedFonts = [self allowedFonts];
+
+    [allAvailableFonts enumerateObjectsUsingBlock:^(NSString *font, NSUInteger idx, BOOL *stop) {
+        if (![allowedFonts containsObject:font]) {
+            [newBlockList addObject:font];
+        } else {
+            NSLog(@"Font: %@", font);
+        }
+    }];
+
+    return newBlockList;
+}
+
+- (BOOL)showDownloadableFonts {
+    return NO;
+}
+
+- (BOOL)searchEnabled {
+    return NO;
+}
+
+@end
