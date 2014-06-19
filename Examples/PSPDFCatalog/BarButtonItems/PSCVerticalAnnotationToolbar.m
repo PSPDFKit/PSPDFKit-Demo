efbd23277eb0de503c8664f797cebf01954f6917
Matej Bukovinski <matej@bukovinski.com>
2014-06-19 16:31:05 +0200

    New PSCVerticalAnnotationToolbarExample based on PSPDFAnnotationStateManager.

---
+++ Examples/PSPDFCatalog/BarButtonItems/PSCVerticalAnnotationToolbar.m
@@ -0,0 +1,89 @@
+//
+//  PSCVerticalAnnotationToolbar.m
+//  PSPDFCatalog
+//
+//  Copyright (c) 2012-2014 PSPDFKit GmbH. All rights reserved.
+//
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
+//
+
+#import "PSCVerticalAnnotationToolbar.h"
+
+@interface PSCVerticalAnnotationToolbar() <PSPDFAnnotationStateManagerDelegate>
+@property (nonatomic, strong) UIButton *drawButton;
+@property (nonatomic, strong) UIButton *freeTextButton;
+@end
+
+@implementation PSCVerticalAnnotationToolbar
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - NSObject
+
+- (id)initWithAnnotationStateManager:(PSPDFAnnotationStateManager *)annotationStateManager {
+    if ((self = [super init])) {
+        _annotationStateManager = annotationStateManager;
+		annotationStateManager.stateDelegate = self;
+
+		PSPDFViewController *pdfController = annotationStateManager.pdfController;
+		self.backgroundColor = pdfController.navigationController.navigationBar.barTintColor;
+		
+        // draw button
+        if ([pdfController.document.editableAnnotationTypes containsObject:PSPDFAnnotationStringInk]) {
+            UIButton *drawButton = [UIButton buttonWithType:UIButtonTypeCustom];
+            UIImage *sketchImage = [PSPDFBundleImage(@"ink") imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+            [drawButton setImage:sketchImage forState:UIControlStateNormal];
+            [drawButton addTarget:self action:@selector(inkButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+            [self addSubview:drawButton];
+            self.drawButton = drawButton;
+        }
+
+        // draw button
+        if ([pdfController.document.editableAnnotationTypes containsObject:PSPDFAnnotationStringFreeText]) {
+			UIButton *freetextButton = [UIButton buttonWithType:UIButtonTypeCustom];
+			UIImage *freeTextImage = [PSPDFBundleImage(@"freetext") imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+			[freetextButton setImage:freeTextImage forState:UIControlStateNormal];
+			[freetextButton addTarget:self action:@selector(freetextButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+			[self addSubview:freetextButton];
+			self.freeTextButton = freetextButton;
+        }
+    }
+    return self;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - UIView
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+
+    CGRect rem = self.bounds, slice;
+    CGRectDivide(rem, &slice, &rem, 44.f, CGRectMinYEdge);
+    self.drawButton.frame = slice;
+    self.freeTextButton.frame = rem;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - Events
+
+- (void)inkButtonPressed:(id)sender {
+	[self.annotationStateManager toggleState:PSPDFAnnotationStringInk];
+}
+
+- (void)freetextButtonPressed:(id)sender {
+	[self.annotationStateManager toggleState:PSPDFAnnotationStringFreeText];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSPDFAnnotationStateManagerDelegate
+
+- (void)annotationStateManager:(PSPDFAnnotationStateManager *)manager didChangeState:(NSString *)state to:(NSString *)newState variant:(NSString *)variant to:(NSString *)newVariant {
+	
+	UIColor *selectedColor = [UIColor colorWithWhite:0.f alpha:.2f];
+	UIColor *deselectedColor = [UIColor clearColor];
+	
+	self.freeTextButton.backgroundColor = newState == PSPDFAnnotationStringFreeText ? selectedColor : deselectedColor;
+	self.drawButton.backgroundColor = newState == PSPDFAnnotationStringInk ? selectedColor : deselectedColor;
+}
+
+@end
---
+++ Examples/PSPDFCatalog/BarButtonItems/PSCVerticalAnnotationToolbarExample.m
@@ -0,0 +1,67 @@
+//
+//  PSCVerticalAnnotationToolbarExample.m
+//  PSPDFCatalog
+//
+//  Copyright (c) 2012-2014 PSPDFKit GmbH. All rights reserved.
+//
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
+//
+
+#import "PSCVerticalAnnotationToolbar.h"
+#import "PSCExample.h"
+#import "PSCAssetLoader.h"
+
+@interface PSCExampleAnnotationViewController : PSPDFViewController
+@property (nonatomic, strong) PSCVerticalAnnotationToolbar *verticalToolbar;
+@end
+
+@interface PSCVerticalAnnotationToolbarExample : PSCExample @end
+@implementation PSCVerticalAnnotationToolbarExample
+
+- (id)init {
+    if (self = [super init]) {
+        self.title = @"Vertical always-visible annotation bar";
+        self.contentDescription = @"Custom, vertically aligned annotation toolbar.";
+        self.category = PSCExampleCategoryViewCustomization;
+        self.priority = 30;
+    }
+    return self;
+}
+
+- (UIViewController *)invokeWithDelegate:(id<PSCExampleRunnerDelegate>)delegate {
+    PSPDFDocument *document = [PSCAssetLoader sampleDocumentWithName:kHackerMagazineExample];
+    PSPDFViewController *controller = [[PSCExampleAnnotationViewController alloc] initWithDocument:document];
+	// Remove the default annotationBarButtonItem
+	NSMutableArray *items = [controller.rightBarButtonItems mutableCopy];
+	[items removeObject:controller.annotationButtonItem];
+	controller.rightBarButtonItems = items;
+    return controller;
+}
+
+@end
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSCExampleAnnotationViewController
+
+@implementation PSCExampleAnnotationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // create the custom toolbar and add it on top of the HUDView.
+    self.verticalToolbar = [[PSCVerticalAnnotationToolbar alloc] initWithAnnotationStateManager:self.annotationStateManager];
+    self.verticalToolbar.frame = CGRectIntegral(CGRectMake(self.view.bounds.size.width-44.f, (self.view.bounds.size.height-44.f)/2, 44.f, 44.f * 2));
+    self.verticalToolbar.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleBottomMargin;
+    [self.HUDView addSubview:self.verticalToolbar];
+}
+
+- (void)setViewMode:(PSPDFViewMode)viewMode animated:(BOOL)animated {
+    [super setViewMode:viewMode animated:animated];
+	// ensure custom toolbar is hidden when we show the thumbnails
+    [UIView animateWithDuration:0.25 delay:0.f options:UIViewAnimationOptionAllowUserInteraction animations:^{
+        self.verticalToolbar.alpha = viewMode == PSPDFViewModeThumbnails ? 0.f : 1.f;
+    } completion:NULL];
+}
+
+@end
