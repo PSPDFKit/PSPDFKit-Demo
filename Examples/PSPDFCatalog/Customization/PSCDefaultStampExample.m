9e8cc07dfcbda68073df3b2203284033d20954f9
Peter Steinberger <steipete@gmail.com>
2014-08-30 23:04:29 +0200

    API updates

--- Examples/PSPDFCatalog/Customization/PSCDefaultStampExample.m
+++ Examples/PSPDFCatalog/Customization/PSCDefaultStampExample.m
@@ -149,7 +149,7 @@ static BOOL PSCIsStampModeEnabledForPDFController(PSPDFViewController *pdfContro
     [super longPress:recognizer];
 
     // Remember start point and handle it as a tap if move distance is below a certain treshold.
-    PSPDFViewController *pdfController = self.configurationDataSource.actionDelegate;
+    PSPDFViewController *pdfController = self.configurationDataSource.pdfController;
     if (PSCIsStampModeEnabledForPDFController(pdfController)) {
         if (recognizer.state == UIGestureRecognizerStateBegan) {
             _startPoint = [recognizer locationInView:self];
@@ -172,7 +172,7 @@ static BOOL PSCIsStampModeEnabledForPDFController(PSPDFViewController *pdfContro
 
     // If not enabled yet, check if we're in our special stamp mode and allow long press there.
     // (Usually, long press is blocked when the annotation toolbar is active)
-    if (!shouldBegin && PSCIsStampModeEnabledForPDFController(self.configurationDataSource.actionDelegate) &&
+    if (!shouldBegin && PSCIsStampModeEnabledForPDFController(self.configurationDataSource.pdfController) &&
         [gestureRecognizer isKindOfClass:UILongPressGestureRecognizer.class]) {
         shouldBegin = YES;
     }
@@ -182,7 +182,7 @@ static BOOL PSCIsStampModeEnabledForPDFController(PSPDFViewController *pdfContro
 - (BOOL)pressRecognizerShouldHandlePressImmediately:(PSPDFLongPressGestureRecognizer *)recognizer {
     // Select a stamp annotation as soon as we start touching it, making dragging instant.
     id <PSPDFConfigurationDataSource> configDataSource = self.configurationDataSource;
-    if (PSCIsStampModeEnabledForPDFController(configDataSource.actionDelegate)) {
+    if (PSCIsStampModeEnabledForPDFController(configDataSource.pdfController)) {
         for (PSPDFPageView *pageView in configDataSource.visiblePageViews) {
             NSArray *stampAnnotations = PSCStampAnnotationsAtPoint(pageView, [recognizer locationInView:pageView]);
             if (stampAnnotations.count > 0) {
