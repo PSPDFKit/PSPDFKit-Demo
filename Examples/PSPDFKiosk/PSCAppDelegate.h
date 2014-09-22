376fdef7acf33b7ba6e91b318d6ef56a9a46a340
Peter Steinberger <steipete@gmail.com>
2014-09-23 00:08:07 +0200

    Update remaining module usage.

--- Examples/PSPDFKiosk/PSCAppDelegate.h
+++ Examples/PSPDFKiosk/PSCAppDelegate.h
@@ -8,7 +8,7 @@
 //  Please see License for details. This notice may not be removed from this file.
 //
 
-@import UIKit;
+#import <UIKit/UIKit.h>
 @class PSCGridViewController;
 
 @interface PSCAppDelegate : UIResponder <UIApplicationDelegate>
