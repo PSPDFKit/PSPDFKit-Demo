e0a6c4ab1c78821f3871c62f81ed958779dfad6b
Petr Pavlik <pavlipe7@gmail.com>
2014-10-07 15:55:00 +0200

    Fix Zooming search sample with PSPDFKit 4.0.3

--- Examples/PSPDFCatalog/Customization/PSCZoomingSearchExample.m
+++ Examples/PSPDFCatalog/Customization/PSCZoomingSearchExample.m
@@ -11,7 +11,11 @@
 #import "PSCAssetLoader.h"
 #import "PSCExample.h"
 
-@interface PSCZoomingSearchPDFViewController : PSPDFViewController @end
+@interface PSCZoomingSearchPDFViewController : PSPDFViewController
+
+@property (nonatomic, assign) dispatch_once_t searchPresentationOnceToken;
+
+@end
 
 @interface PSCZoomingSearchExample : PSCExample @end
 @implementation PSCZoomingSearchExample
@@ -30,6 +34,11 @@
 - (UIViewController *)invokeWithDelegate:(id<PSCExampleRunnerDelegate>)delegate {
     PSPDFDocument *document = [PSCAssetLoader sampleDocumentWithName:kHackerMagazineExample];
     PSPDFViewController *pdfController = [[PSCZoomingSearchPDFViewController alloc] initWithDocument:document];
+    
+    [pdfController updateConfigurationWithBuilder:^(PSPDFConfigurationBuilder *builder) {
+        builder.searchMode = PSPDFSearchModeModal;
+    }];
+    
     return pdfController;
 }
 
@@ -42,9 +51,12 @@
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-
-    // Automatically start searching.
-    [self searchForString:@"Tomat" options:nil sender:nil animated:YES];
+    
+    // Prevents search to reappear when PSPDFSearchViewController is dismissed.
+    dispatch_once(&_searchPresentationOnceToken, ^{
+        // Automatically start searching.
+        [self searchForString:@"Tomat" options:nil sender:nil animated:YES];
+    });
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -52,9 +64,9 @@
 
 - (void)searchViewController:(PSPDFSearchViewController *)searchController didTapSearchResult:(PSPDFSearchResult *)searchResult {
     [super searchViewController:searchController didTapSearchResult:searchResult];
-
+    
     CGRect viewRect = [[self pageViewForPage:searchResult.pageIndex] convertPDFRectToViewRect:searchResult.selection.frame];
-//    viewRect = CGRectInset(viewRect, 20.f, 20.f); // leave some space
+    //    viewRect = CGRectInset(viewRect, 20.f, 20.f); // leave some space
     [self zoomToRect:viewRect page:searchResult.pageIndex animated:YES];
 }
 
