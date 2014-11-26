bc418f035feda080bc5e9f599734e51bab6c9065
Peter Steinberger <steipete@gmail.com>
2014-11-26 23:50:25 +0100

    Add security auditor example

---
+++ Examples/PSPDFCatalog/Plugins/PSCDisallowCopySecurityAuditor.h
@@ -0,0 +1,14 @@
+//
+//  PSCDisallowCopySecurityAuditor.h
+//  PSPDFCatalog
+//
+//  Copyright (c) 2014 PSPDFKit GmbH. All rights reserved.
+//
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PSCDisallowCopySecurityAuditor : NSObject <PSPDFSecurityAuditor>
+@end
---
+++ Examples/PSPDFCatalog/Plugins/PSCDisallowCopySecurityAuditor.m
@@ -0,0 +1,48 @@
+//
+//  PSCDisallowCopySecurityAuditor.m
+//  PSPDFCatalog
+//
+//  Copyright (c) 2014 PSPDFKit GmbH. All rights reserved.
+//
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
+//
+
+#import "PSCDisallowCopySecurityAuditor.h"
+
+@implementation PSCDisallowCopySecurityAuditor
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSPDFPlugin
+
++ (NSDictionary *)pluginInfo {
+    return @{PSPDFPluginIdentifierKey: @"com.pspdfcatalog.security.custom-auditor",
+             PSPDFPluginNameKey: @"Custom Security Auditor",
+             PSPDFPluginProtocolVersionKey : @(PSPDFPluginProtocolVersion_1),
+
+             // Remove this key to actually enable the plugin.
+             // PSPDFKit will discover this class automatically.
+             PSPDFPluginEnabledKey: @NO
+             };
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - NSObject
+
+- (instancetype)initWithPluginRegistry:(id<PSPDFPluginRegistry>)pluginRegistry options:(NSDictionary *)options {
+    if ((self = [super init])) {
+    }
+    return self;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - PSPDFSecurityAuditor
+
+- (BOOL)hasPermissionForEvent:(PSPDFSecurityEvent)event isUserAction:(BOOL)isUserAction {
+    if (event == PSPDFSecurityEventCopyPaste) {
+        return NO;
+    }
+    return YES;
+}
+
+@end
